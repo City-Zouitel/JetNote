16a1abef9a10539515542ce3c08349ac36f52261
Yon <bourntivoli@gmail.com>
2023-06-12 14:09:53 +0100

    12/06/2023.

--- app/src/main/java/com/example/mobile/NoteApplication.kt
+++ app/src/main/java/com/example/mobile/NoteApplication.kt
@@ -12,7 +12,7 @@ import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class NoteApplication: Application(), Configuration.Provider {
+class NoteApplication: Application()/*, Configuration.Provider */ {
 
     override fun onCreate() {
         super.onCreate()
@@ -50,7 +50,7 @@ class NoteApplication: Application(), Configuration.Provider {
     @Inject
     lateinit var hiltWorkerFactory: HiltWorkerFactory
 
-    override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder().setWorkerFactory(hiltWorkerFactory).build()
-    }
+//    override fun getWorkManagerConfiguration(): Configuration {
+//        return Configuration.Builder().setWorkerFactory(hiltWorkerFactory).build()
+//    }
 }
