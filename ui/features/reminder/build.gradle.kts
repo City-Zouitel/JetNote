5c2d85802c8fc601a3bf71a141ddbc04bbc9d246
ylnkhgtlowbnd <bourntivoli@gmail.com>
2024-06-07 00:02:43 +0100

    06/06/2024.

--- ui/features/reminder/build.gradle.kts
+++ ui/features/reminder/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.cityzouitel.androidLibrary)
     alias(libs.plugins.cityzouitel.androidCompose)
+    alias(libs.plugins.cityzouitel.androidNavigation)
 }
 
 android {
