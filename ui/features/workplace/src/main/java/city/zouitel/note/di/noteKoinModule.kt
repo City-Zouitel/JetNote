44fff1b436f2736f2665be0045a48b2d91ea9222
yon <bourntivoli@gmail.com>
2024-10-19 11:07:21 +0100

    Refactor: Remove image directory dependency from DataScreenModel
    Removed the dependency on the image directory (IMG_DIR) from the DataScreenModel constructor. This
     change likely simplifies the model and removes a hardcoded dependency.

--- ui/features/workplace/src/main/java/city/zouitel/note/di/noteKoinModule.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/di/noteKoinModule.kt
@@ -3,9 +3,7 @@ package city.zouitel.note.di
 import city.zouitel.note.mapper.DataMapper
 import city.zouitel.note.ui.DataScreenModel
 import city.zouitel.note.ui.workplace.WorkplaceScreenModel
-import city.zouitel.systemDesign.CommonConstants.IMG_DIR
 import org.koin.core.module.dsl.factoryOf
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val noteKoinModule = module {
@@ -13,6 +11,6 @@ val noteKoinModule = module {
     factoryOf(::DataMapper)
     factoryOf(::WorkplaceScreenModel)
     factory {
-        DataScreenModel(get(), get(), get(), get(), get(), get(named(IMG_DIR)))
+        DataScreenModel(get(), get(), get(), get(), get())
     }
 }
