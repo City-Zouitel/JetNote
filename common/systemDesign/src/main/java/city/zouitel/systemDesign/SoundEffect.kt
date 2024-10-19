4a929a3ad5008aefc6868203491ce7040afaaaf7
yon <bourntivoli@gmail.com>
2024-10-19 11:09:19 +0100

    Refactor: Simplify SoundEffect class
    Refactored the `SoundEffect` class by replacing the `makeSound` property with a more straightforward `performSound
    Effect` function.
    
    This change improves readability and reduces complexity by directly handling the sound effect logic within the function.

--- common/systemDesign/src/main/java/city/zouitel/systemDesign/SoundEffect.kt
+++ common/systemDesign/src/main/java/city/zouitel/systemDesign/SoundEffect.kt
@@ -5,10 +5,8 @@ import android.media.AudioManager
 
 class SoundEffect {
 
-    val makeSound: (ctx: Context, effectType: Int, thereIsSoundEffect: Boolean) -> Unit
-        get() = { ctx, type, sound ->
-            val sysServ = ctx.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-
-            if (sound) sysServ.playSoundEffect(type, 1.0f)
-        }
+    fun performSoundEffect(context: Context, effectType: Int, isMuted: Boolean) {
+        val sysServ = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        if (isMuted.not()) sysServ.playSoundEffect(effectType, 1.0f)
+    }
 }
