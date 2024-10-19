53f3602a6a513accfff99a25684e757a88922cd5
yon <bourntivoli@gmail.com>
2024-10-19 11:07:58 +0100

    Refactor: Remove image saving logic from DataScreenModel
    Removed the `saveImageLocally` function and related dependencies from the `DataScreenModel`. This function
     was responsible for saving images locally, which is now handled elsewhere in the application. This change simplifies the `DataScreenModel` and improves its focus on data management.

--- ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
@@ -1,32 +1,19 @@
 package city.zouitel.note.ui
 
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
-import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
-import androidx.compose.runtime.MutableState
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.DataUseCase
 import city.zouitel.note.mapper.DataMapper
-import city.zouitel.systemDesign.CommonConstants.JPEG
-import city.zouitel.note.model.Data as InData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.*
+import city.zouitel.note.model.Data as InData
 
 class DataScreenModel(
     private val add: DataUseCase.AddData,
     private val edit: DataUseCase.EditData,
     private val delete: DataUseCase.DeleteData,
     private val eraseTrash: DataUseCase.DeleteAllTrashedData,
-    private val mapper: DataMapper,
-    private val imgPath: String
+    private val mapper: DataMapper
 ): ScreenModel {
 
     // for add a dataEntity from NoteEntityState as it to DataEntity class.
@@ -56,14 +43,5 @@ class DataScreenModel(
         screenModelScope.launch(Dispatchers.IO) {
             eraseTrash.invoke()
         }
-    }
-
-    fun saveImageLocally(img:Bitmap?, path:String, name:String?) {
-            FileOutputStream(
-                name?.let { File(imgPath, it) }
-            ).use {
-                img?.compress(Bitmap.CompressFormat.JPEG, 100, it)
-                it.flush()
-            }
     }
 }
