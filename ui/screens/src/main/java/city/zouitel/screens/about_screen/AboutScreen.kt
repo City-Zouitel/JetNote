c7cea438f9fb97d485af746f7e9c7d569fafaf24
yon <bourntivoli@gmail.com>
2024-10-19 11:05:39 +0100

    Refactor: Use isMute instead of isSound in AboutScreen
    Updated AboutScreen to use `isMute` from DataStoreScreenModel instead of `is
    Sound`.
    Also, replaced `sound.makeSound` with `sound.performSoundEffect` and passed the `isMute` state to it.

--- ui/screens/src/main/java/city/zouitel/screens/about_screen/AboutScreen.kt
+++ ui/screens/src/main/java/city/zouitel/screens/about_screen/AboutScreen.kt
@@ -1,13 +1,33 @@
 package city.zouitel.screens.about_screen
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.rememberScaffoldState
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -153,8 +173,8 @@ class AboutScreen: Screen {
     private fun ProjectSources(dataStoreModel: DataStoreScreenModel) {
 
         val uri = LocalUriHandler.current
-        val ctx = LocalContext.current
-        val thereIsSoundEffect = remember(dataStoreModel, dataStoreModel::isSound).collectAsState()
+        val context = LocalContext.current
+        val isMute by remember(dataStoreModel, dataStoreModel::isMute).collectAsState()
 
         Opensource(
             text = "Project Source",
@@ -165,8 +185,8 @@ class AboutScreen: Screen {
                 )
             }
         ) {
+            sound.performSoundEffect(context, CommonConstants.KEY_CLICK, isMute)
             uri.openUri("https://github.com/City-Zouitel/JetNote")
-            sound.makeSound(ctx, CommonConstants.KEY_CLICK,thereIsSoundEffect.value)
         }
     }
 
