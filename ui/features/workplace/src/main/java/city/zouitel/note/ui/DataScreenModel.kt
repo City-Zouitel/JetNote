584b9cab9c3d648a3b29104307e84985bd52439b
yon <bourntivoli@gmail.com>
2024-11-01 16:56:17 +0100

    Refactor: Update DataScreenModel to handle UI events
    Updated `DataScreenModel` to implement `UiEventHandler` and handle UI events for data operations:
    
    - Added `sendUiEvent` to process `UiEvent`s for delete, insert, update, and delete all operations.
    - Added `performUiEvent` to execute data operations in a coroutine scope.
    - Modified existing methods (`add`, `edit`, `delete`, `eraseTrash`) to
     utilize the `performUiEvent` function.
    
    This change centralizes UI event handling within the `DataScreenModel` and improves code organization.

--- ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
@@ -3,7 +3,10 @@ package city.zouitel.note.ui
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.DataUseCase
+import city.zouitel.logic.events.UiEvent
+import city.zouitel.logic.events.UiEventHandler
 import city.zouitel.note.mapper.DataMapper
+import city.zouitel.note.model.Data
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import city.zouitel.note.model.Data as InData
@@ -14,7 +17,7 @@ class DataScreenModel(
     private val delete: DataUseCase.DeleteData,
     private val eraseTrash: DataUseCase.DeleteAllTrashedData,
     private val mapper: DataMapper
-): ScreenModel {
+): ScreenModel, UiEventHandler<Data> {
 
     // for add a dataEntity from NoteEntityState as it to DataEntity class.
     fun addData(data: InData) {
@@ -42,6 +45,21 @@ class DataScreenModel(
     fun eraseNotes() {
         screenModelScope.launch(Dispatchers.IO) {
             eraseTrash.invoke()
+        }
+    }
+
+    override fun sendUiEvent(event: UiEvent<Data>) {
+        when(event) {
+            is UiEvent.Delete -> performUiEvent { delete(mapper.toDomain(event.data)) }
+            is UiEvent.Insert -> performUiEvent { add(mapper.toDomain(event.data)) }
+            is UiEvent.Update -> performUiEvent { edit(mapper.toDomain(event.data)) }
+            is UiEvent.DeleteAll -> performUiEvent { eraseTrash() }
+        }
+    }
+
+    override fun performUiEvent(action: suspend () -> Unit) {
+        screenModelScope.launch(Dispatchers.IO) {
+            action.invoke()
         }
     }
 }
