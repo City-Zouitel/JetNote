ac978e65d1a7f613107bd5c1fa2165d833c3aa0a
yon <bourntivoli@gmail.com>
2024-11-02 14:14:26 +0100

    Refactor: Simplify UiEventHandler in DataScreenModel
    Removed redundant data manipulation functions (add, edit, delete, eraseNotes) from `DataScreenModel`
     as they were already handled by the `UiEventHandler` implementation.
    
    Simplified `performUiEvent` by directly invoking the action within the coroutine scope. This change improves code readability and removes unnecessary duplication.

--- ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
@@ -9,7 +9,6 @@ import city.zouitel.note.mapper.DataMapper
 import city.zouitel.note.model.Data
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import city.zouitel.note.model.Data as InData
 
 class DataScreenModel(
     private val add: DataUseCase.AddData,
@@ -18,35 +17,6 @@ class DataScreenModel(
     private val eraseTrash: DataUseCase.DeleteAllTrashedData,
     private val mapper: DataMapper
 ): ScreenModel, UiEventHandler<Data> {
-
-    // for add a dataEntity from NoteEntityState as it to DataEntity class.
-    fun addData(data: InData) {
-        screenModelScope.launch(Dispatchers.IO) {
-            add.invoke(mapper.toDomain(data))
-        }
-    }
-
-    // for editData a dataEntity from NoteEntityState and put it to DataEntity class,
-    // depending on changes.
-    fun editData(data: InData){
-        screenModelScope.launch(Dispatchers.IO) {
-            edit.invoke(mapper.toDomain(data))
-        }
-    }
-
-    // for deleting a dataEntity by the id.
-    fun deleteData(data: InData){
-        screenModelScope.launch(Dispatchers.IO) {
-            delete.invoke(mapper.toDomain(data))
-        }
-    }
-
-    //delete all removed notes.
-    fun eraseNotes() {
-        screenModelScope.launch(Dispatchers.IO) {
-            eraseTrash.invoke()
-        }
-    }
 
     override fun sendUiEvent(event: UiEvent<Data>) {
         when(event) {
@@ -58,8 +28,6 @@ class DataScreenModel(
     }
 
     override fun performUiEvent(action: suspend () -> Unit) {
-        screenModelScope.launch(Dispatchers.IO) {
-            action.invoke()
-        }
+        screenModelScope.launch(Dispatchers.IO) { action.invoke() }
     }
 }
