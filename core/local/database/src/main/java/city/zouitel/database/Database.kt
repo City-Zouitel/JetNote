e7ef38fd09e8f2587cb311539179bbaad214de92
yon <bourntivoli@gmail.com>
2024-11-09 22:42:32 +0100

    feat: Add ReminderEntity and NoteAndReminderEntity to Database
    Added `ReminderEntity` and `NoteAndReminderEntity` to the database entities list. This
     change enables storing reminders and their relationships with notes in the local database.
    
    - Included `ReminderEntity` and `NoteAndReminderEntity` in the `entities` array of the `Database` class.
    - Added corresponding DAO access methods: `getReminderDao()` and `getNoteAndReminderDao()`.

--- core/local/database/src/main/java/city/zouitel/database/Database.kt
+++ core/local/database/src/main/java/city/zouitel/database/Database.kt
@@ -2,8 +2,34 @@ package city.zouitel.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import city.zouitel.database.dao.*
-import city.zouitel.database.model.*
+import city.zouitel.database.dao.AudioDao
+import city.zouitel.database.dao.DataDao
+import city.zouitel.database.dao.LinkDao
+import city.zouitel.database.dao.MediaDao
+import city.zouitel.database.dao.NoteAndAudioDao
+import city.zouitel.database.dao.NoteAndLinkDao
+import city.zouitel.database.dao.NoteAndMediaDao
+import city.zouitel.database.dao.NoteAndReminderDao
+import city.zouitel.database.dao.NoteAndTagDao
+import city.zouitel.database.dao.NoteAndTaskDao
+import city.zouitel.database.dao.NoteDao
+import city.zouitel.database.dao.ReminderDao
+import city.zouitel.database.dao.TagDao
+import city.zouitel.database.dao.TaskDao
+import city.zouitel.database.dao.WidgetDao
+import city.zouitel.database.model.AudioEntity
+import city.zouitel.database.model.DataEntity
+import city.zouitel.database.model.LinkEntity
+import city.zouitel.database.model.MediaEntity
+import city.zouitel.database.model.NoteAndAudioEntity
+import city.zouitel.database.model.NoteAndLinkEntity
+import city.zouitel.database.model.NoteAndMediaEntity
+import city.zouitel.database.model.NoteAndReminderEntity
+import city.zouitel.database.model.NoteAndTagEntity
+import city.zouitel.database.model.NoteAndTaskEntity
+import city.zouitel.database.model.ReminderEntity
+import city.zouitel.database.model.TagEntity
+import city.zouitel.database.model.TaskEntity
 
 @Database(
     version = 1,
@@ -19,7 +45,9 @@ import city.zouitel.database.model.*
         AudioEntity::class,
         NoteAndAudioEntity::class,
         MediaEntity::class,
-        NoteAndMediaEntity::class
+        NoteAndMediaEntity::class,
+        ReminderEntity::class,
+        NoteAndReminderEntity::class,
     ],
     exportSchema = true
 )
@@ -37,4 +65,6 @@ abstract class Database:RoomDatabase() {
     abstract fun getNoteAndAudioDao(): NoteAndAudioDao
     abstract fun getMediaDao(): MediaDao
     abstract fun getNoteAndMediaDao(): NoteAndMediaDao
+    abstract fun getReminderDao(): ReminderDao
+    abstract fun getNoteAndReminderDao(): NoteAndReminderDao
 }
