90f0c8a5e87fc107c03bd470b7972eba28357d38
yon <bourntivoli@gmail.com>
2024-11-13 13:54:34 +0100

    Style: Refactor
    Refactor: Apply consistent formatting to UiEvent data classes
    
    Applied consistent formatting to the `Insert`, `Update`, `Delete`, and
     `DeleteAll` data classes within the `UiEvent` sealed class.
    
    This change improves code readability and maintainability by ensuring consistent code style.

--- common/logic/src/main/java/city/zouitel/logic/events/UiEvent.kt
+++ common/logic/src/main/java/city/zouitel/logic/events/UiEvent.kt
@@ -1,8 +1,8 @@
 package city.zouitel.logic.events
 
 sealed class UiEvent<out T> {
-    data class Insert<out T> (val data: T): UiEvent<T>()
-    data class Update<out T> (val data: T): UiEvent<T>()
-    data class Delete<out T> (val data: T): UiEvent<T>()
-    class DeleteAll<out T>: UiEvent<T>()
+    data class Insert<out T>(val data: T) : UiEvent<T>()
+    data class Update<out T>(val data: T) : UiEvent<T>()
+    data class Delete<out T>(val data: T) : UiEvent<T>()
+    class DeleteAll<out T> : UiEvent<T>()
 }
