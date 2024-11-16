8cb2141782a7e31a3016dcf3a91fe3207742a4cc
yon <bourntivoli@gmail.com>
2024-11-16 18:49:39 +0100

    feat: Introduce Reminder data class
    Added a new data class `Reminder` in the `notifications` module to represent a reminder.
    
    The `Reminder` class
     includes the following properties:
    - `id`: An integer representing the reminder's ID.
    - `uid`: A string representing the user ID associated with the reminder.
    - `atTime`: A long representing the time the reminder is scheduled for.
    - `isPassed`: A boolean indicating whether the reminder
     has already passed.

---
+++ services/notifications/src/main/java/city/zouitel/notifications/model/Reminder.kt
@@ -0,0 +1,11 @@
+package city.zouitel.notifications.model
+
+import androidx.annotation.Keep
+
+@Keep
+data class Reminder(
+    val id: Int = 0,
+    val uid: String = "",
+    val atTime: Long = 0L,
+    val isPassed: Boolean = false
+)
