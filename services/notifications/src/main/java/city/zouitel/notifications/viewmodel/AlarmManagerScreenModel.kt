9cfd657529fbfd63a0b8d9e8bddc784176cab1ce
yon <bourntivoli@gmail.com>
2024-11-17 14:28:42 +0100

    Refactor: Improve Alarm Scheduling and Data Passing in AlarmManagerScreenModel
    - Replaced hardcoded intent extras with constants for better code readability and maintainability.
    
    - Updated alarm scheduling logic to use `setExactAndAllowWhileIdle` if available, improving alarm accuracy.
    - Updated intent creation and PendingIntent retrieval to use unique request codes for individual alarms, preventing conflicts.

--- services/notifications/src/main/java/city/zouitel/notifications/viewmodel/AlarmManagerScreenModel.kt
+++ services/notifications/src/main/java/city/zouitel/notifications/viewmodel/AlarmManagerScreenModel.kt
@@ -1,33 +1,50 @@
 package city.zouitel.notifications.viewmodel
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import cafe.adriel.voyager.core.model.ScreenModel
+import city.zouitel.notifications.Constants.ID
+import city.zouitel.notifications.Constants.MESSAGE
+import city.zouitel.notifications.Constants.TITLE
+import city.zouitel.notifications.Constants.UID
 import city.zouitel.notifications.reciver.NotificationReceiver
 
 class AlarmManagerScreenModel(private val context: Context): ScreenModel {
-    val intent = Intent(context , NotificationReceiver::class.java)
+    private val intent = Intent(context , NotificationReceiver::class.java)
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
     private val basicPendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
+    @SuppressLint("NewApi")
     fun initializeAlarm(title: String, message: String, uid: String, id: Int, atTime: Long) {
-        intent.putExtra("note_title", title)
-        intent.putExtra("note_description", message)
-        intent.putExtra("uid", uid)
-        intent.putExtra("id", id)
+        intent.putExtra(TITLE, title)
+        intent.putExtra(MESSAGE, message)
+        intent.putExtra(UID, uid)
+        intent.putExtra(ID, id)
 
         scheduleAlarm(atTime, id)
     }
 
+    @SuppressLint("NewApi")
     private fun scheduleAlarm(atTime: Long, id: Int) {
         val pendingIntent = PendingIntent.getBroadcast(context, id, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
-        alarmManager.setAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            atTime,
-            pendingIntent ?: basicPendingIntent
-        )
+        val isScheduleExactAlarms = alarmManager.canScheduleExactAlarms()
+
+        if (isScheduleExactAlarms) {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                atTime,
+                pendingIntent ?: basicPendingIntent
+            )
+        } else {
+            alarmManager.setAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                atTime,
+                pendingIntent ?: basicPendingIntent
+            )
+        }
     }
 
     fun cancelAlarm(id: Int) {
