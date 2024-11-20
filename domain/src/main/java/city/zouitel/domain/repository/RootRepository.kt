62b0ebee22eb3b1aea1cc7d7b234cd28483b4586
yon <bourntivoli@gmail.com>
2024-11-20 18:56:11 +0100

    Refactor: Remove redundant blank line in RootRepository
    Removed a redundant blank line at the end of the `RootRepository` interface. This change improves code readability
     and removes unnecessary whitespace.

--- domain/src/main/java/city/zouitel/domain/repository/RootRepository.kt
+++ domain/src/main/java/city/zouitel/domain/repository/RootRepository.kt
@@ -1,10 +1,9 @@
 package city.zouitel.domain.repository
 
-import city.zouitel.domain.model.Root as OutRoot
 import kotlinx.coroutines.flow.Flow
+import city.zouitel.domain.model.Root as OutRoot
 
 interface RootRepository {
 
     val isDeviceRooted: Flow<Result<OutRoot>>
-
 }
