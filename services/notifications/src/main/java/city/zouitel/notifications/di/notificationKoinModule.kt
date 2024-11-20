04b2574fe62e9ed00b2c44c76f58eea1d9ebc97b
yon <bourntivoli@gmail.com>
2024-11-20 18:55:38 +0100

    Refactor: Move Notification Setup to NotificationHelper
    Moved the setup of `NotificationCompat.Builder`, `NotificationManagerCompat`, and `NotificationChannel` from the
     Koin module to the `NotificationHelper` class.
    
    This change encapsulates notification-related logic within the `NotificationHelper` for better organization and maintainability.

--- services/notifications/src/main/java/city/zouitel/notifications/di/notificationKoinModule.kt
+++ services/notifications/src/main/java/city/zouitel/notifications/di/notificationKoinModule.kt
@@ -14,24 +14,4 @@ import org.koin.dsl.module
 val notificationKoinModule = module {
     factoryOf(::AlarmManagerScreenModel)
 
-    single {
-        NotificationCompat.Builder(androidContext(), Constants.CHANNEL_ID)
-            .setDefaults(android.app.Notification.DEFAULT_ALL)
-            .setSmallIcon(android.R.drawable.ic_popup_reminder)
-            .setColor(Color.YELLOW)
-            .setLights(Color.WHITE, 300, 100)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-    }
-
-    single {
-        NotificationManagerCompat.from(androidContext())
-    }
-
-    single {
-        NotificationChannel(
-            Constants.CHANNEL_ID,
-            Constants.CHANNEL_NAME,
-            NotificationManager.IMPORTANCE_DEFAULT
-        )
-    }
 }
