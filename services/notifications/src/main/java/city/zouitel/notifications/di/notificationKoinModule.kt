5b9738d85cb60ee47144d04e369b5cad8b4309b6
yon <bourntivoli@gmail.com>
2024-11-22 21:11:49 +0100

    Refactor: Remove unused Notification dependencies in notificationKoinModule
    Removed unused dependencies related to Notifications (NotificationChannel, NotificationManager, NotificationCompat, NotificationManagerCompat
    ) and Android Context from the `notificationKoinModule`.
    
    This change removes unnecessary imports and likely simplifies the module.

--- services/notifications/src/main/java/city/zouitel/notifications/di/notificationKoinModule.kt
+++ services/notifications/src/main/java/city/zouitel/notifications/di/notificationKoinModule.kt
@@ -1,17 +1,10 @@
 package city.zouitel.notifications.di
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.graphics.Color
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import city.zouitel.notifications.Constants
 import city.zouitel.notifications.viewmodel.AlarmManagerScreenModel
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val notificationKoinModule = module {
-    factoryOf(::AlarmManagerScreenModel)
 
+    factoryOf(::AlarmManagerScreenModel)
 }
