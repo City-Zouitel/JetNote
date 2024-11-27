189dc4e69b5f164c40a0370096fa4aa1e4e749e3
yon <bourntivoli@gmail.com>
2024-11-27 21:14:24 +0100

    Refactor: Remove unused `getImgPath` function
    Removed the `getImgPath` function from the `utils.kt` file as it was no
     longer used in the application. This change likely removes redundant code and simplifies the utility functions.

--- common/logic/src/main/java/city/zouitel/logic/utils.kt
+++ common/logic/src/main/java/city/zouitel/logic/utils.kt
@@ -1,13 +1,7 @@
 package city.zouitel.logic
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 
 infix fun String.getRecPath(context: Context): String {
     return context.filesDir.path + "/records-directory/" + this + "." + "mp3"
-}
-
-infix fun String.getImgPath(context: Context): String {
-    return context.filesDir.path + "/images-directory/" + this + "." + "jpeg"
 }
