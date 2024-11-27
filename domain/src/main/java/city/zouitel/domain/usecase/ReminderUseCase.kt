955ad29a411c593ee000745b075b5398b7ea2e54
yon <bourntivoli@gmail.com>
2024-11-27 21:43:14 +0100

    Refactor: Update ReminderUseCase to use ReminderDatasource
    Updated `ReminderUseCase` to use `ReminderDatasource` instead of `ReminderRepo`.
    
    This change likely
     improves dependency management and data access consistency.

--- domain/src/main/java/city/zouitel/domain/usecase/ReminderUseCase.kt
+++ domain/src/main/java/city/zouitel/domain/usecase/ReminderUseCase.kt
@@ -1,27 +1,27 @@
 package city.zouitel.domain.usecase
 
 import city.zouitel.domain.model.Reminder
-import city.zouitel.domain.repository.ReminderRepo
+import city.zouitel.domain.repository.ReminderDatasource
 
 sealed class ReminderUseCase {
 
-    data class ObserveById(val repository: ReminderRepo): ReminderUseCase() {
+    data class ObserveById(val repository: ReminderDatasource): ReminderUseCase() {
         suspend operator fun invoke(uid: String) = repository.observeById(uid)
     }
 
-    data class Insert(val repository: ReminderRepo): ReminderUseCase() {
+    data class Insert(val repository: ReminderDatasource): ReminderUseCase() {
         suspend operator fun invoke(reminder: Reminder) = repository.insert(reminder)
     }
 
-    data class Update(val repository: ReminderRepo): ReminderUseCase() {
+    data class Update(val repository: ReminderDatasource): ReminderUseCase() {
         suspend operator fun invoke(reminder: Reminder) = repository.update(reminder)
     }
 
-    data class DeleteReminder(val repository: ReminderRepo): ReminderUseCase() {
+    data class DeleteReminder(val repository: ReminderDatasource): ReminderUseCase() {
         suspend operator fun invoke(id: Long) = repository.delete(id)
     }
 
-    data class DeleteAll(val repository: ReminderRepo): ReminderUseCase() {
+    data class DeleteAll(val repository: ReminderDatasource): ReminderUseCase() {
         suspend operator fun invoke() = repository.deleteAll()
     }
 }
