b0b4c7796daf5fa04f6950ae3ba56cd791246f99
yon <bourntivoli@gmail.com>
2024-11-27 21:32:58 +0100

    feat: Add Generative AI Module
    A new module `generativeai` has been created under `core/network`.
    
    This module provides Generative AI
     functionality by integrating with:
    
    -   Gemini library.
    -   DataStore library.
    -   `core.network.repository` module.

---
+++ core/network/generativeai/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.cityzouitel.androidLibrary)
+}
+
+android {
+    namespace = "city.zouitel.generativeai"
+}
+
+dependencies {
+    //Modules.
+    implementation(projects.core.network.repository)
+
+    //Gemini.
+    implementation(libs.generativeai)
+
+    //DataStore.
+    implementation (libs.datastore)
+}
