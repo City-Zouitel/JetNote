53091434fc7be94ffbbeaa6de13bfe91b7653045
yon <bourntivoli@gmail.com>
2024-11-27 21:34:36 +0100

    Refactor: Introduce Context Extensions and Logic Flow Utility
    This commit introduces several helper functions as extensions for `Context` and `ScreenModel`.
    
    - Added `
    makeSoundFor` context extension to play sound effects based on a Boolean condition.
    - Added `asShortToast` and `asLongToast` context extensions for displaying string as Toast messages with different durations.
    - Added `asLogicFlow` screenModel extension to convert a `Flow` into a `StateFlow
    ` with a 5-second subscription timeout using `stateIn`. This utility is designed to be used within ScreenModels.

--- common/logic/src/main/java/city/zouitel/logic/contexts.kt
+++ common/logic/src/main/java/city/zouitel/logic/contexts.kt
@@ -5,6 +5,12 @@ package city.zouitel.logic
 import android.content.Context
 import android.media.AudioManager
 import android.widget.Toast
+import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 context (Context)
 infix fun Boolean.makeSoundFor(audioManager: Int) {
@@ -13,12 +19,47 @@ infix fun Boolean.makeSoundFor(audioManager: Int) {
     if (this) systemService.playSoundEffect(audioManager, 1.0f)
 }
 
+/**
+ * Displays this string as a short toast message.
+ *
+ * This is an extension function on String, allowing you to easily display
+ * the string as a toast message with a short duration.
+ *
+ * @receiver The string to be displayed as the toast message.
+ */
 context (Context)
 fun String.asShortToast() {
     Toast.makeText(this@Context, this, Toast.LENGTH_SHORT).show()
 }
 
+/**
+ * Displays this string as a long toast message.
+ *
+ * This is an extension function on String, allowing you to easily display
+ * the string as a toast with a long duration.
+ *
+ * @receiver The string to be displayed in the toast.
+ */
 context (Context)
 fun String.asLongToast()  {
     Toast.makeText(this@Context, this, Toast.LENGTH_LONG).show()
+}
+
+/**
+ * Converts this [Flow] to a [StateFlow] using [stateIn] operator.
+ *
+ * This extension function is designed for use within a ScreenModel (or similar architecture)
+ * and automatically uses the `screenModelScope` for collection. It also configures the
+ * [stateIn] operator to keep the state for 5 seconds after the last subscriber is gone.
+ *
+ * @param initialValue The initial value of the state flow.
+ * @return A [StateFlow] representing the latest value emitted by this flow.
+ */
+context(ScreenModel)
+fun <T> Flow<T>.asLogicFlow(initialValue: T): StateFlow<T> {
+    return stateIn(
+        scope = screenModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = initialValue
+    )
 }
