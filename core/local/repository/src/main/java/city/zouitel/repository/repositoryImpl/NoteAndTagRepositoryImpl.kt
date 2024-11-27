a54fa6ed618ddd7f3e8a39ddc114260a084b8591
yon <bourntivoli@gmail.com>
2024-11-27 21:44:47 +0100

    Refactor: Remove unused import in NoteAndTagRepositoryImpl
    Removed the unused import of `city.zouitel.repository.model.NoteAndTag`
     which was aliased as `InNoteAndTag`. This likely improves code cleanliness and reduces unnecessary imports.

--- core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/NoteAndTagRepositoryImpl.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/NoteAndTagRepositoryImpl.kt
@@ -4,7 +4,6 @@ import city.zouitel.domain.model.NoteAndTag as OutNoteAndTag
 import city.zouitel.domain.repository.NoteAndTagRepository
 import city.zouitel.repository.datasource.NoteAndTagDataSource
 import city.zouitel.repository.mapper.NoteAndTagMapper
-import city.zouitel.repository.model.NoteAndTag as InNoteAndTag
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
