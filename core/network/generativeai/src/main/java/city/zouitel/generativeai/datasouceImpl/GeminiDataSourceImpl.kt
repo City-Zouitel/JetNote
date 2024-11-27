6f2772891e629a4a541f057fb778cfb677efe6ba
yon <bourntivoli@gmail.com>
2024-11-27 21:35:01 +0100

    feat: Implement GeminiDataSourceImpl
    Implemented `GeminiDataSourceImpl` to handle sending prompts to Gemini and processing responses.
    
    - Sends prompts using the `Gener
    ativeModel` and maps the request using `GeminiMapper`.
    - Processes the response, emitting the text content or error messages.
    - Maps the response back to the repository model using `GeminiMapper`.

---
+++ core/network/generativeai/src/main/java/city/zouitel/generativeai/datasouceImpl/GeminiDataSourceImpl.kt
@@ -0,0 +1,38 @@
+package city.zouitel.generativeai.datasouceImpl
+
+import city.zouitel.generativeai.mapper.GeminiMapper
+import city.zouitel.repository.datasource.GeminiDataSource
+import city.zouitel.repository.model.GeminiQuest
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GeminiDataSourceImpl(
+    private val generativeModel: GenerativeModel,
+    private val mapper: GeminiMapper
+): GeminiDataSource {
+
+    override suspend fun sendGeminiPrompt(geminiQuest: GeminiQuest): Flow<String> {
+        var response = flow { emit("...") }
+        runCatching {
+            generativeModel.generateContent(
+                content {
+                    mapper.fromRepo(geminiQuest).image?.let { image(it) }
+                    text(mapper.fromRepo(geminiQuest).prompt)
+                }
+            ).run {
+                text?.let {
+                    response = flow { emit(it) }
+                } ?: run {
+                    response = flow { emit("null") }
+                }
+                println(this.promptFeedback)
+                println(this.usageMetadata)
+            }
+        }.onFailure {
+            response = flow { emit(it.message.toString()) }
+        }
+        return mapper.toRepo(response)
+    }
+}
