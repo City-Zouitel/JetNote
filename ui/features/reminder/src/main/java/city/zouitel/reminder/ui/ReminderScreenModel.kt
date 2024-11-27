9e8efe6fdf6dab4b204e8c5f32582cc452d47be6
yon <bourntivoli@gmail.com>
2024-11-27 21:40:10 +0100

    Refactor: Simplify stateIn in ReminderScreenModel
    Simplified the `stateIn` operator usage in `ReminderScreenModel` by using the `asLogicFlow
    ` extension function.
    
    This removes the explicit `stateIn` call and replaces it with a more concise and reusable approach.

--- ui/features/reminder/src/main/java/city/zouitel/reminder/ui/ReminderScreenModel.kt
+++ ui/features/reminder/src/main/java/city/zouitel/reminder/ui/ReminderScreenModel.kt
@@ -3,15 +3,14 @@ package city.zouitel.reminder.ui
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.ReminderUseCase
+import city.zouitel.logic.asLogicFlow
 import city.zouitel.logic.events.UiEvent
 import city.zouitel.logic.events.UiEventHandler
 import city.zouitel.reminder.mapper.ReminderMapper
 import city.zouitel.reminder.model.Reminder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class ReminderScreenModel(
@@ -24,12 +23,7 @@ class ReminderScreenModel(
 ): ScreenModel, UiEventHandler<Reminder> {
 
     private val _observeAllById = MutableStateFlow<List<Reminder>>(emptyList())
-    val observeAllById: StateFlow<List<Reminder>> = _observeAllById
-        .stateIn(
-            screenModelScope,
-            SharingStarted.WhileSubscribed(),
-            listOf()
-        )
+    val observeAllById: StateFlow<List<Reminder>> = _observeAllById.asLogicFlow(emptyList())
 
     fun observeRemindersByUid(uid: String) {
         performUiEvent {
