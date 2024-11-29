6222fe0d17a5f19a5c6793a038e245877c67687a
yon <bourntivoli@gmail.com>
2024-11-29 20:48:20 +0100

    feat: Implement sendPrompt function in AssistantScreenModel
    Implemented the `sendPrompt` function in `AssistantScreenModel` to send a Gemini prompt and update the
     `getGenerativeResponse` state with the result.
    
    - Uses `screenModelScope` and `Dispatchers.IO` for coroutine management.
    - Leverages `AssistantMapper` to convert `GeminiQuest` to a domain object.
    - Collects the response from `sendGeminiPrompt` and updates
     the `_getGenerativeResponse` state flow.

--- ui/features/assistant/src/main/java/city/zouitel/assistant/ui/AssistantScreenModel.kt
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/ui/AssistantScreenModel.kt
@@ -1,13 +1,29 @@
 package city.zouitel.assistant.ui
 
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import city.zouitel.assistant.mapper.AssistantMapper
+import city.zouitel.assistant.model.GeminiQuest
 import city.zouitel.domain.usecase.GeminiUseCase
+import city.zouitel.logic.asLogicFlow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class AssistantScreenModel(
     private val sendGeminiPrompt: GeminiUseCase.SendGeminiPrompt,
+    private val mapper: AssistantMapper
 ): ScreenModel {
 
-    fun sendPrompt() {
+    private val _getGenerativeResponse = MutableStateFlow("")
+    val getGenerativeResponse: StateFlow<String> = _getGenerativeResponse.asLogicFlow("")
 
+    fun sendPrompt(geminiQuest: GeminiQuest) {
+        screenModelScope.launch(Dispatchers.IO) {
+            sendGeminiPrompt(mapper.toDomain(geminiQuest)).collect {
+                _getGenerativeResponse.value = it
+            }
+        }
     }
 }
