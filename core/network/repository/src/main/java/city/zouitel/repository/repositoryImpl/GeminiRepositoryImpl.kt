e8aef87f930a841483357523c18e959f13ecd435
yon <bourntivoli@gmail.com>
2024-11-29 20:49:20 +0100

    Refactor: Remove unused flow and update sendGeminiPrompt
    Removed the unused `flow` and updated the `sendGeminiPrompt` function to directly return the result
     from `dataSource.sendGeminiPrompt`.
    
    This change simplifies the code and removes unnecessary intermediate steps.

--- core/network/repository/src/main/java/city/zouitel/repository/repositoryImpl/GeminiRepositoryImpl.kt
+++ core/network/repository/src/main/java/city/zouitel/repository/repositoryImpl/GeminiRepositoryImpl.kt
@@ -5,7 +5,6 @@ import city.zouitel.domain.repository.GeminiRepository
 import city.zouitel.repository.datasource.GeminiDataSource
 import city.zouitel.repository.mapper.GeminiMapper
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 class GeminiRepositoryImpl(
     private val dataSource: GeminiDataSource,
@@ -13,8 +12,6 @@ class GeminiRepositoryImpl(
 ): GeminiRepository {
 
     override suspend fun sendGeminiPrompt(geminiQuest: GeminiQuest): Flow<String> {
-        val response = flow { emit("...") }
-        dataSource.sendGeminiPrompt(mapper.fromDomain(geminiQuest))
-        return mapper.toDomain(response)
+        return dataSource.sendGeminiPrompt(mapper.fromDomain(geminiQuest))
     }
 }
