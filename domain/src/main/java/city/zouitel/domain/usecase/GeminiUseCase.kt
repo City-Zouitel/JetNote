8e62fc182404c9ba05f712633f04a48741253ef0
yon <bourntivoli@gmail.com>
2024-12-02 18:28:17 +0100

    Refactor: Wrap GeminiUseCase response in RequestState
    Modified `SendGeminiPrompt` use case to wrap its response in a `RequestState` object.
    
    This change likely improves error handling and provides a more structured way to represent the result of the Gemini prompt request. It changes the return type of `invoke` from `Flow<String>` to `Flow<RequestState<String>>`.

--- domain/src/main/java/city/zouitel/domain/usecase/GeminiUseCase.kt
+++ domain/src/main/java/city/zouitel/domain/usecase/GeminiUseCase.kt
@@ -2,12 +2,14 @@ package city.zouitel.domain.usecase
 
 import city.zouitel.domain.model.GeminiQuest
 import city.zouitel.domain.repository.GeminiRepository
+import city.zouitel.domain.utils.RequestState
 import kotlinx.coroutines.flow.Flow
 
 sealed class GeminiUseCase {
-    data class SendGeminiPrompt(
-        private val repository: GeminiRepository
-    ): GeminiUseCase() {
-        suspend operator fun invoke(geminiQuest: GeminiQuest): Flow<String> = repository.sendGeminiPrompt(geminiQuest)
+
+    data class SendGeminiPrompt(private val repository: GeminiRepository): GeminiUseCase() {
+        suspend operator fun invoke(geminiQuest: GeminiQuest): Flow<RequestState<String>> {
+           return repository.sendGeminiPrompt(geminiQuest)
+        }
     }
 }
