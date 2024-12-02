5e3cb2bb2cc7b5ad23d0033f8e1e4e8e7747548a
yon <bourntivoli@gmail.com>
2024-12-02 18:25:45 +0100

    Refactor: Display RequestState in AssistantScreen
    Refactored the `AssistantScreen` to display the current state of the request using the `RequestState`
     sealed class.
    
    - Replaced direct access to the response with a call to `DisplayResult` which handles loading, success, and error states.
    - Removed unnecessary padding and divider elements from the layout.

--- ui/features/assistant/src/main/java/city/zouitel/assistant/ui/AssistantScreen.kt
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/ui/AssistantScreen.kt
@@ -1,9 +1,7 @@
 package city.zouitel.assistant.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -12,29 +10,44 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import city.zouitel.assistant.model.GeminiQuest
+import city.zouitel.domain.utils.RequestState
 
 class AssistantScreen: Screen {
     @Composable
     override fun Content() {
         var textState by remember { mutableStateOf("") }
         val viewModel = getScreenModel<AssistantScreenModel>()
-        val response by remember(viewModel, viewModel::getGenerativeResponse).collectAsState()
+        val response by remember(viewModel, viewModel::getGenerativeResponse).collectAsState(RequestState.Idle)
 
         Column {
             TextField(
                 value = textState,
                 onValueChange = { textState = it }
             )
+
             Button({ viewModel.sendPrompt(GeminiQuest(null, textState)) }) {
                 Text("prompt")
             }
-            HorizontalDivider(modifier = Modifier.padding(20.dp))
-            Text(text = response)
+
+            response.DisplayResult(
+                onLoading = { Text("Loading") },
+                onSuccess = { Text(it) },
+                onError = { Text(it) }
+            )
         }
+//        Column {
+//            TextField(
+//                value = textState,
+//                onValueChange = { textState = it }
+//            )
+//            Button({ viewModel.sendPrompt(GeminiQuest(null, textState)) }) {
+//                Text("prompt")
+//            }
+//            HorizontalDivider(modifier = Modifier.padding(20.dp))
+//            Text(text = response)
+//        }
     }
 }
