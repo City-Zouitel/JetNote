55ca4d1c04053403f513d3c55fa5d7c9a00773e8
yon <bourntivoli@gmail.com>
2024-12-02 18:28:28 +0100

    feat: Integrate Gemini API key from DataStore
    This commit integrates the Gemini API key from DataStore into the `GenerativeModel` initialization.
    
    -
     It fetches the API key from the DataStore using a coroutine and updates the `GenerativeModel` with the retrieved key.
    - `GeminiDataSourceImpl` and `GeminiMapper` are added to the dependency injection.
    - The hardcoded API key is removed.
    
    This change allows for dynamic configuration of
     the Gemini API key and improves security by storing the key in a secure location.

--- core/network/generativeai/src/main/java/city/zouitel/generativeai/di/generativeKoinModule.kt
+++ core/network/generativeai/src/main/java/city/zouitel/generativeai/di/generativeKoinModule.kt
@@ -1,14 +1,51 @@
 package city.zouitel.generativeai.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import city.zouitel.datastore.Keys
+import city.zouitel.generativeai.Constants.GEMINI_MODEL
+import city.zouitel.generativeai.datasouceImpl.GeminiDataSourceImpl
+import city.zouitel.generativeai.mapper.GeminiMapper
+import city.zouitel.repository.datasource.GeminiDataSource
 import com.google.ai.client.generativeai.GenerativeModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val generativeKoinModule = module {
 
-    single {
+    singleOf(::GeminiDataSourceImpl) bind GeminiDataSource::class
+    factoryOf(::GeminiMapper)
+
+    single { param ->
+        var _apiKey: String? = null
+        param.insert(
+            index = 0,
+            value = CoroutineScope(Dispatchers.IO)
+        )
+        param.insert(
+            index = 1,
+            value = get<DataStore<Preferences>>().data
+                .catch { emit(emptyPreferences()) }
+                .map { it[Keys.GEMINI_API_KEY] ?: "" }
+        )
+
+        param.component1<CoroutineScope>().launch {
+            param.component2<Flow<String>>().collect { key ->
+                _apiKey = key
+            }
+        }
         GenerativeModel(
-            modelName = "gemini-1.5-flash-latest",
-            apiKey = "AIzaSyAsJtZ7sHR-gq0pym_w8aPCrn6X0KnxKZY"
+            modelName = GEMINI_MODEL,
+            apiKey = _apiKey ?: ""
         )
     }
 }
