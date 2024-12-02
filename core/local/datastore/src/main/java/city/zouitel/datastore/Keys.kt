bbc73c82b00929361f35beea10b365f43b7a8731
yon <bourntivoli@gmail.com>
2024-12-02 18:28:54 +0100

    Refactor: Use Constants for Datastore Keys
    Updated the Datastore `Keys` object to use constants from the `Constants` object for key definitions.
    
    This improves code readability and maintainability by centralizing the definition of preference keys.

--- core/local/datastore/src/main/java/city/zouitel/datastore/Keys.kt
+++ core/local/datastore/src/main/java/city/zouitel/datastore/Keys.kt
@@ -2,12 +2,20 @@ package city.zouitel.datastore
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
+import city.zouitel.datastore.Constants.GEMINI_API
+import city.zouitel.datastore.Constants.LAYOUT
+import city.zouitel.datastore.Constants.LOCK_MODE
+import city.zouitel.datastore.Constants.MUTE
+import city.zouitel.datastore.Constants.ORDINATION
+import city.zouitel.datastore.Constants.SCREENSHOT_BLOCK
+import city.zouitel.datastore.Constants.THEME
 
 object Keys {
-    val LAYOUT_KEY = stringPreferencesKey(Constants.LAYOUT)
-    val ORDINATION_KEY = stringPreferencesKey(Constants.ORDINATION)
-    val THEME_KEY = stringPreferencesKey(Constants.THEME)
-    val MUTE_KEY = booleanPreferencesKey(Constants.MUTE)
-    val LOCK_MODE_KEY = booleanPreferencesKey(Constants.LOCK_MODE)
-    val SCREENSHOT_BLOCK_KEY = booleanPreferencesKey(Constants.SCREENSHOT_BLOCK  )
+    val LAYOUT_KEY = stringPreferencesKey(LAYOUT)
+    val ORDINATION_KEY = stringPreferencesKey(ORDINATION)
+    val THEME_KEY = stringPreferencesKey(THEME)
+    val MUTE_KEY = booleanPreferencesKey(MUTE)
+    val LOCK_MODE_KEY = booleanPreferencesKey(LOCK_MODE)
+    val SCREENSHOT_BLOCK_KEY = booleanPreferencesKey(SCREENSHOT_BLOCK)
+    val GEMINI_API_KEY = stringPreferencesKey(GEMINI_API)
 }
