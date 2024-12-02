18818c6baedd6a330f974f48d634a66b6dba1c39
yon <bourntivoli@gmail.com>
2024-12-02 18:27:52 +0100

    Refactor: Remove unused function in GeminiMapper
    Removed the unused `toDomain` function from the `GeminiMapper` class. This function was likely a placeholder
     or remnant from previous code and is no longer needed.

--- core/network/repository/src/main/java/city/zouitel/repository/mapper/GeminiMapper.kt
+++ core/network/repository/src/main/java/city/zouitel/repository/mapper/GeminiMapper.kt
@@ -1,12 +1,11 @@
 package city.zouitel.repository.mapper
 
+import city.zouitel.domain.utils.RequestState
 import kotlinx.coroutines.flow.Flow
 import city.zouitel.domain.model.GeminiQuest as OutGeminiQuest
 import city.zouitel.repository.model.GeminiQuest as InGeminiQuest
 
 class GeminiMapper {
-    fun toDomain(response: Flow<String>) = response
-
 
     fun fromDomain(geminiQuest: OutGeminiQuest) = InGeminiQuest(
         image = geminiQuest.image,
