09149fa2d3b59d4047708930bb8d95a0bea27106
yon <bourntivoli@gmail.com>
2024-12-02 18:27:17 +0100

    feat: Wrap Gemini response in RequestState
    Wrapped the response from `sendGeminiPrompt` in `GeminiDataSource` with `RequestState`.
    
    This change allows
     handling different states (loading, success, error) of the Gemini prompt request and provides better error handling and feedback to the user.

--- core/network/repository/src/main/java/city/zouitel/repository/datasource/GeminiDataSource.kt
+++ core/network/repository/src/main/java/city/zouitel/repository/datasource/GeminiDataSource.kt
@@ -1,8 +1,10 @@
 package city.zouitel.repository.datasource
 
+import city.zouitel.domain.utils.RequestState
 import city.zouitel.repository.model.GeminiQuest
 import kotlinx.coroutines.flow.Flow
 
 interface GeminiDataSource {
-    suspend fun sendGeminiPrompt(geminiQuest: GeminiQuest): Flow<String>
+
+    suspend fun sendGeminiPrompt(geminiQuest: GeminiQuest): Flow<RequestState<String>>
 }
