e431ae095e5af1ae5131c0df2182fa47965a4cc2
yon <bourntivoli@gmail.com>
2024-12-02 18:27:43 +0100

    Refactor: Update GeminiMapper to remove redundant mapping
    Removed the `toRepo` function in `GeminiMapper` which was redundant as it simply returned the input
     `Flow<String>`. The remaining `fromRepo` function maps the OutGeminiQuest domain model to the InGeminiQuest data model.
    
    This change simplifies the mapper by removing unnecessary logic and improves code clarity.

--- core/network/generativeai/src/main/java/city/zouitel/generativeai/mapper/GeminiMapper.kt
+++ core/network/generativeai/src/main/java/city/zouitel/generativeai/mapper/GeminiMapper.kt
@@ -1,12 +1,11 @@
 package city.zouitel.generativeai.mapper
 
+import city.zouitel.domain.utils.RequestState
 import kotlinx.coroutines.flow.Flow
 import city.zouitel.generativeai.model.GeminiQuest as InGeminiQuest
 import city.zouitel.repository.model.GeminiQuest as OutGeminiQuest
 
 class GeminiMapper {
-
-    fun toRepo(response: Flow<String>) = response
 
     fun fromRepo(geminiQuest: OutGeminiQuest) = InGeminiQuest(
         image = geminiQuest.image,
