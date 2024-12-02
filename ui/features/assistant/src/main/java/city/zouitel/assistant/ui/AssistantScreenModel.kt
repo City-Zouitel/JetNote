ad2328167bd21b8ec660a6978710b96d2dda4e37
yon <bourntivoli@gmail.com>
2024-12-02 18:25:52 +0100

    Refactor: Wrap Assistant Response in RequestState
    Wrapped the `getGenerativeResponse` state in `RequestState` to represent the different states of the request
     (Idle, Loading, Success, Error).
    
    This change likely improves the handling of loading and error states in the UI.

--- ui/features/assistant/src/main/java/city/zouitel/assistant/ui/AssistantScreenModel.kt
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/ui/AssistantScreenModel.kt
@@ -5,6 +5,7 @@ import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.assistant.mapper.AssistantMapper
 import city.zouitel.assistant.model.GeminiQuest
 import city.zouitel.domain.usecase.GeminiUseCase
+import city.zouitel.domain.utils.RequestState
 import city.zouitel.logic.asLogicFlow
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,13 +17,16 @@ class AssistantScreenModel(
     private val mapper: AssistantMapper
 ): ScreenModel {
 
-    private val _getGenerativeResponse = MutableStateFlow("")
-    val getGenerativeResponse: StateFlow<String> = _getGenerativeResponse.asLogicFlow("")
+    private val _getGenerativeResponse: MutableStateFlow<RequestState<String>>
+        get() = MutableStateFlow(RequestState.Idle)
+
+    val getGenerativeResponse: StateFlow<RequestState<String>>
+        get() = _getGenerativeResponse.asLogicFlow(RequestState.Idle)
 
     fun sendPrompt(geminiQuest: GeminiQuest) {
         screenModelScope.launch(Dispatchers.IO) {
-            sendGeminiPrompt(mapper.toDomain(geminiQuest)).collect {
-                _getGenerativeResponse.value = it
+            sendGeminiPrompt(mapper.toDomain(geminiQuest)).collect { response ->
+                _getGenerativeResponse.value = response
             }
         }
     }
