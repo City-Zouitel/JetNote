3ced99d1f25c1ed5e3dd8737505fe287c77c2da0
yon <bourntivoli@gmail.com>
2024-12-02 18:28:37 +0100

    chore: Remove GenerativeResponse sealed class
    Removed the `GenerativeResponse` sealed class which was used to represent the different states of a Generative AI
     response. This change is likely part of a larger refactoring or change in approach to handling responses.

--- core/network/generativeai/src/main/java/city/zouitel/generativeai/model/GenerativeResponse.kt
+++
@@ -1,7 +0,0 @@
-package city.zouitel.generativeai.model
-
-sealed class GenerativeResponse<out T> {
-    data object Loading: GenerativeResponse<Nothing>()
-    data class Success<out T>(val data: T): GenerativeResponse<T>()
-    data class Failure(val e: Exception?): GenerativeResponse<Nothing>()
-}
