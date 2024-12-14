a4346aa798d7b45a8859d7462da2c7abfd8181af
yon <bourntivoli@gmail.com>
2024-12-14 17:01:42 +0100

    feat: Add Assistant screen to navigation drawer
    Added a new screen option, `ASSISTANT_SCREEN`, to the `NoteScreens` enum.
    This likely
     adds an "Assistant" screen to the navigation drawer.

--- ui/screens/src/main/java/city/zouitel/screens/navigation_drawer/NoteScreens.kt
+++ ui/screens/src/main/java/city/zouitel/screens/navigation_drawer/NoteScreens.kt
@@ -2,6 +2,7 @@ package city.zouitel.screens.navigation_drawer
 
 enum class NoteScreens(val screen: String) {
     MAIN_SCREEN("Notes"),
+    ASSISTANT_SCREEN("Assistant"),
     TAGS_SCREEN("Tags"),
     SETTINGS_SCREEN("Settings"),
     REMOVED_SCREEN("Removed"),
