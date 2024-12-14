95be79c113522591817e32a479cdabdeff74d3ed
yon <bourntivoli@gmail.com>
2024-12-14 17:01:10 +0100

    feat: Add Gemini API key to DataStore
    Added `GEMINI_API_KEY` as a string preference key to DataStore for storing the Gemini API
     key.
    
    This change allows the app to securely store and retrieve the Gemini API key for accessing Generative AI features.

---
+++ core/network/generativeai/src/main/java/city/zouitel/generativeai/Keys.kt
@@ -0,0 +1,8 @@
+package city.zouitel.generativeai
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object Keys {
+
+    val GEMINI_API_KEY = stringPreferencesKey(Constants.GEMINI_API)
+}
