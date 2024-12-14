4c67938f6c4fca01238167c495618551ed169309
yon <bourntivoli@gmail.com>
2024-12-14 17:00:57 +0100

    feat: Add operation type to GeminiQuest
    Added an `OperationType` property to the `GeminiQuest` data class.
    
    This change likely allows specifying
     the type of operation to be performed for a given quest, such as a text request or an image request.

--- ui/features/assistant/src/main/java/city/zouitel/assistant/model/GeminiQuest.kt
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/model/GeminiQuest.kt
@@ -2,9 +2,11 @@ package city.zouitel.assistant.model
 
 import android.graphics.Bitmap
 import androidx.annotation.Keep
+import city.zouitel.assistant.OperationType
 
 @Keep
 data class GeminiQuest(
     val image: Bitmap? = null,
     val prompt: String = "",
+    val type: OperationType = OperationType.REQUEST
 )
