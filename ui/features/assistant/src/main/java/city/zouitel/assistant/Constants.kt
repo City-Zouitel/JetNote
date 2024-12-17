1a2c84dbcb46ec6f52f3bf41221c70d210c8df1b
yon <bourntivoli@gmail.com>
2024-12-17 21:13:50 +0100

    feat: Add Constants for Gemini API Information
    Added a new `Constants` object in the assistant module to store links to Gemini API key and models information.
    
    - `GEMINI_API_KEY_INFO`: Constant for Gemini API key information URL.
    - `GEMINI_MODELS_INFO`: Constant for Gemini models information URL.

---
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/Constants.kt
@@ -0,0 +1,7 @@
+package city.zouitel.assistant
+
+object Constants {
+
+    val GEMINI_API_KEY_INFO = "https://ai.google.dev/gemini-api/docs/api-key"
+    val GEMINI_MODELS_INFO = "https://ai.google.dev/gemini-api/docs/models/gemini"
+}
