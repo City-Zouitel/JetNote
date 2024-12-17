3285402f995466100b492e17d6fda7409ea84b2f
yon <bourntivoli@gmail.com>
2024-12-17 21:13:36 +0100

    chore: Update Gemini model to gemini-2.0-flash-exp
    Updated the Gemini model used for generative AI requests from `gemini-1
    .5-flash-latest` to `gemini-2.0-flash-exp`.

--- core/network/generativeai/src/main/java/city/zouitel/generativeai/Constants.kt
+++ core/network/generativeai/src/main/java/city/zouitel/generativeai/Constants.kt
@@ -3,5 +3,5 @@ package city.zouitel.generativeai
 object Constants {
 
     const val GEMINI_API = "gemini_api_key_preferences"
-    const val GEMINI_MODEL = "gemini-1.5-flash-latest"
+    const val GEMINI_MODEL = "gemini-2.0-flash-exp"
 }
