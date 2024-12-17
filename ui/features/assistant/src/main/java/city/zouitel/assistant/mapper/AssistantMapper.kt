3b660d50ac58f0068d030682b3c44ac54f259ede
yon <bourntivoli@gmail.com>
2024-12-17 20:50:53 +0100

    Refactor: Update AssistantMapper to handle Message objects
    Updated `AssistantMapper` to map between `Message` objects from the UI and domain layers instead of `
    GeminiQuest` objects.
    
    - Added methods `fromDomain` and `toDomain` to handle `Message` objects.
    - Removed methods for `GeminiQuest` mapping.

--- ui/features/assistant/src/main/java/city/zouitel/assistant/mapper/AssistantMapper.kt
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/mapper/AssistantMapper.kt
@@ -1,12 +1,21 @@
 package city.zouitel.assistant.mapper
 
-import city.zouitel.assistant.model.GeminiQuest as InGeminiQuest
-import city.zouitel.domain.model.GeminiQuest as OutGeminiQuest
+import city.zouitel.assistant.model.Message
+import city.zouitel.domain.model.Message as OutMessage
 
 class AssistantMapper {
 
-    fun toDomain(geminiQuest: InGeminiQuest) = OutGeminiQuest(
-        image = geminiQuest.image,
-        prompt = geminiQuest.prompt
+    fun fromDomain(messages: List<OutMessage>) = messages.map { fromDomain(it) }
+
+    fun toDomain(message: Message) = OutMessage(
+        id = message.id,
+        isRequest = message.isRequest,
+        prompt = message.prompt
+    )
+
+    fun fromDomain(message: OutMessage) = Message(
+        id = message.id,
+        isRequest = message.isRequest,
+        prompt = message.prompt
     )
 }
