5eeffaab25909652f4be76fbaf9ce537db582a45
yon <bourntivoli@gmail.com>
2024-12-17 21:15:21 +0100

    feat: Remove Reminding Column from DataEntity
    Removed the `reminding` column from the `DataEntity` table in the Room database.
    
    This
     change removes the functionality for reminders from the data model.

--- core/local/database/src/main/java/city/zouitel/database/model/DataEntity.kt
+++ core/local/database/src/main/java/city/zouitel/database/model/DataEntity.kt
@@ -1,19 +1,17 @@
 package city.zouitel.database.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.ColumnInfo
-import city.zouitel.database.utils.Constants.DURATION
 import city.zouitel.database.utils.Constants.COLOR
 import city.zouitel.database.utils.Constants.DATE
 import city.zouitel.database.utils.Constants.DESCRIPTION
 import city.zouitel.database.utils.Constants.NON
 import city.zouitel.database.utils.Constants.NOTES
 import city.zouitel.database.utils.Constants.PRIORITY
-import city.zouitel.database.utils.Constants.REMINDING
-import city.zouitel.database.utils.Constants.TXT_COLOR
-import city.zouitel.database.utils.Constants.TITLE
 import city.zouitel.database.utils.Constants.REMOVED
+import city.zouitel.database.utils.Constants.TITLE
+import city.zouitel.database.utils.Constants.TXT_COLOR
 import city.zouitel.database.utils.Constants.UUID
 
 @Entity(
@@ -28,6 +26,5 @@ data class DataEntity(
     @ColumnInfo(name = COLOR) var color: Int = 0,
     @ColumnInfo(name = TXT_COLOR) var textColor: Int = 0x000000,
     @ColumnInfo(name = DATE) var date: String = "",
-    @ColumnInfo(name = REMOVED) var removed: Int = 0,
-    @ColumnInfo(name = REMINDING) var reminding: Long = 0L,
+    @ColumnInfo(name = REMOVED) var removed: Int = 0
 )
