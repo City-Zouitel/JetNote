77ce467b6d0e9380794b534cb08793d33c5ceec2
yon <bourntivoli@gmail.com>
2024-12-17 21:25:11 +0100

    Refactor: Rename Gemini to Message in network repository module
    Renamed GeminiRepository, GeminiUseCase, and GeminiMapper to MessageRepository, MessageUseCase, and Message
    Mapper, respectively, in the network repository module.
    
    - Replaced GeminiRepository with MessageRepository.
    - Replaced GeminiUseCase with MessageUseCase, including renaming SendGeminiPrompt to InsertMessage and introducing ObserveAllMessages.
    - Replaced GeminiMapper with MessageMapper.

--- core/network/repository/src/main/java/city/zouitel/repository/di/networkRepositoryKoinModule.kt
+++ core/network/repository/src/main/java/city/zouitel/repository/di/networkRepositoryKoinModule.kt
@@ -1,8 +1,8 @@
 package city.zouitel.repository.di
 
-import city.zouitel.domain.repository.GeminiRepository
-import city.zouitel.domain.usecase.GeminiUseCase
-import city.zouitel.repository.mapper.GeminiMapper
+import city.zouitel.domain.repository.MessageRepository
+import city.zouitel.domain.usecase.MessageUseCase
+import city.zouitel.repository.mapper.MessageMapper
 import city.zouitel.repository.repositoryImpl.GeminiRepositoryImpl
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
@@ -10,8 +10,9 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val networkRepositoryKoinModule = module {
-    factoryOf(::GeminiMapper)
-    singleOf(::GeminiRepositoryImpl) bind GeminiRepository::class
+    factoryOf(::MessageMapper)
+    singleOf(::GeminiRepositoryImpl) bind MessageRepository::class
 
-    factoryOf(GeminiUseCase::SendGeminiPrompt)
+    factoryOf(MessageUseCase::ObserveAllMessages)
+    factoryOf(MessageUseCase::InsertMessage)
 }
