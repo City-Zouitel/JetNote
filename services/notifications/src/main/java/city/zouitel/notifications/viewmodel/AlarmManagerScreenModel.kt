af5b2bb407df95821ebf146becf75c652a9a54a8
yon <bourntivoli@gmail.com>
2024-12-17 20:50:30 +0100

    feat: Add Reminder Update functionality to AlarmManagerScreenModel
    Added functionality to update reminders using `ReminderUseCase.Update` in `AlarmManagerScreenModel`.
    
    - The `AlarmManagerScreenModel` now implements `UiEventHandler` to handle `UiEvent.Update` events.
    - `sendUiEvent` now calls `updateReminder` when receiving a `UiEvent.Update` event.
    - A `performUiEvent` function was added to execute the update operation
     using coroutines on the IO dispatcher.

--- services/notifications/src/main/java/city/zouitel/notifications/viewmodel/AlarmManagerScreenModel.kt
+++ services/notifications/src/main/java/city/zouitel/notifications/viewmodel/AlarmManagerScreenModel.kt
@@ -6,13 +6,23 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import city.zouitel.domain.usecase.ReminderUseCase
+import city.zouitel.logic.events.UiEvent
+import city.zouitel.logic.events.UiEventHandler
 import city.zouitel.notifications.Constants.ID
 import city.zouitel.notifications.Constants.MESSAGE
 import city.zouitel.notifications.Constants.TITLE
 import city.zouitel.notifications.Constants.UID
 import city.zouitel.notifications.reciver.NotificationReceiver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class AlarmManagerScreenModel(private val context: Context): ScreenModel {
+class AlarmManagerScreenModel(
+    private val context: Context,
+    private val updateReminder: ReminderUseCase.Update
+): ScreenModel, UiEventHandler<Long> {
+
     private val intent = Intent(context , NotificationReceiver::class.java)
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
     private val basicPendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
@@ -56,5 +66,16 @@ class AlarmManagerScreenModel(private val context: Context): ScreenModel {
                 PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
             )
         )
+    }
+
+    override fun sendUiEvent(event: UiEvent<Long>) {
+        when(event) {
+            is UiEvent.Update -> performUiEvent { updateReminder(event.data) }
+            else -> throw NotImplementedError("UiEvent not implemented: $event")
+        }
+    }
+
+    override fun performUiEvent(action: suspend () -> Unit) {
+        screenModelScope.launch(Dispatchers.IO) { action.invoke() }
     }
 }
