b755c3b99576357f06a73a4d20ae450119f21044
yon <bourntivoli@gmail.com>
2024-12-18 21:51:08 +0100

    Refactor: Remove unused fields from WorkplaceUiState
    Removed unused fields from `WorkplaceUiState`, including:
    - `remindingDialogState`
    -
     `recordedDialogState`
    - `photo`
    - `isFocused`
    
    This change simplifies the state and removes unnecessary data.

--- ui/features/workplace/src/main/java/city/zouitel/note/state/WorkplaceUiState.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/state/WorkplaceUiState.kt
@@ -1,16 +1,11 @@
 package city.zouitel.note.state
 
-import android.net.Uri
 import city.zouitel.note.ui.utils.PriorityColorsList
 
 internal data class WorkplaceUiState(
     val priority: String = PriorityColorsList.NON.priority,
     val textColor: Int = 0,
     val backgroundColor: Int = 0,
-    val remindingDialogState: Boolean = false,
-    val recordedDialogState: Boolean = false,
     val isTitleFieldFocused: Boolean = false,
-    val isDescriptionFieldFocused: Boolean = false,
-    val photo: Uri? = null, // TODO:
-    val isFocused: Boolean = false // TODO:
+    val isDescriptionFieldFocused: Boolean = false
 )
