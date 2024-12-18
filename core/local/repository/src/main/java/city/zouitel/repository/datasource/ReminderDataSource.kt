dccaab2c72f95e3e2ab505b4ee3219d3f83f8dd9
yon <bourntivoli@gmail.com>
2024-12-18 21:46:37 +0100

    Refactor: Update ReminderDataSource interface
    Updated the `ReminderDataSource` interface to use the `Reminder` model directly and added documentation for each function.
    
    -
     Changed `observeById` to return a `Flow<List<Reminder>>`.
    - Renamed `delete` to accept an `Int` for the reminder ID.
    - Updated the parameter type of `insert` and `update` to `Reminder`.
    - Added `update` function that updates a reminder by
     its id
    - Added documentation for all functions.

--- core/local/repository/src/main/java/city/zouitel/repository/datasource/ReminderDataSource.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/datasource/ReminderDataSource.kt
@@ -1,17 +1,47 @@
 package city.zouitel.repository.datasource
 
+import city.zouitel.repository.model.Reminder
 import kotlinx.coroutines.flow.Flow
-import city.zouitel.repository.model.Reminder as InReminder
 
+/**
+ * Data source interface for accessing and managing [Reminder]s.
+ */
 interface ReminderDataSource {
 
-    suspend fun observeById(uid: String): Flow<List<InReminder>>
+    /**
+     * Observes reminders for a specific user ID.
+     *
+     * This function returns a [Flow] that emits a list of Reminder objects
+     * associated with the given user ID whenever there are changes in the data.
+     *
+     * @param uid The unique identifier of the user.
+     * @return A [Flow] emitting lists of Reminder objects for the user.
+     */
+    suspend fun observeById(uid: String): Flow<List<Reminder>>
 
-    suspend fun insert(reminder: InReminder)
+    /**
+     * Inserts a new reminder into the data source.
+     *
+     * @param reminder The reminder to be inserted.
+     */
+    suspend fun insert(reminder: Reminder)
 
-    suspend fun update(reminder: InReminder)
+    /**
+     * Updates an entity with the given [id].
+     *
+     * @param id The identifier of the entity to update.
+     */
+    suspend fun update(id: Int)
 
-    suspend fun delete(id: Long)
+    /**
+     * Deletes an entity with the given ID.
+     *
+     * @param id The ID of the entity to delete.
+     */
+    suspend fun delete(id: Int)
 
+    /**
+     * Deletes all entries from the data source.
+     */
     suspend fun deleteAll()
 }
