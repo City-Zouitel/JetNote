e5e5d712ddd777f35ac0901f19ca07b0f843ccd6
yon <bourntivoli@gmail.com>
2024-12-18 21:47:47 +0100

    Refactor: Improve ReminderDataSourceImpl with Documentation and Type Safety
    - Added comprehensive documentation to `ReminderDataSourceImpl` clarifying its purpose and functionality.
    - Updated
     `update` and `delete` methods to use `Int` instead of `Long` for the reminder ID, improving type safety.
    - Improved code readability and maintainability.

--- core/local/database/src/main/java/city/zouitel/database/datasourceImpl/ReminderDataSourceImpl.kt
+++ core/local/database/src/main/java/city/zouitel/database/datasourceImpl/ReminderDataSourceImpl.kt
@@ -7,27 +7,57 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import city.zouitel.repository.model.Reminder as OutReminder
 
+/**
+ * Implementation of the [ReminderDataSource] interface using a [ReminderDao] to access the database.
+ * This class is responsible for providing data related to reminders.
+ * It also implements the [ReminderMapper] interface to handle data mapping between domain and data layer.
+ */
 class ReminderDataSourceImpl(
     private val dao: ReminderDao,
     private val mapper: ReminderMapper
 ): ReminderDataSource {
 
+    /**
+     * Observes a list of reminders for a specific user ID.
+     *
+     * @param uid The user ID to filter reminders by.
+     * @return A flow emitting a list of [OutReminder] objects for the given user ID.
+     *         The list will be updated whenever there are changes in the database.
+     */
     override suspend fun observeById(uid: String): Flow<List<OutReminder>> {
         return dao.observeAllById(uid).map { mapper.toRepo(it) }
     }
 
+    /**
+     * Inserts a new reminder into the data source.
+     *
+     * @param reminder The reminder to be inserted.
+     */
     override suspend fun insert(reminder: OutReminder) {
         dao.insert(mapper.fromRepo(reminder))
     }
 
-    override suspend fun update(reminder: OutReminder) {
-        dao.update(mapper.fromRepo(reminder))
+    /**
+     * Updates an entity in the database based on its ID.
+     *
+     * @param id The ID of the entity to update.
+     */
+    override suspend fun update(id: Int) {
+        dao.update(id)
     }
 
-    override suspend fun delete(id: Long) {
+    /**
+     * Deletes a reminder with the given ID.
+     *
+     * @param id The ID of the reminder entity to delete.
+     */
+    override suspend fun delete(id: Int) {
         dao.delete(id)
     }
 
+    /**
+     * Deletes all reminders from the database.
+     */
     override suspend fun deleteAll() {
         dao.deleteAll()
     }
