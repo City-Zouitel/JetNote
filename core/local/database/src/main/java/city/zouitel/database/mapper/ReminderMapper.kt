07f4a232543f92818b27ba949509b70908bece0b
yon <bourntivoli@gmail.com>
2024-12-18 21:48:14 +0100

    Refactor: Improve ReminderMapper with Documentation and Type Renaming
    Improved the `ReminderMapper` by adding comprehensive documentation and renaming the input type from `InReminder
    ` to `Reminder` for better clarity.
    
    This change enhances the readability and maintainability of the mapper by providing clear explanations of its functionality and using more descriptive type names.

--- core/local/database/src/main/java/city/zouitel/database/mapper/ReminderMapper.kt
+++ core/local/database/src/main/java/city/zouitel/database/mapper/ReminderMapper.kt
@@ -1,21 +1,47 @@
 package city.zouitel.database.mapper
 
-import city.zouitel.database.model.ReminderEntity as InReminder
+import city.zouitel.database.model.Reminder
 import city.zouitel.repository.model.Reminder as OutReminder
 
 
+/**
+ * A mapper class responsible for converting between repository [Reminder] and database [OutReminder].
+ * This class provides methods for mapping lists of reminders and individual reminders between the two types.
+ */
 class ReminderMapper {
 
-    fun toRepo(reminders: List<InReminder>) = reminders.map { toRepo(it) }
+    /**
+     * Maps a list of Reminder objects to a list of their repository representations.
+     *
+     * @param reminders The list of Reminder objects to map.
+     * @return A list containing the repository representations of the given reminders
+     * that are mapped from Reminder to OutReminder.
+     */
+    fun toRepo(reminders: List<Reminder>) = reminders.map { toRepo(it) }
 
-    fun toRepo(reminder: InReminder) = OutReminder(
+    /**
+     * Maps a Reminder object to an OutReminder object.
+     *
+     * This function converts a Reminder object, which is typically used internally,
+     * to an OutReminder object, which is likely used for external communication or data transfer.
+     *
+     * @param reminder The input Reminder object to be mapped.
+     * @return An OutReminder object representing the mapped Reminder.
+     */
+    fun toRepo(reminder: Reminder) = OutReminder(
         id = reminder.id,
         uid = reminder.uid,
         atTime = reminder.atTime,
         isPassed = reminder.isPassed
     )
 
-    fun fromRepo(reminder: OutReminder) = InReminder(
+    /**
+     * Maps an OutReminder data object from the repository layer to a Reminder data object for the domain layer.
+     *
+     * @param reminder The OutReminder object to map.
+     * @return A Reminder object representing the same data.
+     */
+    fun fromRepo(reminder: OutReminder) = Reminder(
         id = reminder.id,
         uid = reminder.uid,
         atTime = reminder.atTime,
