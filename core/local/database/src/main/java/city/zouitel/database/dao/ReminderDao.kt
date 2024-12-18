64c0c512d340fabe1d873a90c19b9601556f6c71
yon <bourntivoli@gmail.com>
2024-12-18 21:46:14 +0100

    Refactor: Update ReminderDao and ReminderEntity
    Updated the `ReminderDao` and `ReminderEntity` to better manage reminders.
    
    - Renamed `Reminder
    Entity` to `Reminder`.
    - Added documentation to `ReminderDao` methods.
    - Modified `update` method in `ReminderDao` to mark a reminder as passed using a query.
    - Changed the ID type in `ReminderDao` and `Reminder` from Long to Int.
    - Added `PASSED
    ` column in `Reminder` and updated related queries.
    
    These changes improve the clarity and efficiency of reminder management.

--- core/local/database/src/main/java/city/zouitel/database/dao/ReminderDao.kt
+++ core/local/database/src/main/java/city/zouitel/database/dao/ReminderDao.kt
@@ -4,28 +4,56 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
-import city.zouitel.database.model.ReminderEntity
-import city.zouitel.database.model.ReminderEntity.Companion.TABLE_NAME
+import city.zouitel.database.model.Reminder
+import city.zouitel.database.model.Reminder.Companion.TABLE_NAME
 import city.zouitel.database.utils.Constants.ID
+import city.zouitel.database.utils.Constants.PASSED
 import city.zouitel.database.utils.Constants.UUID
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Data Access Object for the Reminder entity.
+ * Provides methods for interacting with the reminder database table.
+ */
 @Dao
 interface ReminderDao {
 
+    /**
+     * Observes all reminders associated with a specific user ID.
+     *
+     * @param uid: The unique identifier of the user.
+     * @return A Flow emitting a list of reminders for the given user ID.
+     */
     @Query("SELECT * FROM $TABLE_NAME WHERE $UUID = :uid")
-    fun observeAllById(uid: String): Flow<List<ReminderEntity>>
+    fun observeAllById(uid: String): Flow<List<Reminder>>
 
+    /**
+     * Inserts a new reminder into the database.
+     *
+     * @param reminder: The reminder to be inserted.
+     */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(reminder: ReminderEntity)
+    suspend fun insert(reminder: Reminder)
 
-    @Update(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun update(reminder: ReminderEntity)
+    /**
+     * Updates the record with the given ID, setting the 'passed' column to 1 (true).
+     *
+     * @param id The ID of the record to update.
+     */
+    @Query("UPDATE $TABLE_NAME SET $PASSED = 1 WHERE $ID = :id")
+    suspend fun update(id: Int)
 
+    /**
+     * Deletes all rows from the reminder table.
+     */
     @Query("DELETE FROM $TABLE_NAME")
     suspend fun deleteAll()
 
+    /**
+     * Deletes a row from the database table based on the provided ID.
+     *
+     * @param id: The ID of the row to be deleted.
+     */
     @Query("DELETE FROM $TABLE_NAME WHERE $ID = :id")
-    suspend fun delete(id: Long)
+    suspend fun delete(id: Int)
 }
