caeb231fa8af1bc79d7ffe348f6f0f11b4ddca3c
yon <bourntivoli@gmail.com>
2024-12-18 21:48:30 +0100

    Refactor: Add documentation and rename Reminder data model
    Added documentation to the `ReminderMapper` class to clarify its role in mapping between data and domain models.
    
    Renamed the `InReminder` data model to `Reminder` for better clarity and consistency.
    
    These changes improve the readability and maintainability of the code.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/ReminderMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/ReminderMapper.kt
@@ -1,19 +1,41 @@
 package city.zouitel.repository.mapper
 
+import city.zouitel.repository.model.Reminder
 import city.zouitel.domain.model.Reminder as OutReminder
-import city.zouitel.repository.model.Reminder as InReminder
 
+/**
+ * A mapper class responsible for converting between [Reminder] (repository module) and [OutReminder] (domain layer) objects.
+ */
 class ReminderMapper {
-    fun toDomain(reminders: List<InReminder>) = reminders.map { toDomain(it) }
 
-    fun toDomain(reminder: InReminder) = OutReminder(
+    /**
+     * Maps a list of Reminder data objects to a list of domain models.
+     *
+     * @param reminders The list of Reminder data objects to map.
+     * @return A list of domain models representing the given reminders.
+     */
+    fun toDomain(reminders: List<Reminder>) = reminders.map { toDomain(it) }
+
+    /**
+     * Maps a Reminder data object to an OutReminder domain object.
+     *
+     * @param reminder The Reminder data object to map.
+     * @return An OutReminder domain object representing the same reminder.
+     */
+    fun toDomain(reminder: Reminder) = OutReminder(
         id = reminder.id,
         uid = reminder.uid,
         atTime = reminder.atTime,
         isPassed = reminder.isPassed
     )
 
-    fun fromDomain(reminder: OutReminder) = InReminder(
+    /**
+     * Maps an OutReminder (domain model) to a Reminder (data model).
+     *
+     * @param reminder The domain model representation of a reminder.
+     * @return The data model representation of the reminder.
+     */
+    fun fromDomain(reminder: OutReminder) = Reminder(
         id = reminder.id,
         uid = reminder.uid,
         atTime = reminder.atTime,
