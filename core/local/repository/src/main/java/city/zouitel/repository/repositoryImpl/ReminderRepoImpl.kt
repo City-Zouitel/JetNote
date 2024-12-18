226eaf721e59643b70f9544801ac4c9cf4b0cc9c
yon <bourntivoli@gmail.com>
2024-12-18 21:49:33 +0100

    feat: Implement ReminderRepoImpl
    This commit introduces the `ReminderRepoImpl` class, which implements the `ReminderRepo` interface.
    
    - It uses
     `ReminderDataSource` for data access and `ReminderMapper` for object mapping.
    - The `observeById` function observes reminders for a specific user ID and maps them to domain objects using a flow.
    - It also provides functions for inserting, updating, deleting, and deleting all reminders.

---
+++ core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/ReminderRepoImpl.kt
@@ -0,0 +1,69 @@
+package city.zouitel.repository.repositoryImpl
+
+import city.zouitel.domain.repository.ReminderRepo
+import city.zouitel.repository.datasource.ReminderDataSource
+import city.zouitel.repository.mapper.ReminderMapper
+import city.zouitel.repository.model.Reminder
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import city.zouitel.domain.model.Reminder as OutReminder
+
+/**
+ * Implementation of the [ReminderRepo] interface.
+ * This class interacts with the [dataSource] to perform CRUD operations on reminders.
+ * It uses the [mapper] to convert between domain model [OutReminder] and repository model [Reminder].
+ */
+class ReminderRepoImpl(
+    private val dataSource: ReminderDataSource,
+    private val mapper: ReminderMapper
+): ReminderRepo {
+
+    /**
+     * Observes reminders for a specific user ID.
+     *
+     * This function observes the reminders associated with the given user ID and emits a flow of
+     * domain-specific reminder objects (OutReminder).
+     * It leverages the data source to retrieve the reminders and then uses a mapper to transform
+     * them into the desired domain format.
+     *
+     * @param uid The user ID for which to observe reminders.
+     * @return A flow of lists of OutReminder objects, representing the reminders for the user.
+     */
+    override suspend fun observeById(uid: String): Flow<List<OutReminder>> {
+        return dataSource.observeById(uid).map { reminders -> mapper.toDomain(reminders) }
+    }
+
+    /**
+     * Inserts a new reminder into the data source.
+     *
+     * @param reminder The reminder to be inserted.
+     */
+    override suspend fun insert(reminder: OutReminder) {
+        dataSource.insert(mapper.fromDomain(reminder))
+    }
+
+    /**
+     * Updates an entity with the given ID.
+     *
+     * @param id The ID of the entity to update.
+     */
+    override suspend fun update(id: Int) {
+        dataSource.update(id)
+    }
+
+    /**
+     * Deletes an entity with the given ID.
+     *
+     * @param id The ID of the entity to delete.
+     */
+    override suspend fun delete(id: Int) {
+        dataSource.delete(id)
+    }
+
+    /**
+     * Deletes all items from the database.
+     */
+    override suspend fun deleteAll() {
+        dataSource.deleteAll()
+    }
+}
