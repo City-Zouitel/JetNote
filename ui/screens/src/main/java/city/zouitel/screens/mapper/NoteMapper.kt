5bac27747980b87a05c66c24d391962c053b4a56
yon <bourntivoli@gmail.com>
2024-12-24 19:16:29 +0100

    Refactor: Update Note model imports in NoteMapper
    Updated the import paths for `Note` models in `NoteMapper`:
    
    - Replaced `city.zou
    itel.domain.model.Note` with `city.zouitel.note.model.Note` for the internal representation.
    - Replaced `city.zouitel.note.model.Note` with `city.zouitel.domain.model.Note` for the external (domain) representation.
    
    This
     change likely clarifies the distinction between internal and external Note representations and improves code organization.
    Additionally, removed taskEntities mapping which was unused.

--- ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
+++ ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
@@ -4,10 +4,10 @@ import city.zouitel.audios.mapper.AudioMapper
 import city.zouitel.links.mapper.LinkMapper
 import city.zouitel.media.mapper.MediaMapper
 import city.zouitel.note.mapper.DataMapper
-import city.zouitel.domain.model.Note as OutNote
+import city.zouitel.note.model.Note
 import city.zouitel.tags.mapper.TagMapper
 import city.zouitel.tasks.mapper.TaskMapper
-import city.zouitel.note.model.Note as InNote
+import city.zouitel.domain.model.Note as OutNote
 
 class NoteMapper(
     private val dataMapper: DataMapper,
@@ -19,11 +19,10 @@ class NoteMapper(
 ) {
     fun fromDomain(notes: List<OutNote>) = notes.map { fromDomain(it) }
 
-    private fun fromDomain(data: OutNote): InNote = with(data){
-        InNote(
+    private fun fromDomain(data: OutNote): Note = with(data){
+        Note(
             dataEntity = dataMapper.fromDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.fromDomain(it) },
-            taskEntities = taskEntities.map { taskMapper.fromDomain(it) },
             linkEntities = linkEntities.map { linkMapper.fromDomain(it) },
             audioEntities = audioEntities.map { audioMapper.fromDomain(it) },
             mediaEntities = mediaEntities.map { mediaMapper.fromDomain(it) }
