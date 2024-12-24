0dd684acfa5290f2a4d92e2500fdaa61391e9ab1
yon <bourntivoli@gmail.com>
2024-12-24 20:16:41 +0100

    feat: Remove taskEntities from Note model
    Removed the `taskEntities` property from the `Note` data class in the domain layer. This suggests that tasks
     are no longer directly associated with notes and might be managed separately.

--- domain/src/main/java/city/zouitel/domain/model/Note.kt
+++ domain/src/main/java/city/zouitel/domain/model/Note.kt
@@ -6,7 +6,6 @@ import androidx.annotation.Keep
 data class Note(
     val dataEntity: Data,
     val tagEntities: List<Tag>,
-    val taskEntities: List<Task>,
     val linkEntities: List<Link>,
     val audioEntities: List<Audio>,
     val mediaEntities: List<Media>
