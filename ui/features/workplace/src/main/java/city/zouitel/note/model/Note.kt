f1e4cb598977406a9fa2346e7b941a4ad85ca661
yon <bourntivoli@gmail.com>
2024-12-24 19:14:10 +0100

    feat: Remove taskEntities from Note data class
    Removed the `taskEntities` property from the `Note` data class. This change likely removes the association between
     notes and tasks.

--- ui/features/workplace/src/main/java/city/zouitel/note/model/Note.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/model/Note.kt
@@ -4,15 +4,12 @@ import androidx.annotation.Keep
 import city.zouitel.audios.model.Audio
 import city.zouitel.links.model.Link
 import city.zouitel.media.model.Media
-import city.zouitel.note.model.Data
 import city.zouitel.tags.model.Tag
-import city.zouitel.tasks.model.Task
 
 @Keep
 data class Note(
     val dataEntity: Data,
     val tagEntities: List<Tag>,
-    val taskEntities: List<Task>,
     val linkEntities: List<Link>,
     val audioEntities: List<Audio>,
     val mediaEntities: List<Media>
