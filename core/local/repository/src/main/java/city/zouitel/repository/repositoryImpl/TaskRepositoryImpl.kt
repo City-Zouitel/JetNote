51d6b49d73ecaaaca9f6494c6681ab67a0897a35
yon <bourntivoli@gmail.com>
2024-12-24 20:23:56 +0100

    Refactor: Update TaskRepositoryImpl to use Flow and new methods
    Updated `TaskRepositoryImpl` to use Flow for observing tasks and introduced new methods for data
     manipulation.
    
    - Changed `getAllTaskItems` to `observeAll` and implemented it using `Flow` and `map` to transform data to the domain model.
    - Added `observeByUid` to observe tasks by uid using Flow.
    - Replaced `addTaskItem`, `updateTaskItem`, and `
    deleteTaskItem` with `insert`, `updateById`, and `deleteById`, respectively. These new methods operate on task ids instead of entire task objects.

--- core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/TaskRepositoryImpl.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/TaskRepositoryImpl.kt
@@ -1,28 +1,33 @@
 package city.zouitel.repository.repositoryImpl
 
-import city.zouitel.domain.model.Task as OutTask
+import city.zouitel.domain.model.Task
 import city.zouitel.domain.repository.TaskRepository
 import city.zouitel.repository.datasource.TaskDataSource
 import city.zouitel.repository.mapper.TaskMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import city.zouitel.domain.model.Task as OutTask
 
 class TaskRepositoryImpl(
     private val dataSource: TaskDataSource,
     private val mapper: TaskMapper
 ): TaskRepository {
-    override val getAllTaskItems: Flow<List<OutTask>>
-        get() = dataSource.getAllTaskItems.map { tasks -> mapper.toDomain(tasks) }
+    override val observeAll: Flow<List<Task>>
+        get() = dataSource.observeAll.map { mapper.toDomain(it) }
 
-    override suspend fun addTaskItem(task: OutTask) {
-        dataSource.addTaskItem(mapper.fromDomain(task))
+    override fun observeByUid(uid: String): Flow<List<Task>> {
+        return dataSource.observeByUid(uid).map { mapper.toDomain(it) }
     }
 
-    override suspend fun updateTaskItem(task: OutTask) {
-        dataSource.updateTaskItem(mapper.fromDomain(task))
+    override suspend fun insert(task: OutTask) {
+        dataSource.insert(mapper.fromDomain(task))
     }
 
-    override suspend fun deleteTaskItem(task: OutTask) {
-        dataSource.deleteTaskItem(mapper.fromDomain(task))
+    override suspend fun updateById(id: Long) {
+        dataSource.updateById(id)
+    }
+
+    override suspend fun deleteById(id: Long) {
+        dataSource.deleteById(id)
     }
 }
