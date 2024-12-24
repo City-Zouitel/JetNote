ae7a15a50b828467cdbb6815bd12f3776b1138f8
yon <bourntivoli@gmail.com>
2024-12-24 20:16:48 +0100

    Refactor: Remove NoteAndTask related dependencies from tasksKoinModule
    Removed the `NoteAndTaskMapper` and `NoteAndTaskScreenModel` dependencies
     from the `tasksKoinModule`. This change likely removes the integration of notes and tasks within the tasks module.

--- ui/features/tasks/src/main/java/city/zouitel/tasks/di/tasksKoinModule.kt
+++ ui/features/tasks/src/main/java/city/zouitel/tasks/di/tasksKoinModule.kt
@@ -1,16 +1,11 @@
 package city.zouitel.tasks.di
 
-import city.zouitel.tasks.mapper.NoteAndTaskMapper
 import city.zouitel.tasks.mapper.TaskMapper
-import city.zouitel.tasks.ui.NoteAndTaskScreenModel
 import city.zouitel.tasks.ui.TaskScreenModel
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val tasksKoinModule = module {
     factoryOf(::TaskMapper)
-    factoryOf(::NoteAndTaskMapper)
-
     factoryOf(::TaskScreenModel)
-    factoryOf(::NoteAndTaskScreenModel)
 }
