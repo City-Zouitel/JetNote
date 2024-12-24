480ea4983cb401af217373bdf13b46e9295cf3eb
yon <bourntivoli@gmail.com>
2024-12-24 19:17:49 +0100

    Refactor: Remove unnecessary inheritance in RootMapper
    Removed the `Mapper` interface inheritance and direct dependency on the `Root` data class from the `RootMapper
    `.
    
    This change simplifies the mapper and removes unnecessary dependencies. It is likely part of a larger refactoring effort to improve the codebase's structure and maintainability.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/RootMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/RootMapper.kt
@@ -1,8 +1,6 @@
 package city.zouitel.repository.mapper
 
 import city.zouitel.domain.model.Root as OutRoot
-import city.zouitel.repository.mapper.base.Mapper
-import city.zouitel.repository.model.Root
 import city.zouitel.repository.model.Root as InRoot
 
 class RootMapper {
