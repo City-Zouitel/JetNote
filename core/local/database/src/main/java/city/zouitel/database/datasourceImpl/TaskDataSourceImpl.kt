781eec7ff06bde2f7e70570dd25cbdf989c85f2f
yon <bourntivoli@gmail.com>
2024-12-24 20:23:10 +0100

    Refactor: Update TaskDataSourceImpl to use new DAO methods
    Updated `TaskDataSourceImpl` to use the new DAO methods introduced in a previous commit.
    
    - Renamed `getAllTaskItems` to `observeAll` and updated its implementation to use `dao.observeAll`.
    - Added `observeByUid` to observe tasks by their UID.
    - Renamed `addTaskItem` to `insert` and updated its implementation to use `dao.insert`.
    
    - Renamed `updateTaskItem` to `updateById` and updated its implementation to use `dao.updateById`.
    - Renamed `deleteTaskItem` to `deleteById` and updated its implementation to use `dao.deleteById`.
    
    This change aligns `TaskDataSourceImpl` with the updated DAO interface.

--- core/local/database/src/main/java/city/zouitel/database/datasourceImpl/TaskDataSourceImpl.kt
+++ core/local/database/src/main/java/city/zouitel/database/datasourceImpl/TaskDataSourceImpl.kt
@@ -3,26 +3,31 @@ package city.zouitel.database.datasourceImpl
 import city.zouitel.database.dao.TaskDao
 import city.zouitel.database.mapper.TaskMapper
 import city.zouitel.repository.datasource.TaskDataSource
-import city.zouitel.repository.model.Task as OutTask
+import city.zouitel.repository.model.Task
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import city.zouitel.repository.model.Task as OutTask
 
 class TaskDataSourceImpl(
     private val dao: TaskDao,
     private val mapper: TaskMapper
 ): TaskDataSource {
-    override val getAllTaskItems: Flow<List<OutTask>>
-        get() = dao.getAllTaskItems().map { tasks -> mapper.toRepo(tasks) }
+    override val observeAll: Flow<List<Task>>
+        get() = dao.observeAll.map { mapper.toRepo(it) }
 
-    override suspend fun addTaskItem(task: OutTask) {
-        dao.addTaskItem(mapper.fromRepo(task))
+    override fun observeByUid(uid: String): Flow<List<Task>> {
+        return dao.observeByUid(uid).map { mapper.toRepo(it) }
     }
 
-    override suspend fun updateTaskItem(task: OutTask) {
-        dao.updateTaskItem(mapper.fromRepo(task))
+    override suspend fun insert(task: OutTask) {
+        dao.insert(mapper.fromRepo(task))
     }
 
-    override suspend fun deleteTaskItem(task: OutTask) {
-        dao.deleteTaskItem(mapper.fromRepo(task))
+    override suspend fun updateById(id: Long) {
+        dao.updateById(id)
+    }
+
+    override suspend fun deleteById(id: Long) {
+        dao.deleteById(id)
     }
 }
