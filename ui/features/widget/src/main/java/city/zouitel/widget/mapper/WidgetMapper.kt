99c620b872364c0c9c02904cbf82e13751169324
yon <bourntivoli@gmail.com>
2024-12-24 20:17:51 +0100

    Refactor: Rename WidgetNote and remove TaskMapper dependency in WidgetMapper
    Renamed `WidgetNote` from `InNote` for better clarity and removed the
     unused dependency on `TaskMapper` in `WidgetMapper`. This refactoring simplifies the code and improves its readability.

--- ui/features/widget/src/main/java/city/zouitel/widget/mapper/WidgetMapper.kt
+++ ui/features/widget/src/main/java/city/zouitel/widget/mapper/WidgetMapper.kt
@@ -2,25 +2,22 @@ package city.zouitel.widget.mapper
 
 import city.zouitel.links.mapper.LinkMapper
 import city.zouitel.note.mapper.DataMapper
-import city.zouitel.note.model.Note
-import city.zouitel.domain.model.Note as OutNote
 import city.zouitel.tags.mapper.TagMapper
 import city.zouitel.tasks.mapper.TaskMapper
-import city.zouitel.widget.model.WidgetNote as InNote
+import city.zouitel.widget.model.WidgetNote
+import city.zouitel.domain.model.Note as OutNote
 
 class WidgetMapper(
     private val dataMapper: DataMapper,
     private val tagMapper: TagMapper,
-    private val taskMapper: TaskMapper,
     private val linkMapper: LinkMapper
 ) {
     fun fromDomain(notes: List<OutNote>) = notes.map { fromDomain(it) }
 
-    fun fromDomain(data: OutNote): InNote = with(data){
-        InNote(
+    fun fromDomain(data: OutNote): WidgetNote = with(data){
+        WidgetNote(
             dataEntity = dataMapper.fromDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.fromDomain(it) },
-            taskEntities = taskEntities.map { taskMapper.fromDomain(it) },
             linkEntities = linkEntities.map { linkMapper.fromDomain(it) }
         )
     }
