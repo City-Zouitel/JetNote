2e9aac09b45d446acab286be8d693355e4d7f4a2
yon <bourntivoli@gmail.com>
2024-12-25 17:20:26 +0100

    docs: Add KDoc to TaskMapper
    Added KDoc documentation to the `TaskMapper` class and its methods, providing clear explanations of their purpose and functionality
    . This improves code readability and understanding.

--- core/local/database/src/main/java/city/zouitel/database/mapper/TaskMapper.kt
+++ core/local/database/src/main/java/city/zouitel/database/mapper/TaskMapper.kt
@@ -3,10 +3,32 @@ package city.zouitel.database.mapper
 import city.zouitel.database.model.Task
 import city.zouitel.repository.model.Task as OutTask
 
+/**
+ * Mapper class responsible for converting between [Task] (domain model) and [OutTask] (repository model).
+ *
+ * This class provides methods to transform a list of tasks or a single task
+ * between the domain and repository representations.
+ */
 class TaskMapper {
 
+    /**
+     * Maps a list of Task objects to a list of their repository representations.
+     *
+     * @param tasks The list of Tasks objects to map.
+     * @return A list containing the repository representations of the given tasks
+     * that are mapped from Task to OutTask.
+     */
     fun toRepo(tasks: List<Task>) = tasks.map { toRepo(it) }
 
+    /**
+     * Maps a Task object to an OutTask object.
+     *
+     * This function converts a Task object, which is typically used internally,
+     * to an OutTask object, which is likely used for external communication or data transfer.
+     *
+     * @param task The input Task object to be mapped.
+     * @return An OutTask object representing the mapped Task.
+     */
     fun toRepo(task: Task) = OutTask(
         id = task.id,
         uid = task.uid,
@@ -14,6 +36,12 @@ class TaskMapper {
         isDone = task.isDone
     )
 
+    /**
+     * Maps an OutTask data object from the repository layer to a Task data object for the repository layer.
+     *
+     * @param task The OutTask object to map.
+     * @return A Task object representing the same data.
+     */
     fun fromRepo(task: OutTask) = Task(
         id = task.id,
         uid = task.uid,
