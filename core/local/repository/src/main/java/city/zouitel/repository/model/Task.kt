309eb56918b2037e3d3a11d523cb1ebaf7d4d983
yon <bourntivoli@gmail.com>
2024-12-25 17:15:49 +0100

    feat: Add documentation for Task data class
    Added documentation for the `Task` data class, providing descriptions for its properties: `id`, `uid`, `
    item`, and `isDone`. This clarifies the purpose and usage of each property within the task model.

--- core/local/repository/src/main/java/city/zouitel/repository/model/Task.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Task.kt
@@ -2,6 +2,14 @@ package city.zouitel.repository.model
 
 import androidx.annotation.Keep
 
+/**
+ * Represents a single task in a to-do list.
+ *
+ * @property id The unique identifier for the task. Defaults to 0L for new, unsaved tasks.
+ * @property uid A unique identifier for the task that can be used across devices or databases, if needed. Defaults to an empty string.
+ * @property item The description or content of the task. Can be null if there's no specific description.
+ * @property isDone Indicates whether the task has been completed. Defaults to false.
+ */
 @Keep
 data class Task(
     val id: Long = 0L,
