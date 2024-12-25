eecaf32545e6fd7f7a33141861602a71e4355c81
yon <bourntivoli@gmail.com>
2024-12-25 17:15:40 +0100

    docs: Add documentation for Task entity
    Added documentation to the `Task` entity class, clarifying its purpose, properties, and usage within the Room database. This
     improves code readability and understanding.

--- core/local/database/src/main/java/city/zouitel/database/model/Task.kt
+++ core/local/database/src/main/java/city/zouitel/database/model/Task.kt
@@ -9,6 +9,21 @@ import city.zouitel.database.utils.Constants.ID
 import city.zouitel.database.utils.Constants.TITLE
 import city.zouitel.database.utils.Constants.UUID
 
+/**
+ * Represents a task entity in the database.
+ *
+ * This data class defines the schema for the "tasks_table" in the Room database.
+ * Each instance of this class represents a single task.
+ *
+ * @property id The unique identifier of the task. It serves as the primary key.
+ *              It's manually set and not auto-generated. Defaults to 0.
+ * @property uid A unique identifier (UUID) for the task, primarily used for synchronization or external referencing.
+ *              Defaults to an empty string.
+ * @property item The title or description of the task. Can be null. Defaults to "null".
+ * @property isDone Indicates whether the task is completed or not. Defaults to false.
+ *
+ * @constructor Creates a new task with the specified properties.
+ */
 @Entity(tableName = TABLE_NAME)
 data class Task(
     @PrimaryKey(autoGenerate = false)
