c7cc85455018f62a63ce76efb85acf28696f33e8
yon <bourntivoli@gmail.com>
2024-12-26 19:53:03 +0100

    feat: Add UiEvents and UpdateById UiEvent
    Added `UiEvents` sealed class to hold UI events, including `Insert`, `Update`, `Delete
    `, and `DeleteAll`.
    Added `UpdateById` to the `UiEvent` sealed class for updating data by ID.
    
    These changes introduce new UI event types and a mechanism for handling updates by ID.

--- common/logic/src/main/java/city/zouitel/logic/events/UiEvent.kt
+++ common/logic/src/main/java/city/zouitel/logic/events/UiEvent.kt
@@ -3,6 +3,14 @@ package city.zouitel.logic.events
 sealed class UiEvent<out T> {
     data class Insert<out T>(val data: T) : UiEvent<T>()
     data class Update<out T>(val data: T) : UiEvent<T>()
+    data class UpdateById<out T>(val data: T) : UiEvent<T>()
     data class Delete<out T>(val data: T) : UiEvent<T>()
     class DeleteAll<out T> : UiEvent<T>()
+}
+
+sealed class UiEvents {
+    data class Insert<out T>(val data: T) : UiEvents()
+    data class Update<out T>(val data: T) : UiEvents()
+    data class Delete<out T> (val data: T) : UiEvents()
+    class DeleteAll<out T> : UiEvents()
 }
