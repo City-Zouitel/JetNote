11024fa24de52dd73ebea2acece003b6fbb8e18f
yon <bourntivoli@gmail.com>
2024-12-26 19:54:15 +0100

    feat: Implement Task Update Functionality in Local Repository
    Added `update(task: Task)` function to `TaskRepositoryImpl` to update an existing Task in the
     local data source. This function leverages the existing mapper to convert the domain Task object to the data source representation and then delegates the update operation to the underlying data source.

--- core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/TaskRepositoryImpl.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/TaskRepositoryImpl.kt
@@ -65,6 +65,20 @@ class TaskRepositoryImpl(
     }
 
     /**
+     * Updates an existing [Task] in the data source.
+     *
+     * This function maps the domain [Task] to a data source-specific representation
+     * using the [mapper] and then delegates the update operation to the underlying
+     * [dataSource].
+     *
+     * @param task The [Task] object containing the updated information.
+     * @throws Exception if an error occurs during the update process in the data source.
+     */
+    override suspend fun update(task: Task) {
+        dataSource.update(mapper.fromDomain(task))
+    }
+
+    /**
      * Updates an entity in the data source by its ID.
      *
      * This function delegates the update operation to the underlying [dataSource].
