0f96a3d732b3d09421c462845a0acfcd9e19ea3e
yon <bourntivoli@gmail.com>
2024-12-29 21:47:42 +0100

    Refactor: Remove MediaMapper dependency from NoteMapper
    Removed the `MediaMapper` dependency and the mapping of `mediaEntities` from the `NoteMapper`.
     This suggests that media entities are no longer associated with notes in the domain model.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/NoteMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/NoteMapper.kt
@@ -1,24 +1,22 @@
 package city.zouitel.repository.mapper
 
+import city.zouitel.repository.model.Note
 import city.zouitel.domain.model.Note as OutNote
-import city.zouitel.repository.model.Note as InNote
 
 class NoteMapper(
     private val dataMapper: DataMapper,
     private val tagMapper: TagMapper,
     private val linkMapper: LinkMapper,
     private val audioMapper: AudioMapper,
-    private val mediaMapper: MediaMapper
 ) {
-    fun toDomain(notes: List<InNote>) = notes.map { toDomain(it) }
+    fun toDomain(notes: List<Note>) = notes.map { toDomain(it) }
 
-    fun toDomain(data: InNote): OutNote = with(data){
+    fun toDomain(data: Note): OutNote = with(data){
         OutNote(
             dataEntity = dataMapper.toDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.toDomain(it) },
             linkEntities = linkEntities.map { linkMapper.toDomain(it) },
-            audioEntities = audioEntities.map { audioMapper.toDomain(it) },
-            mediaEntities = mediaEntities.map { mediaMapper.toDomain(it) }
+            audioEntities = audioEntities.map { audioMapper.toDomain(it) }
         )
     }
 }
