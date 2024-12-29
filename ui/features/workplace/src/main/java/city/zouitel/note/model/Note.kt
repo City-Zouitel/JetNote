40f9e6133f040d8eb302c6a82d0e4a5af56854d0
yon <bourntivoli@gmail.com>
2024-12-29 21:49:40 +0100

    Refactor: Remove mediaEntities from Note model
    Removed the `mediaEntities` property from the `Note` data class. This change likely simplifies the Note model
     by removing a redundant or unused field for media entities.

--- ui/features/workplace/src/main/java/city/zouitel/note/model/Note.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/model/Note.kt
@@ -3,7 +3,6 @@ package city.zouitel.note.model
 import androidx.annotation.Keep
 import city.zouitel.audios.model.Audio
 import city.zouitel.links.model.Link
-import city.zouitel.media.model.Media
 import city.zouitel.tags.model.Tag
 
 @Keep
@@ -11,6 +10,5 @@ data class Note(
     val dataEntity: Data,
     val tagEntities: List<Tag>,
     val linkEntities: List<Link>,
-    val audioEntities: List<Audio>,
-    val mediaEntities: List<Media>
+    val audioEntities: List<Audio>
 )
