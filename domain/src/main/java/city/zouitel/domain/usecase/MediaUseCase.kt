e0c4683baf7ed5ed5484d7f751ab3331c73d9d03
yon <bourntivoli@gmail.com>
2024-12-29 21:49:56 +0100

    Refactor: Refactor MediaUseCase to use sealed class and data classes
    Refactored the `MediaUseCase` class to use a sealed class and data classes
     for each use case.
    
    This change improves code readability and type safety, making it easier to understand and maintain the different media-related operations.
    
    - Replaced individual classes with sealed class `MediaUseCase`.
    - Introduced data classes for `ObserveAll`, `ObserveByUid`, `Insert`, `UpdateMedia`,
     and `DeleteById`.
    - Updated function calls to use the new data class structure.
    - Updated `getAllMedias` to `observeAll`, `addMedia` to `insert`, and `deleteMedia` to `deleteById`.

--- domain/src/main/java/city/zouitel/domain/usecase/MediaUseCase.kt
+++ domain/src/main/java/city/zouitel/domain/usecase/MediaUseCase.kt
@@ -4,20 +4,23 @@ import city.zouitel.domain.model.Media
 import city.zouitel.domain.repository.MediaRepository
 
 sealed class MediaUseCase {
-
-    class GetAllMedias(private val repo: MediaRepository): MediaUseCase() {
-        operator fun invoke() = repo.getAllMedias
+    data class ObserveAll(private val repo: MediaRepository): MediaUseCase() {
+        operator fun invoke() = repo.observeAll
     }
 
-    class AddMedia(private val repo: MediaRepository): MediaUseCase() {
-        operator fun invoke(media: Media) = repo.addMedia(media)
+    data class ObserveByUid(private val repo: MediaRepository): MediaUseCase() {
+        operator fun invoke(uid: String) = repo.observeByUid(uid)
     }
 
-    class UpdateMedia(private val repo: MediaRepository): MediaUseCase() {
+    data class Insert(private val repo: MediaRepository): MediaUseCase() {
+        operator fun invoke(media: Media) = repo.insert(media)
+    }
+
+    data class UpdateMedia(private val repo: MediaRepository): MediaUseCase() {
         operator fun invoke(media: Media) = repo.updateMedia(media)
     }
 
-    class DeleteMedia(private val repo: MediaRepository): MediaUseCase() {
-        operator fun invoke(media: Media) = repo.deleteMedia(media)
+    data class DeleteById(private val repo: MediaRepository): MediaUseCase() {
+        operator fun invoke(id: Long) = repo.deleteById(id)
     }
 }
