bfda7687eafc11d70e4fff129eb9d676cd0d0a1b
yon <bourntivoli@gmail.com>
2024-12-29 21:49:26 +0100

    Refactor: Remove MediaMapper dependency from NoteMapper
    Removed the `MediaMapper` dependency and related mappings from the `NoteMapper`. This change suggests a simplification
     of note entity mapping by removing the media entity handling.

--- ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
+++ ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
@@ -2,20 +2,16 @@ package city.zouitel.screens.mapper
 
 import city.zouitel.audios.mapper.AudioMapper
 import city.zouitel.links.mapper.LinkMapper
-import city.zouitel.media.mapper.MediaMapper
 import city.zouitel.note.mapper.DataMapper
 import city.zouitel.note.model.Note
 import city.zouitel.tags.mapper.TagMapper
-import city.zouitel.tasks.mapper.TaskMapper
 import city.zouitel.domain.model.Note as OutNote
 
 class NoteMapper(
     private val dataMapper: DataMapper,
     private val tagMapper: TagMapper,
-    private val taskMapper: TaskMapper,
     private val linkMapper: LinkMapper,
-    private val audioMapper: AudioMapper,
-    private val mediaMapper: MediaMapper
+    private val audioMapper: AudioMapper
 ) {
     fun fromDomain(notes: List<OutNote>) = notes.map { fromDomain(it) }
 
@@ -24,8 +20,7 @@ class NoteMapper(
             dataEntity = dataMapper.fromDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.fromDomain(it) },
             linkEntities = linkEntities.map { linkMapper.fromDomain(it) },
-            audioEntities = audioEntities.map { audioMapper.fromDomain(it) },
-            mediaEntities = mediaEntities.map { mediaMapper.fromDomain(it) }
+            audioEntities = audioEntities.map { audioMapper.fromDomain(it) }
         )
     }
 }
