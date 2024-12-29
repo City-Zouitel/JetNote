de8e104d127d2c7e6c129b38872e918f80ab28bc
yon <bourntivoli@gmail.com>
2024-12-29 21:46:22 +0100

    Refactor: Update MediaScreenModel to use LogicFlow and UiEvents
    Refactored the `MediaScreenModel` to use `LogicFlow` and `
    UiEvents` for better state management and event handling.
    
    - Replaced `stateIn` with `LogicFlow` for managing the `allMedias` state.
    - Introduced `_observeByUid` and `observeByUid` to manage state based on uid.
    - Updated `sendUiEvent` to
     handle `UiEvents` for delete and insert operations.
    - Replaced direct calls to use cases with `LogicFlow` based event handling.
    - Initialized `_observeAll` and `_observeByUid` as `MutableStateFlow` with empty lists.

--- ui/features/media/src/main/java/city/zouitel/media/ui/MediaScreenModel.kt
+++ ui/features/media/src/main/java/city/zouitel/media/ui/MediaScreenModel.kt
@@ -3,44 +3,45 @@ package city.zouitel.media.ui
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.MediaUseCase
-import city.zouitel.logic.events.UiEvent
-import city.zouitel.logic.events.UiEventHandler
+import city.zouitel.logic.asLogicFlow
+import city.zouitel.logic.events.UiEvents
+import city.zouitel.logic.events.UiEventsHandler
 import city.zouitel.media.mapper.MediaMapper
 import city.zouitel.media.model.Media
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class MediaScreenModel(
-    getAllMedias: MediaUseCase.GetAllMedias,
-    private val addMedia: MediaUseCase.AddMedia,
-    private val deleteMedia: MediaUseCase.DeleteMedia,
+    _observeAll_: MediaUseCase.ObserveAll,
+    private val _observeByUid_: MediaUseCase.ObserveByUid,
+    private val insert: MediaUseCase.Insert,
+    private val deleteById: MediaUseCase.DeleteById,
     private val mapper: MediaMapper
-): ScreenModel, UiEventHandler<Media> {
+): ScreenModel, UiEventsHandler {
 
-    private val _allMedias: MutableStateFlow<List<Media>> = MutableStateFlow(
-        emptyList()
-    )
+    private val _observeAll: MutableStateFlow<List<Media>> = MutableStateFlow(emptyList())
+    val observeAll: StateFlow<List<Media>> = _observeAll
+        .asLogicFlow(emptyList()) {
+            performUiEvent {
+                _observeAll_().collect { _observeAll.value = mapper.fromDomain(it) }
+            }
+        }
 
-    val allMedias: StateFlow<List<Media>> = _allMedias.stateIn(
-            screenModelScope,
-            SharingStarted.WhileSubscribed(),
-            listOf()
-        )
+    private val _observeByUid: MutableStateFlow<List<Media>> = MutableStateFlow(emptyList())
+    val observeByUid: StateFlow<List<Media>> = _observeByUid.asLogicFlow(emptyList())
 
-    init {
+    fun initializeUid(uid: String) {
         performUiEvent {
-            getAllMedias().collect { audios -> _allMedias.value = mapper.fromDomain(audios) }
+            _observeByUid_(uid).collect { _observeByUid.value = mapper.fromDomain(it) }
         }
     }
 
-    override fun sendUiEvent(event: UiEvent<Media>) {
+    override fun sendUiEvent(event: UiEvents) {
         when(event) {
-            is UiEvent.Delete -> performUiEvent { deleteMedia(mapper.toDomain(event.data)) }
-            is UiEvent.Insert -> performUiEvent { addMedia(mapper.toDomain(event.data)) }
+            is UiEvents.Delete<*> -> performUiEvent { deleteById(event.data as Long) }
+            is UiEvents.Insert<*> -> performUiEvent { insert(mapper.toDomain(event.data as Media)) }
             else -> throw Exception("Not Implemented!")
         }
     }
