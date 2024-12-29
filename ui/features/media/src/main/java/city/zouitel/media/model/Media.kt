9a825ece040eff9645d51749a600dc2ef9803743
yon <bourntivoli@gmail.com>
2024-12-29 21:43:34 +0100

    feat: Add uid property to Media model
    Added a new property `uid` of type `String` to the `Media` data class.
    Also
    , changed the default values of the properties to use constants from `Constants` object.
    Finally, renamed the `path` property to `uri`.

--- ui/features/media/src/main/java/city/zouitel/media/model/Media.kt
+++ ui/features/media/src/main/java/city/zouitel/media/model/Media.kt
@@ -1,10 +1,14 @@
 package city.zouitel.media.model
 
 import androidx.annotation.Keep
+import city.zouitel.domain.utils.Constants.DEFAULT_BOOLEAN
+import city.zouitel.domain.utils.Constants.DEFAULT_NUM
+import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
 @Keep
 data class Media(
-    val id: Long = 0,
-    val isVideo: Boolean = false,
-    val path: String = ""
+    val id: Long = DEFAULT_NUM,
+    val uid: String = DEFAULT_TXT,
+    val isVideo: Boolean = DEFAULT_BOOLEAN,
+    val uri: String = DEFAULT_TXT
 )
