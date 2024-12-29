5d97dda2eed51873368b6ae78a26ee675dbfaf02
yon <bourntivoli@gmail.com>
2024-12-29 21:51:12 +0100

    Refactor: Update MediaMapper to use uri instead of path
    Updated `MediaMapper` to use `uri` instead of `path` for media representation.
    
    - Added `uid` property to both `Media` and `OutMedia` models.
    - Renamed `path` to `uri` in `OutMedia` model.
    - Updated `toDomain` and `fromDomain` functions accordingly to reflect these changes.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/MediaMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/MediaMapper.kt
@@ -1,21 +1,23 @@
 package city.zouitel.repository.mapper
 
-import city.zouitel.repository.model.Media as InMedia
+import city.zouitel.repository.model.Media
 import city.zouitel.domain.model.Media as OutMedia
 
 class MediaMapper {
 
-    fun toDomain(medias: List<InMedia>) = medias.map { toDomain(it) }
+    fun toDomain(medias: List<Media>) = medias.map { toDomain(it) }
 
-    fun toDomain(media: InMedia) = OutMedia(
+    fun toDomain(media: Media) = OutMedia(
         id = media.id,
+        uid = media.uid,
         isVideo = media.isVideo,
-        path = media.path
+        uri = media.path
     )
 
-    fun fromDomain(media: OutMedia) = InMedia(
+    fun fromDomain(media: OutMedia) = Media(
         id = media.id,
+        uid = media.uid,
         isVideo = media.isVideo,
-        path = media.path
+        path = media.uri
     )
 }
