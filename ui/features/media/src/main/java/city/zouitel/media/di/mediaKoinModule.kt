de45ee3bd28935d43c74ffc7b02f539bd16f6d12
yon <bourntivoli@gmail.com>
2024-12-29 21:51:27 +0100

    Refactor: Remove NoteAndMedia related dependencies from media module
    Removed the `NoteAndMediaScreenModel` and `NoteAndMediaMapper` dependencies from the
     `mediaKoinModule`. This change likely isolates the media module from note-and-media specific logic.

--- ui/features/media/src/main/java/city/zouitel/media/di/mediaKoinModule.kt
+++ ui/features/media/src/main/java/city/zouitel/media/di/mediaKoinModule.kt
@@ -1,14 +1,11 @@
 package city.zouitel.media.di
 
+import city.zouitel.media.mapper.MediaMapper
+import city.zouitel.media.ui.MediaScreenModel
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
-import city.zouitel.media.mapper.*
-import city.zouitel.media.ui.*
 
 val mediaKoinModule = module {
-
     factoryOf(::MediaScreenModel)
-    factoryOf(::NoteAndMediaScreenModel)
     factoryOf(::MediaMapper)
-    factoryOf(::NoteAndMediaMapper)
 }
