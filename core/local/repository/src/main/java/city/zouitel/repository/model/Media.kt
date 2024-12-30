f7d632c4c8e85559c1d84faba6530936e4427d0e
yon <bourntivoli@gmail.com>
2024-12-30 17:06:49 +0100

    Refactor: Update Media data class with default values and documentation
    Updated the `Media` data class to include default values for its properties and added documentation.
    
    - Default values are now provided for `id`, `uid`, `isVideo`, and `uri` using constants from `Constants`.
    - `path` property was renamed to `uri` to better reflect its purpose.
    - Comprehensive documentation was added to explain the purpose and properties of the `Media` class.
    
    This change improves code readability and maintainability.

--- core/local/repository/src/main/java/city/zouitel/repository/model/Media.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Media.kt
@@ -1,11 +1,26 @@
 package city.zouitel.repository.model
 
 import androidx.annotation.Keep
+import city.zouitel.domain.utils.Constants.DEFAULT_BOOLEAN
+import city.zouitel.domain.utils.Constants.DEFAULT_NUM
+import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
+/**
+ * Represents a media item, such as an image or video, stored locally.
+ *
+ * @property id A unique identifier for this media item. Defaults to [DEFAULT_NUM].
+ * @property uid A unique identifier that may be assigned externally or by a remote system for the media item. Defaults to [DEFAULT_TXT].
+ * @property isVideo `true` if this media item is a video, `false` if it is an image. Defaults to [DEFAULT_BOOLEAN].
+ * @property uri The file uri to the media item on the device's storage. Defaults to [DEFAULT_TXT].
+ *
+ * @see DEFAULT_NUM
+ * @see DEFAULT_TXT
+ * @see DEFAULT_BOOLEAN
+ */
 @Keep
 data class Media(
-    val id: Long = 0,
-    val uid: String = "",
-    val isVideo: Boolean = false,
-    val path: String = ""
+    val id: Long = DEFAULT_NUM,
+    val uid: String = DEFAULT_TXT,
+    val isVideo: Boolean = DEFAULT_BOOLEAN,
+    val uri: String = DEFAULT_TXT
 )
