56275bf7c388c49da5ab2628149a1e4ec74a113f
yon <bourntivoli@gmail.com>
2024-12-30 17:08:21 +0100

    Refactor: Update MediaMapper and rename path to uri
    Updated `MediaMapper` to map between `Media` (repo module) and `OutMedia`
     (domain layer) objects.
    
    - Renamed `path` to `uri` in both `Media` and `OutMedia` models.
    - Added documentation for the mapping functions.
    - Improved code clarity and consistency.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/MediaMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/MediaMapper.kt
@@ -3,21 +3,42 @@ package city.zouitel.repository.mapper
 import city.zouitel.repository.model.Media
 import city.zouitel.domain.model.Media as OutMedia
 
+/**
+ * A mapper class responsible for converting between [Media] (repo module) and [OutMedia] (domain layer) objects.
+ */
 class MediaMapper {
 
+    /**
+     * Maps a list of Reminder data objects to a list of domain models.
+     *
+     * @param medias The list of Media data objects to map.
+     * @return A list of domain models representing the given medias.
+     */
     fun toDomain(medias: List<Media>) = medias.map { toDomain(it) }
 
+    /**
+     * Maps a Media data object to an OutMedia domain object.
+     *
+     * @param media The Media data object to map.
+     * @return An OutMedia domain object representing the same media.
+     */
     fun toDomain(media: Media) = OutMedia(
         id = media.id,
         uid = media.uid,
         isVideo = media.isVideo,
-        uri = media.path
+        uri = media.uri
     )
 
+    /**
+     * Maps an OutMedia (domain model) to a Media (data model).
+     *
+     * @param media The domain model representation of a media.
+     * @return The data model representation of the media.
+     */
     fun fromDomain(media: OutMedia) = Media(
         id = media.id,
         uid = media.uid,
         isVideo = media.isVideo,
-        path = media.uri
+        uri = media.uri
     )
 }
