c61bae4fb634a19129714a3d8b890d3351a18a4c
yon <bourntivoli@gmail.com>
2024-12-30 17:09:17 +0100

    Refactor: Introduce Sealed Class for Media Use Cases
    Refactored `MediaUseCase` into a sealed class to represent different media operations.
    
    This change
     improves code organization and type safety by explicitly defining each use case as a subclass. Each subclass, such as `ObserveAll`, `ObserveByUid`, `Insert`, and `DeleteById`, encapsulates a specific media-related action and provides an `invoke` operator for execution.
    
    Additionally, detailed KDoc documentation has
     been added for each use case, enhancing code clarity and maintainability.

--- domain/src/main/java/city/zouitel/domain/usecase/MediaUseCase.kt
+++ domain/src/main/java/city/zouitel/domain/usecase/MediaUseCase.kt
@@ -3,23 +3,58 @@ package city.zouitel.domain.usecase
 import city.zouitel.domain.model.Media
 import city.zouitel.domain.repository.MediaRepository
 
+/**
+ * Sealed class representing different use cases for interacting with media data.
+ * Each subclass represents a specific operation that can be performed on the media repository.
+ *
+ * This class promotes type safety and allows for exhaustive when statements when handling
+ * different media operations.
+ */
 sealed class MediaUseCase {
+    /**
+     * [ObserveAll] is a use case that provides a stream of all media items from the [MediaRepository].
+     *
+     * This use case is designed to observe changes in the entire media collection.
+     * It retrieves a Flow that emits a list of [Media] objects whenever the underlying data in the [MediaRepository] is updated.
+     *
+     * @property repo The [MediaRepository] used as the data source for media items.
+     * @constructor Creates an instance of [ObserveAll] with the provided [MediaRepository].
+     *
+     * @return A Flow emitting a list of all [Media] items.
+     */
     data class ObserveAll(private val repo: MediaRepository): MediaUseCase() {
         operator fun invoke() = repo.observeAll
     }
 
+    /**
+     * `ObserveByUid` is a [MediaUseCase] that provides a stream of medias associated with a given UID.
+     *
+     * This use case leverages the [MediaRepository] to observe changes to media items filtered by a specific UID.
+     *
+     * @property repo The [MediaRepository] used to interact with the underlying data source.
+     */
     data class ObserveByUid(private val repo: MediaRepository): MediaUseCase() {
         operator fun invoke(uid: String) = repo.observeByUid(uid)
     }
 
+    /**
+     * [Insert] is a [MediaUseCase] responsible for inserting a new [Media] item into the data source.
+     *
+     * This use case delegates the actual insertion operation to the provided [MediaRepository].
+     *
+     * @property repo The [MediaRepository] instance used to interact with the media data source.
+     */
     data class Insert(private val repo: MediaRepository): MediaUseCase() {
         operator fun invoke(media: Media) = repo.insert(media)
     }
 
-    data class UpdateMedia(private val repo: MediaRepository): MediaUseCase() {
-        operator fun invoke(media: Media) = repo.updateMedia(media)
-    }
-
+    /**
+     * [MediaUseCase] implementation to delete a media item by its ID.
+     *
+     * This use case delegates the actual deletion to the provided [MediaRepository].
+     *
+     * @property repo The [MediaRepository] used for data access.
+     */
     data class DeleteById(private val repo: MediaRepository): MediaUseCase() {
         operator fun invoke(id: Long) = repo.deleteById(id)
     }
