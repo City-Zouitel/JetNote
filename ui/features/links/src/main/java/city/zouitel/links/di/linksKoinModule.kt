c3beb0bc80366d9f67e108274e44af9f7bdba789
yon <bourntivoli@gmail.com>
2025-01-09 21:30:38 +0100

    Refactor: Simplify Links Koin Module
    - Removed `NoteAndLinkMapper` and `NoteAndLinkScreenModel` from the Koin module.
    
    - Updated `LinkScreenModel` dependencies.
    - Updated `LinkWorker` dependencies.
    These changes likely reflect a simplification or refactoring of how links and notes are handled within the app, potentially removing the need for a separate mapper and screen model.

--- ui/features/links/src/main/java/city/zouitel/links/di/linksKoinModule.kt
+++ ui/features/links/src/main/java/city/zouitel/links/di/linksKoinModule.kt
@@ -1,27 +1,21 @@
 package city.zouitel.links.di
 
-import org.koin.dsl.module
-import city.zouitel.links.ui.*
-import city.zouitel.links.mapper.*
+import city.zouitel.links.mapper.LinkMapper
+import city.zouitel.links.ui.LinkScreenModel
 import city.zouitel.links.worker.LinkWorker
-import city.zouitel.systemDesign.CommonConstants.LINK_DIR
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.workmanager.dsl.worker
 import org.koin.core.module.dsl.factoryOf
-import org.koin.core.qualifier.named
+import org.koin.dsl.module
 
 val linksKoinModule = module {
     factoryOf(::LinkMapper)
-    factoryOf(::NoteAndLinkMapper)
 
     factory {
-        LinkScreenModel(get(), get(), get(), get())
-    }
-    factory {
-        NoteAndLinkScreenModel(get(), get(), get())
+        LinkScreenModel(get(), get(), get(), get(), get())
     }
 
     worker {
-        LinkWorker(androidContext(), get(), get(), get(), get(), get(), get())
+        LinkWorker(androidContext(), get(), get(), get(), get())
     }
 }
