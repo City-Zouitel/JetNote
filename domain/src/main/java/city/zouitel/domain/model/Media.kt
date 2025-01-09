17a7eadb8cdc372d02818b264599dc690d9aa2f5
yon <bourntivoli@gmail.com>
2025-01-09 21:49:11 +0100

    Refactor: Change Media id type to Long
    Changed the `id` field in the `Media` data class from `DEFAULT_NUM` to `DEFAULT
    _LONG`. This aligns the id to be a Long type which can avoid problems while using JPA repositories.

--- domain/src/main/java/city/zouitel/domain/model/Media.kt
+++ domain/src/main/java/city/zouitel/domain/model/Media.kt
@@ -2,24 +2,24 @@ package city.zouitel.domain.model
 
 import androidx.annotation.Keep
 import city.zouitel.domain.utils.Constants.DEFAULT_BOOLEAN
-import city.zouitel.domain.utils.Constants.DEFAULT_NUM
+import city.zouitel.domain.utils.Constants.DEFAULT_LONG
 import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
 /**
  * Represents a media item, such as an image or video, stored locally.
  *
- * @property id A unique identifier for this media item. Defaults to [DEFAULT_NUM].
+ * @property id A unique identifier for this media item. Defaults to [DEFAULT_LONG].
  * @property uid A unique identifier that may be assigned externally or by a remote system for the media item. Defaults to [DEFAULT_TXT].
  * @property isVideo `true` if this media item is a video, `false` if it is an image. Defaults to [DEFAULT_BOOLEAN].
  * @property uri The file uri to the media item on the device's storage. Defaults to [DEFAULT_TXT].
  *
- * @see DEFAULT_NUM
+ * @see DEFAULT_LONG
  * @see DEFAULT_TXT
  * @see DEFAULT_BOOLEAN
  */
 @Keep
 data class Media(
-    val id: Long = DEFAULT_NUM,
+    val id: Long = DEFAULT_LONG,
     val uid: String = DEFAULT_TXT,
     val isVideo: Boolean = DEFAULT_BOOLEAN,
     val uri: String = DEFAULT_TXT
