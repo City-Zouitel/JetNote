77f7b37d9671d5361cf6a3c690b2c1c82d704463
yon <bourntivoli@gmail.com>
2025-01-09 21:28:49 +0100

    chore: Update app version to 7.1.0
    Updated the app version to 7.1.0 in `CommonConstants`.
    - Updated
     `APP_VERSION` constant in `CommonConstants` from "7.0.3" to "7.1.0".

--- common/systemDesign/src/main/java/city/zouitel/systemDesign/CommonConstants.kt
+++ common/systemDesign/src/main/java/city/zouitel/systemDesign/CommonConstants.kt
@@ -2,7 +2,7 @@ package city.zouitel.systemDesign
 
 
 object CommonConstants {
-    const val APP_VERSION = "7.0.3"
+    const val APP_VERSION = "7.1.0"
     const val APP_NAME = "JetNote"
     const val NONE = "unspecified"
 
