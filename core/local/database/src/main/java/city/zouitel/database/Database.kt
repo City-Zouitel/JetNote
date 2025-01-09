177b980f9228db2634eca1932cb64545ec5c4277
yon <bourntivoli@gmail.com>
2025-01-09 21:29:26 +0100

    feat: Update database version to 4 and remove NoteAndLink entity
    Updated the database version to 4 in `Database.kt`.
    Removed `Note
    AndLinkEntity` and `NoteAndLinkDao`.
    This change implies that the database schema has been updated, and the NoteAndLink feature or entity has been removed.

--- core/local/database/src/main/java/city/zouitel/database/Database.kt
+++ core/local/database/src/main/java/city/zouitel/database/Database.kt
@@ -7,7 +7,6 @@ import city.zouitel.database.dao.DataDao
 import city.zouitel.database.dao.LinkDao
 import city.zouitel.database.dao.MediaDao
 import city.zouitel.database.dao.NoteAndAudioDao
-import city.zouitel.database.dao.NoteAndLinkDao
 import city.zouitel.database.dao.NoteAndTagDao
 import city.zouitel.database.dao.NoteDao
 import city.zouitel.database.dao.ReminderDao
@@ -16,25 +15,23 @@ import city.zouitel.database.dao.TaskDao
 import city.zouitel.database.dao.WidgetDao
 import city.zouitel.database.model.AudioEntity
 import city.zouitel.database.model.DataEntity
-import city.zouitel.database.model.LinkEntity
+import city.zouitel.database.model.Link
 import city.zouitel.database.model.Media
 import city.zouitel.database.model.NoteAndAudioEntity
-import city.zouitel.database.model.NoteAndLinkEntity
 import city.zouitel.database.model.NoteAndTagEntity
 import city.zouitel.database.model.Reminder
 import city.zouitel.database.model.TagEntity
 import city.zouitel.database.model.Task
 
 @Database(
-    version = 3,
+    version = 4,
     autoMigrations = [],
     entities = [
         DataEntity::class,
         TagEntity::class,
         NoteAndTagEntity::class,
         Task::class,
-        LinkEntity::class,
-        NoteAndLinkEntity::class,
+        Link::class,
         AudioEntity::class,
         NoteAndAudioEntity::class,
         Media::class,
@@ -50,7 +47,6 @@ abstract class Database: RoomDatabase() {
     abstract fun getWidgetEntityDao(): WidgetDao
     abstract fun getTaskDao(): TaskDao
     abstract fun getLinkDao(): LinkDao
-    abstract fun getNoteAndLinkDao(): NoteAndLinkDao
     abstract fun getAudioDao(): AudioDao
     abstract fun getNoteAndAudioDao(): NoteAndAudioDao
     abstract fun getMediaDao(): MediaDao
