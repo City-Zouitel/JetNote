864ed9f85dec91e67aa1100f720b09909eaaa5da
yon <bourntivoli@gmail.com>
2025-01-09 21:49:21 +0100

    Refactor: Use DEFAULT_LONG for Media ID
    Replaced `DEFAULT_NUM` with `DEFAULT_LONG` as the default value for `id`
     in the `Media` data class. This change ensures consistency and uses the correct default value for long type.

--- ui/features/media/src/main/java/city/zouitel/media/model/Media.kt
+++ ui/features/media/src/main/java/city/zouitel/media/model/Media.kt
@@ -2,12 +2,12 @@ package city.zouitel.media.model
 
 import androidx.annotation.Keep
 import city.zouitel.domain.utils.Constants.DEFAULT_BOOLEAN
-import city.zouitel.domain.utils.Constants.DEFAULT_NUM
+import city.zouitel.domain.utils.Constants.DEFAULT_LONG
 import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
 @Keep
 data class Media(
-    val id: Long = DEFAULT_NUM,
+    val id: Long = DEFAULT_LONG,
     val uid: String = DEFAULT_TXT,
     val isVideo: Boolean = DEFAULT_BOOLEAN,
     val uri: String = DEFAULT_TXT
