6b27e9fbfcae6bcd7e727ef0c66d4af58420b5bd
yon <bourntivoli@gmail.com>
2025-01-09 21:53:43 +0100

    Refactor: Remove LinkMapper from WidgetMapper
    Removed the `LinkMapper` dependency and related logic from `WidgetMapper`. The `linkEntities` mapping is
     no longer performed in this class.
    
    This change simplifies the `WidgetMapper` by removing unused dependency and mapping logic.

--- ui/features/widget/src/main/java/city/zouitel/widget/mapper/WidgetMapper.kt
+++ ui/features/widget/src/main/java/city/zouitel/widget/mapper/WidgetMapper.kt
@@ -1,16 +1,13 @@
 package city.zouitel.widget.mapper
 
-import city.zouitel.links.mapper.LinkMapper
 import city.zouitel.note.mapper.DataMapper
 import city.zouitel.tags.mapper.TagMapper
-import city.zouitel.tasks.mapper.TaskMapper
 import city.zouitel.widget.model.WidgetNote
 import city.zouitel.domain.model.Note as OutNote
 
 class WidgetMapper(
     private val dataMapper: DataMapper,
     private val tagMapper: TagMapper,
-    private val linkMapper: LinkMapper
 ) {
     fun fromDomain(notes: List<OutNote>) = notes.map { fromDomain(it) }
 
@@ -18,7 +15,6 @@ class WidgetMapper(
         WidgetNote(
             dataEntity = dataMapper.fromDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.fromDomain(it) },
-            linkEntities = linkEntities.map { linkMapper.fromDomain(it) }
         )
     }
 }
