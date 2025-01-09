eff618ca04a43c212547c2b962b084ae3685b529
yon <bourntivoli@gmail.com>
2025-01-09 21:43:29 +0100

    Refactor: Use Upsert and remove redundant queries in LinkDao
    - Replaced `@Insert` with `@Upsert` for adding/updating `Link`
     entities, simplifying the logic.
    - Removed `getLinkById` and `deleteLink` queries and replaced them with `observeByUid` and `deleteByUid` respectively to improve query efficiency and clarity.
    - Updated the `observeAll` query to use a getter.
    - Modified all queries to use
     a constant `TABLE_NAME` and `UUID` for column names.

--- core/local/database/src/main/java/city/zouitel/database/dao/LinkDao.kt
+++ core/local/database/src/main/java/city/zouitel/database/dao/LinkDao.kt
@@ -1,26 +1,25 @@
 package city.zouitel.database.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import city.zouitel.database.model.LinkEntity
-import city.zouitel.database.utils.Constants
+import androidx.room.Upsert
+import city.zouitel.database.model.Link
+import city.zouitel.database.model.Link.Companion.TABLE_NAME
+import city.zouitel.database.utils.Constants.UUID
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface LinkDao {
 
-    @Query("SELECT * FROM LINKS_TABLE")
-    fun getAllLinks(): Flow<List<LinkEntity>>
+    @get:Query("SELECT * FROM $TABLE_NAME")
+    val observeAll: Flow<List<Link>>
 
-    @Query("SELECT * FROM LINKS_TABLE WHERE ${Constants.ID} = :id")
-    suspend fun getLinkById(id: Long): LinkEntity?
+    @Query("SELECT * FROM $TABLE_NAME WHERE $UUID = :uid")
+    fun observeByUid(uid: String): Flow<List<Link>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addLink(link: LinkEntity)
+    @Upsert(entity = Link::class)
+    suspend fun insert(link: Link)
 
-    @Delete
-    suspend fun deleteLink(link: LinkEntity)
+    @Query("DELETE FROM $TABLE_NAME WHERE $UUID = :uid")
+    suspend fun deleteByUid(uid: String)
 }
