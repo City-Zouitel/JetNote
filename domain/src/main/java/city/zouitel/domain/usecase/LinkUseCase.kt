3ad34bef885d8ffdf19944ad5c6a2ad9301feedf
yon <bourntivoli@gmail.com>
2025-01-09 21:48:31 +0100

    Refactor: Update LinkUseCase to use data classes and new repo methods
    Replaced the anonymous classes in `LinkUseCase` with data classes. Updated use case
     methods to use `observeAll`, `observeByUid`, `insert`, and `deleteByUid` from the repository.
    
    - Replaced `GetAllLinks` with `ObserveAll`.
    - Added `ObserveByUid` for observing a link by uid.
    - Replaced `AddLink` with `Insert
    `.
    - Replaced `DeleteLink` with `DeleteByUid`.

--- domain/src/main/java/city/zouitel/domain/usecase/LinkUseCase.kt
+++ domain/src/main/java/city/zouitel/domain/usecase/LinkUseCase.kt
@@ -4,21 +4,19 @@ import city.zouitel.domain.model.Link
 import city.zouitel.domain.repository.LinkRepository
 
 sealed class LinkUseCase {
-    class GetAllLinks(
-        private val repository: LinkRepository
-    ): LinkUseCase() {
-        operator fun invoke() = repository.getAllLinks
+    data class ObserveAll(private val repo: LinkRepository) : LinkUseCase() {
+        operator fun invoke() = repo.observeAll
     }
 
-    class AddLink(
-        private val repository: LinkRepository
-    ): LinkUseCase() {
-        suspend operator fun invoke(link: Link) = repository.addLink(link)
+    data class ObserveByUid(private val repo: LinkRepository) : LinkUseCase() {
+        suspend operator fun invoke(uid: String) = repo.observeByUid(uid)
     }
 
-    class DeleteLink(
-        private val repository: LinkRepository
-    ): LinkUseCase() {
-        suspend operator fun invoke(link: Link) = repository.deleteLink(link)
+    data class Insert(private val repo: LinkRepository): LinkUseCase() {
+        suspend operator fun invoke(link: Link) = repo.insert(link)
+    }
+
+    data class DeleteByUid(private val repo: LinkRepository) : LinkUseCase() {
+        suspend operator fun invoke(uid: String) = repo.deleteByUid(uid)
     }
 }
