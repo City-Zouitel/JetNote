14fc8b9adf09ef25933329d729c590ae949a9ec9
yon <bourntivoli@gmail.com>
2025-01-09 21:43:41 +0100

    Refactor: Improve LinkDataSource with Flow and Uid
    - Renamed `getAllLinks` to `observeAll` to better reflect its reactive nature as a
     Flow.
    - Added `observeByUid` to fetch links based on uid as a Flow.
    - Renamed `addLink` to `insert` for clarity and consistency with other data sources.
    - Changed `deleteLink` to `deleteByUid` to delete link by uid.
    - Updated m
    appers usage in `observeAll` and `observeByUid`.

--- core/local/database/src/main/java/city/zouitel/database/datasourceImpl/LinkDataSourceImpl.kt
+++ core/local/database/src/main/java/city/zouitel/database/datasourceImpl/LinkDataSourceImpl.kt
@@ -3,22 +3,26 @@ package city.zouitel.database.datasourceImpl
 import city.zouitel.database.dao.LinkDao
 import city.zouitel.database.mapper.LinkMapper
 import city.zouitel.repository.datasource.LinkDataSource
-import city.zouitel.repository.model.Link as OutLink
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import city.zouitel.repository.model.Link as OutLink
 
 class LinkDataSourceImpl(
     private val dao: LinkDao,
     private val mapper: LinkMapper
 ): LinkDataSource {
-    override val getAllLinks: Flow<List<OutLink>>
-        get() = dao.getAllLinks().map { links -> mapper.toRepo(links) }
+    override val observeAll: Flow<List<OutLink>>
+        get() = dao.observeAll.map { mapper.toRepo(it) }
 
-    override suspend fun addLink(link: OutLink) {
-        dao.addLink(mapper.fromRepo(link))
+    override suspend fun observeByUid(uid: String): Flow<List<OutLink>> {
+        return dao.observeByUid(uid).map { mapper.toRepo(it) }
     }
 
-    override suspend fun deleteLink(link: OutLink) {
-        dao.deleteLink(mapper.fromRepo(link))
+    override suspend fun insert(link: OutLink) {
+        dao.insert(mapper.fromRepo(link))
+    }
+
+    override suspend fun deleteByUid(uid: String) {
+        dao.deleteByUid(uid)
     }
 }
