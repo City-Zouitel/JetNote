c4cd0fb385cdb1121ba84bf3c7f0d092acc22a24
yon <bourntivoli@gmail.com>
2025-01-09 21:29:12 +0100

    Refactor: Rename and add default constants
    Renamed `DEFAULT_NUM` to `DEFAULT_LONG` and added `DEFAULT_INT` with value
    0 to `Constants`. This change improves clarity and provides more specific default values.

--- domain/src/main/java/city/zouitel/domain/utils/Constants.kt
+++ domain/src/main/java/city/zouitel/domain/utils/Constants.kt
@@ -3,7 +3,8 @@ package city.zouitel.domain.utils
 object Constants {
     const val NON = "non_specified"
 
-    const val DEFAULT_NUM = 0L
+    const val DEFAULT_LONG = 0L
+    const val DEFAULT_INT = 0
     const val DEFAULT_BOOLEAN = false
     const val DEFAULT_TXT = ""
 }
