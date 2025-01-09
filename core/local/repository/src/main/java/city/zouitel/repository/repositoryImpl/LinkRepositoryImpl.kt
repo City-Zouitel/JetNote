b6b750545bdde38a875cde46f0544236e3ca2fda
yon <bourntivoli@gmail.com>
2025-01-09 21:44:06 +0100

    Refactor: Rename and improve LinkRepository methods
    - Renamed `getAllLinks` to `observeAll` in `LinkRepository` and `LinkRepositoryImpl
    ` for better clarity on its reactive nature.
    - Added `observeByUid` function to observe links by their UID.
    - Renamed `addLink` to `insert` in both interfaces for more standard naming.
    - Renamed `deleteLink` to `deleteByUid` in both interfaces and data
     sources, and now it takes UID as an argument for more precise deletion.
    - Updated mappers in the repository implementation.

--- core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/LinkRepositoryImpl.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/LinkRepositoryImpl.kt
@@ -1,24 +1,28 @@
 package city.zouitel.repository.repositoryImpl
 
-import city.zouitel.domain.model.Link as OutLink
 import city.zouitel.domain.repository.LinkRepository
 import city.zouitel.repository.datasource.LinkDataSource
 import city.zouitel.repository.mapper.LinkMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import city.zouitel.domain.model.Link as OutLink
 
 class LinkRepositoryImpl(
     private val dataSource: LinkDataSource,
     private val mapper: LinkMapper
 ): LinkRepository {
-    override val getAllLinks: Flow<List<OutLink>>
-        get() = dataSource.getAllLinks.map { links -> mapper.toDomain(links) }
+    override val observeAll: Flow<List<OutLink>>
+        get() = dataSource.observeAll.map { mapper.toDomain(it) }
 
-    override suspend fun addLink(link: OutLink) {
-        dataSource.addLink(mapper.fromDomain(link))
+    override suspend fun observeByUid(uid: String): Flow<List<OutLink>> {
+        return dataSource.observeByUid(uid).map { mapper.toDomain(it) }
     }
 
-    override suspend fun deleteLink(link: OutLink) {
-        dataSource.deleteLink(mapper.fromDomain(link))
+    override suspend fun insert(link: OutLink) {
+        dataSource.insert(mapper.fromDomain(link))
+    }
+
+    override suspend fun deleteByUid(uid: String) {
+        dataSource.deleteByUid(uid)
     }
 }
