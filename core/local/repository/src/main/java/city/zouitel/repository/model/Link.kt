23f174ff11655a16ffa8bdef2e4ad2a1e694f67a
yon <bourntivoli@gmail.com>
2025-01-09 21:30:14 +0100

    Refactor: Update Link data class properties
    Changed the properties of the `Link` data class.
    - Replaced `Long` id with `Int`
     id.
    - Changed all `String` properties to use `Constants.DEFAULT_TXT` as default value.
    - Renamed `val host` to `val uid`.
    - Removed nullable types.

--- core/local/repository/src/main/java/city/zouitel/repository/model/Link.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Link.kt
@@ -1,13 +1,16 @@
 package city.zouitel.repository.model
 
 import androidx.annotation.Keep
+import city.zouitel.domain.utils.Constants.DEFAULT_INT
+import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
 @Keep
 data class Link(
-     var id: Long = 0L,
-     var url: String = "",
-     var host: String = "",
-     var image: String? = "",
-     val title: String? = "",
-     var description: String? = ""
+     val id: Int = DEFAULT_INT,
+     val uid: String = DEFAULT_TXT,
+     val url: String = DEFAULT_TXT,
+     val title: String = DEFAULT_TXT,
+     val description: String = DEFAULT_TXT,
+     val image: String = DEFAULT_TXT,
+     val icon: String = DEFAULT_TXT
 )
