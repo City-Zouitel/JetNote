bb08cb1b8f8e595f757f5640b6a387be3798503f
yon <bourntivoli@gmail.com>
2025-01-09 21:53:36 +0100

    Refactor: Remove link mapper from WidgetMapper
    Removed the `linkMapper` from the `WidgetMapper` class. The `linkEntities` mapping is also
     removed in the `toDomain` function. This change indicates that `WidgetMapper` no longer handles link entities.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/WidgetMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/WidgetMapper.kt
@@ -6,7 +6,6 @@ import city.zouitel.repository.model.Note as InNote
 class WidgetMapper(
     private val dataMapper: DataMapper,
     private val tagMapper: TagMapper,
-    private val linkMapper: LinkMapper,
     private val audioMapper: AudioMapper
 ) {
     fun toDomain(notes: List<InNote>) = notes.map { toDomain(it) }
@@ -15,7 +14,6 @@ class WidgetMapper(
         OutNote(
             dataEntity = dataMapper.toDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.toDomain(it) },
-            linkEntities = linkEntities.map { linkMapper.toDomain(it) },
             audioEntities = audioEntities.map { audioMapper.toDomain(it) }
         )
     }
