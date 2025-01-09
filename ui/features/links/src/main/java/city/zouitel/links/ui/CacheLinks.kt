1d0eda5033f45c8a6ab5c0ea42842888c661cedd
yon <bourntivoli@gmail.com>
2025-01-09 21:27:23 +0100

    Refactor: Cache link data using MetaProbe and coroutines
    Replaced the composable-based `CacheLinks` with a function `cacheLink` that
     uses MetaProbe to fetch link metadata within a coroutine scope. The new approach caches link details (title, description, image, icon, and url) asynchronously using `MetaProbe`, then stores them using `LinkScreenModel`.
    Additionally, `DEFAULT_TXT` has been added to handle cases with null title,
     description or images.
    The URL hashcode is used as an ID.

--- ui/features/links/src/main/java/city/zouitel/links/ui/CacheLinks.kt
+++ ui/features/links/src/main/java/city/zouitel/links/ui/CacheLinks.kt
@@ -1,40 +1,32 @@
+@file:Suppress("DeferredResultUnused")
+
 package city.zouitel.links.ui
 
-import android.annotation.SuppressLint
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
+import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 import city.zouitel.links.model.Link
-import city.zouitel.logic.asShortToast
-import java.util.*
+import com.devscion.metaprobekmp.MetaProbe
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.java.KoinJavaComponent.inject
 
-@SuppressLint("CoroutineCreationDuringComposition")
-@Composable
-fun CacheLinks(
-    linkModel: LinkScreenModel,
-    noteId: String,
-    url: String
-) {
-    val context = LocalContext.current
-    val observeLinks = remember(linkModel, linkModel::getAllLinks).collectAsState()
-    val uiState by remember(linkModel, linkModel::uiState).collectAsState()
-
-    val id by lazy { Random().nextLong() }
-
-    runCatching {
-        linkModel.urlPreview(context, url)?.fetchUrlPreview()
-    }.onSuccess {
-        if (
-            observeLinks.value.none { it.url == url } &&
-            uiState.title.isNotBlank() &&
-            uiState.host.isNotBlank() &&
-            uiState.img.isNotBlank()
-        ) {
+fun cacheLink(url: String, uid: String) {
+    val scope = CoroutineScope(Dispatchers.IO)
+    val linkModel by inject<LinkScreenModel>(LinkScreenModel::class.java)
+    val id by lazy { url.hashCode() }
+    scope.launch {
+        MetaProbe(url).probeLink().onSuccess {
             linkModel.doWork(
-                noteId = noteId,
-                link = Link(id, url, uiState.host, uiState.img, uiState.title, uiState.description)
+                Link(
+                    id = id,
+                    uid = uid,
+                    title = it.title ?: DEFAULT_TXT,
+                    description = it.description ?: DEFAULT_TXT,
+                    image = it.image ?: DEFAULT_TXT,
+                    url = url,
+                    icon = it.icon ?: DEFAULT_TXT
+                )
             )
         }
-    }.onFailure {
-        context.apply { it.message?.asShortToast() }
     }
 }
