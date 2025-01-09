5982a2708b79a924d17494c648cac16f1455c502
yon <bourntivoli@gmail.com>
2025-01-09 21:30:48 +0100

    feat: Implement LinkWorker for background link processing
    Introduced `LinkWorker` to handle link processing in the background.
    - Added `doWork` function
     in `LinkScreenModel` to enqueue `LinkWorker` with constraints (network connected) and input data (link details).
    - Updated LinkScreenModel to use `ObserveAll` instead of `GetAllLinks` for observing links and use `ObserveByUid` and `DeleteByUid`
    - Implemented `
    initializeUid` to observe links by uid.
    - Added `sendAction` function to handle delete by uid action.
    - Added `findUrlLink` function to find urls from description.
    - Removed unused url preview function.
    - Updated the  UiState flow using asLogicFlow.
    - Updated the observeAll flow using asLogicFlow.
    - Updated the observeByUid flow using asLogicFlow.

--- ui/features/links/src/main/java/city/zouitel/links/ui/LinkScreenModel.kt
+++ ui/features/links/src/main/java/city/zouitel/links/ui/LinkScreenModel.kt
@@ -1,109 +1,69 @@
 package city.zouitel.links.ui
 
 import android.app.Application
-import android.content.Context
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.work.*
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
 import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
-import com.baha.url.preview.BahaUrlPreview
-import com.baha.url.preview.IUrlPreviewCallback
-import com.baha.url.preview.UrlInfoItem
 import city.zouitel.domain.usecase.LinkUseCase
+import city.zouitel.domain.utils.Action
+import city.zouitel.domain.utils.exe
 import city.zouitel.links.mapper.LinkMapper
 import city.zouitel.links.model.Link
 import city.zouitel.links.state.UiState
 import city.zouitel.links.utils.Constants.DESCRIBE
-import city.zouitel.links.utils.Constants.HOST
+import city.zouitel.links.utils.Constants.ICON
 import city.zouitel.links.utils.Constants.IMG
 import city.zouitel.links.utils.Constants.LINK_ID
 import city.zouitel.links.utils.Constants.LINK_TAG
-import city.zouitel.links.utils.Constants.NOTE_ID
 import city.zouitel.links.utils.Constants.TITLE
+import city.zouitel.links.utils.Constants.UID
 import city.zouitel.links.utils.Constants.UNIQUE_LINK_WORK
 import city.zouitel.links.utils.Constants.URL
-import city.zouitel.links.model.Link as InLink
 import city.zouitel.links.worker.LinkWorker
-import city.zouitel.logic.asShortToast
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import java.net.URL
+import city.zouitel.logic.asLogicFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class LinkScreenModel(
     application: Application,
-    getAll: LinkUseCase.GetAllLinks,
-    private val delete: LinkUseCase.DeleteLink,
+    private val _observeAll_: LinkUseCase.ObserveAll,
+    private val _observeByUid_: LinkUseCase.ObserveByUid,
+    private val deleteByUid: LinkUseCase.DeleteByUid,
     private val mapper: LinkMapper,
 ): ScreenModel {
 
-    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(
-        UiState()
-    )
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState())
     internal val uiState: StateFlow<UiState> = _uiState
-        .stateIn(
-            screenModelScope,
-            SharingStarted.WhileSubscribed(),
-            UiState()
-        )
+        .asLogicFlow(UiState())
 
-    private val _getAllLinks: MutableStateFlow<List<Link>> = MutableStateFlow(
-        emptyList()
-    )
-    val getAllLinks: StateFlow<List<InLink>> = _getAllLinks
-            .stateIn(
-                screenModelScope,
-                SharingStarted.WhileSubscribed(),
-                listOf()
-            )
+    private val _observeAll: MutableStateFlow<List<Link>> = MutableStateFlow(emptyList())
+    val observeAll: StateFlow<List<Link>> = _observeAll
+        .asLogicFlow(emptyList()) {
+            exe {
+                _observeAll_.invoke().collect {
+                    _observeAll.value = mapper.fromDomain(it)
+                }
+            }
+        }
+
+    private val _observeByUid: MutableStateFlow<List<Link>> = MutableStateFlow(emptyList())
+    val observeByUid: StateFlow<List<Link>> = _observeByUid
+        .asLogicFlow(emptyList())
 
     private var workManager = WorkManager.getInstance(application)
 
-    init {
-        screenModelScope.launch(Dispatchers.IO) {
-            getAll.invoke().collect { links ->
-                _getAllLinks.value = mapper.fromDomain(links)
-            }
+    fun initializeUid(uid: String) = exe {
+        _observeByUid_(uid).collect {
+            _observeByUid.value = mapper.fromDomain(it)
         }
     }
 
-    fun deleteLink(link: InLink) {
-        screenModelScope.launch(Dispatchers.IO) {
-            delete.invoke(mapper.toDomain(link))
-        }
-    }
-
-    fun urlPreview(
-        context: Context,
-        res: String?,
-    ) = res?.let {
-        BahaUrlPreview(it, object : IUrlPreviewCallback {
-            override fun onComplete(urlInfo: UrlInfoItem) {
-                screenModelScope.launch(Dispatchers.IO) {
-                    urlInfo.apply {
-                        _uiState.value = _uiState.value.copy(
-                            title = title,
-                            description = description,
-                            host = URL(url).host,
-                            img = image
-                        )
-                    }
-                }
-            }
-
-            override fun onFailed(throwable: Throwable) {
-                context.apply { "Can't load link".asShortToast() }
-            }
-        })
-    }
-
-    fun doWork(
-        noteId: String,
-        link: Link
-    ) = screenModelScope.launch(Dispatchers.IO) {
-
+    internal fun doWork(link: Link) = exe {
         val constraints = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
@@ -112,13 +72,13 @@ class LinkScreenModel(
             .addTag(LINK_TAG)
             .setInputData(
                 Data.Builder()
-                    .putString(NOTE_ID, noteId)
-                    .putLong(LINK_ID, link.id)
+                    .putInt(LINK_ID, link.id)
+                    .putString(UID, link.uid)
                     .putString(TITLE, link.title)
                     .putString(DESCRIBE, link.description)
                     .putString(URL, link.url)
                     .putString(IMG, link.image)
-                    .putString(HOST, link.host)
+                    .putString(ICON, link.icon)
                     .build()
             )
             .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
@@ -130,5 +90,28 @@ class LinkScreenModel(
             ExistingWorkPolicy.KEEP,
             worker
         )
+    }
+
+    fun sendAction(act: Action) {
+        when(act) {
+            is Action.DeleteByUid -> exe { deleteByUid(act.uid) }
+            else -> throw Exception("Unknown event")
+        }
+    }
+
+
+    /**
+     * Extracts and returns a list of URLs found within a given string description.
+     *
+     * This function takes an optional string [des] as input, splits it into words,
+     * filters out words that match a URL pattern (starting with "http://" or "https://"),
+     * and returns the matching URLs in reversed order (because there will issue in updating without reverse it).
+     *
+     * @param des The optional string description to search for URLs.
+     * @return A list of URLs found in the description, or null if the description is null or no URLs are found.
+     * The URLs are returned in reversed order of their appearance in the original string.
+     */
+    val findUrlLink: (String?) -> List<String>? = { des ->
+        des ?. split(' ') ?. filter { s -> s.matches("https?://.+".toRegex()) } ?. reversed()
     }
 }
