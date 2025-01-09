8ec37b8146c140d4a983373c646686a21cd6d43b
yon <bourntivoli@gmail.com>
2025-01-09 21:52:04 +0100

    Refactor: Remove LinkMapper from NoteMapper
    Removed the `LinkMapper` dependency and `linkEntities` mapping from the `NoteMapper`.
    
    This change
     simplifies `NoteMapper` by removing link mapping logic.

--- ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
+++ ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
@@ -1,7 +1,6 @@
 package city.zouitel.screens.mapper
 
 import city.zouitel.audios.mapper.AudioMapper
-import city.zouitel.links.mapper.LinkMapper
 import city.zouitel.note.mapper.DataMapper
 import city.zouitel.note.model.Note
 import city.zouitel.tags.mapper.TagMapper
@@ -10,7 +9,6 @@ import city.zouitel.domain.model.Note as OutNote
 class NoteMapper(
     private val dataMapper: DataMapper,
     private val tagMapper: TagMapper,
-    private val linkMapper: LinkMapper,
     private val audioMapper: AudioMapper
 ) {
     fun fromDomain(notes: List<OutNote>) = notes.map { fromDomain(it) }
@@ -19,7 +17,6 @@ class NoteMapper(
         Note(
             dataEntity = dataMapper.fromDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.fromDomain(it) },
-            linkEntities = linkEntities.map { linkMapper.fromDomain(it) },
             audioEntities = audioEntities.map { audioMapper.fromDomain(it) }
         )
     }
