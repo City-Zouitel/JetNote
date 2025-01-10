667b399477b6387c2aa64accea9a3a9273821092
yon <bourntivoli@gmail.com>
2025-01-10 22:25:47 +0100

    docs: Add KDoc for Link data class
    Added KDoc documentation to the `Link` data class, describing its purpose and properties. This enhances code clarity
     and provides better understanding of the class within the project.

--- core/local/repository/src/main/java/city/zouitel/repository/model/Link.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Link.kt
@@ -4,6 +4,24 @@ import androidx.annotation.Keep
 import city.zouitel.domain.utils.Constants.DEFAULT_INT
 import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
+/**
+ * Represents a link with associated metadata.
+ *
+ * This data class encapsulates information about a web link, including its identifier,
+ * unique user ID (UID), URL, title, description, image, and icon. It's designed to
+ * store and manage link details within an application.
+ *
+ * @property id A unique numerical identifier for the link. Defaults to [DEFAULT_INT].
+ * @property uid A unique string identifier associated with the user who created or owns the link. Defaults to [DEFAULT_TXT].
+ * @property url The actual URL address of the link. Defaults to [DEFAULT_TXT].
+ * @property title The title or headline of the linked content. Defaults to [DEFAULT_TXT].
+ * @property description A brief summary or description of the linked content. Defaults to [DEFAULT_TXT].
+ * @property image A URL or path to an image associated with the link (e.g., a thumbnail). Defaults to [DEFAULT_TXT].
+ * @property icon A URL or path to an icon representing the link or its source. Defaults to [DEFAULT_TXT].
+ *
+ * @see DEFAULT_INT
+ * @see DEFAULT_TXT
+ */
 @Keep
 data class Link(
      val id: Int = DEFAULT_INT,
