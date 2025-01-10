38b0e35efffe5e085c76d95c2758e7e31e10dda6
yon <bourntivoli@gmail.com>
2025-01-10 22:25:23 +0100

    Refactor: Replace `exe` with `withAsync` for asynchronous operations
    Replaced the `exe` function with `withAsync` in the `Context
    .kt` file.
    The new `withAsync` function executes a suspending block of code asynchronously within the `screenModelScope` using `async`. It also catches exceptions within the provided lambda, wraps it in a new Exception with the same message and rethrows it.
    This change provides a more robust and clearer
     way to manage asynchronous operations within ScreenModels.

--- domain/src/main/java/city/zouitel/domain/utils/Context.kt
+++ domain/src/main/java/city/zouitel/domain/utils/Context.kt
@@ -3,9 +3,32 @@ package city.zouitel.domain.utils
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 
 context(ScreenModel)
-fun exe(scope: suspend CoroutineScope.() -> Unit) = screenModelScope
-    .launch(context = Dispatchers.IO, block = scope)
+/**
+ * Executes a suspending block of code within the `screenModelScope` asynchronously.
+ *
+ * This function provides a convenient way to launch asynchronous tasks within the lifecycle
+ * of a screen model. It utilizes [async] to run the provided [scope] within the screen model's
+ * coroutine scope.  Any exception thrown within the `scope` will be caught, wrapped in a new Exception
+ * and rethrown.
+ *
+ * @param scope The suspending block of code to execute asynchronously. This lambda will be executed
+ *              within the [screenModelScope].
+ * @return A [Deferred] that completes when the [scope] lambda finishes executing.
+ *         The result of the [Deferred] is [Unit], or it will throw an exception if the [scope] lambda failed.
+ * @throws Exception if the provided [scope] lambda throws an exception. The original exception's message
+ *                 will be wrapped within this new [Exception].
+ */
+fun withAsync(scope: suspend CoroutineScope.() -> Unit): Deferred<Unit> {
+    return screenModelScope
+        .async {
+            runCatching {
+                scope()
+            }.onFailure {
+                throw Exception(it.message)
+            }
+        }
+}
