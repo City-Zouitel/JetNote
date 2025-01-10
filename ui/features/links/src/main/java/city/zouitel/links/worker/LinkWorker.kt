980c6260f11e194ccba2ade46cf557906126455c
yon <bourntivoli@gmail.com>
2025-01-10 22:27:24 +0100

    Refactor: Update LinkWorker input data keys
    Renamed input data keys in `LinkWorker` to use `LINK_` prefix for better clarity and consistency
    .
    
    - Replaced `LINK_ID` with `ID`
    - Replaced `DESCRIBE` with `LINK_DESCRIBE`
    - Replaced `ICON` with `LINK_ICON`
    - Replaced `IMG` with `LINK_IMG`
    - Replaced `TITLE` with `
    LINK_TITLE`

--- ui/features/links/src/main/java/city/zouitel/links/worker/LinkWorker.kt
+++ ui/features/links/src/main/java/city/zouitel/links/worker/LinkWorker.kt
@@ -8,11 +8,11 @@ import city.zouitel.domain.utils.Constants.DEFAULT_INT
 import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 import city.zouitel.links.mapper.LinkMapper
 import city.zouitel.links.model.Link
-import city.zouitel.links.utils.Constants.DESCRIBE
-import city.zouitel.links.utils.Constants.ICON
-import city.zouitel.links.utils.Constants.IMG
-import city.zouitel.links.utils.Constants.LINK_ID
-import city.zouitel.links.utils.Constants.TITLE
+import city.zouitel.links.utils.Constants.LINK_DESCRIBE
+import city.zouitel.links.utils.Constants.LINK_ICON
+import city.zouitel.links.utils.Constants.LINK_IMG
+import city.zouitel.links.utils.Constants.ID
+import city.zouitel.links.utils.Constants.LINK_TITLE
 import city.zouitel.links.utils.Constants.UID
 import city.zouitel.links.utils.Constants.URL
 import kotlinx.coroutines.CoroutineDispatcher
@@ -28,13 +28,13 @@ class LinkWorker(
 
     override suspend fun doWork(): Result = withContext(ioDeprecated) {
         return@withContext try {
-            val id_data = inputData.getInt(LINK_ID, DEFAULT_INT)
+            val id_data = inputData.getInt(ID, DEFAULT_INT)
             val uid_data = inputData.getString(UID) ?: DEFAULT_TXT
             val url_data = inputData.getString(URL) ?: DEFAULT_TXT
-            val title_data = inputData.getString(TITLE) ?: DEFAULT_TXT
-            val description_data = inputData.getString(DESCRIBE) ?: DEFAULT_TXT
-            val image_data = inputData.getString(IMG) ?: DEFAULT_TXT
-            val icon_data = inputData.getString(ICON) ?: DEFAULT_TXT
+            val title_data = inputData.getString(LINK_TITLE) ?: DEFAULT_TXT
+            val description_data = inputData.getString(LINK_DESCRIBE) ?: DEFAULT_TXT
+            val image_data = inputData.getString(LINK_IMG) ?: DEFAULT_TXT
+            val icon_data = inputData.getString(LINK_ICON) ?: DEFAULT_TXT
 
             insert.invoke(
                linkMapper.toDomain(
