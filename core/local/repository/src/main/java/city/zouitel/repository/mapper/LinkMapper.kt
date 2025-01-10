05b2dc6a8fe74d8703be46387ab7e2eb41f9079e
yon <bourntivoli@gmail.com>
2025-01-10 22:26:39 +0100

    feat: Implement LinkMapper for domain-repository model conversion
    Added `LinkMapper` class with methods to convert between `Link` (repository) and `Out
    Link` (domain) models. This includes `toDomain(links: List<Link>)`, `toDomain(link: Link)` and `fromDomain(link: OutLink)`. These methods are used to map properties from one layer to the other.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/LinkMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/LinkMapper.kt
@@ -3,10 +3,37 @@ package city.zouitel.repository.mapper
 import city.zouitel.repository.model.Link
 import city.zouitel.domain.model.Link as OutLink
 
+/**
+ * Mapper class responsible for converting between [Link] (repository layer representation) and
+ * [OutLink] (domain layer representation) objects.
+ */
 class LinkMapper {
 
+    /**
+     * Converts a list of [Link] entities to a list of corresponding domain [Link] models.
+     *
+     * This function iterates through a list of [Link] entities and applies the [toDomain(Link)]
+     * conversion function to each entity, resulting in a new list containing the domain
+     * representations of those Links.
+     *
+     * @param links The list of [Link] entities to convert.
+     * @return A list of [Link] domain models, where each model corresponds to a
+     *         converted entity from the input list.
+     *
+     * @see toDomain(Link)
+     */
     fun toDomain(links: List<Link>) = links.map { toDomain(it) }
 
+    /**
+     * Converts a [Link] entity repository model to an [OutLink] domain model.
+     *
+     * This function maps the properties of a [Link] object to a corresponding [OutLink] object,
+     * effectively transforming a data-layer representation of a Link into a domain-layer
+     * representation. The mapping includes the Link's ID, UID, item description, and completion status.
+     *
+     * @param link The [Link] entity to convert.
+     * @return An [OutLink] domain model representing the provided Link.
+     */
     fun toDomain(link: Link) = OutLink(
         id = link.id,
         uid = link.uid,
@@ -17,6 +44,16 @@ class LinkMapper {
         icon = link.icon
     )
 
+    /**
+     * Converts an [OutLink] (domain model) to a [Link] (repository model).
+     *
+     * This function maps the properties of an [OutLink] to a new [Link] instance.
+     * It's typically used when transferring data from the domain layer to the
+     * data/presentation layer.
+     *
+     * @param link The [OutLink] object to convert.
+     * @return A new [Link] object with properties copied from the input [OutLink].
+     */
     fun fromDomain(link: OutLink) = Link(
         id = link.id,
         uid = link.uid,
