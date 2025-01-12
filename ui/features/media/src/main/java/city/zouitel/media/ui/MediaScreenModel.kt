b087744c2520ec9d5a039807a9198f0b6a7b774b
yon <bourntivoli@gmail.com>
2025-01-12 20:31:50 +0100

    Refactor: Use withAsync for coroutine handling in MediaScreenModel
    Replaced `screenModelScope.launch(Dispatchers.IO)` with `with
    Async` for handling coroutines in `MediaScreenModel`. This simplifies coroutine management and exception handling.
    
    - Removed `act` function.
    - Replaced `screenModelScope.launch(Dispatchers.IO) { ... }` with `withAsync { ... }` in `observeAll`, `initialize
    Uid` and `sendAction` functions.
    - Added `deleteByUid` to the class.
    - Updated the `sendAction` function to handle `Action.DeleteById` and `Action.DeleteByUid`.

--- ui/features/media/src/main/java/city/zouitel/media/ui/MediaScreenModel.kt
+++ ui/features/media/src/main/java/city/zouitel/media/ui/MediaScreenModel.kt
@@ -1,29 +1,29 @@
 package city.zouitel.media.ui
 
 import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.MediaUseCase
 import city.zouitel.domain.utils.Action
+import city.zouitel.domain.utils.withAsync
 import city.zouitel.logic.withFlow
 import city.zouitel.media.mapper.MediaMapper
 import city.zouitel.media.model.Media
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
+@Suppress("DeferredResultUnused")
 class MediaScreenModel(
     _observeAll_: MediaUseCase.ObserveAll,
     private val _observeByUid_: MediaUseCase.ObserveByUid,
     private val insert: MediaUseCase.Insert,
     private val deleteById: MediaUseCase.DeleteById,
+    private val deleteByUid: MediaUseCase.DeleteByUid,
     private val mapper: MediaMapper
 ): ScreenModel {
 
     private val _observeAll: MutableStateFlow<List<Media>> = MutableStateFlow(emptyList())
     val observeAll: StateFlow<List<Media>> = _observeAll
         .withFlow(emptyList()) {
-            act {
+            withAsync {
                 _observeAll_().collect { _observeAll.value = mapper.fromDomain(it) }
             }
         }
@@ -32,20 +32,17 @@ class MediaScreenModel(
     val observeByUid: StateFlow<List<Media>> = _observeByUid.withFlow(emptyList())
 
     fun initializeUid(uid: String) {
-        act {
+        withAsync {
             _observeByUid_(uid).collect { _observeByUid.value = mapper.fromDomain(it) }
         }
     }
 
-    fun sendAction(action: Action) {
-        when(action) {
-            is Action.Delete<*> -> act { deleteById(action.data as Long) }
-            is Action.Insert<*> -> act { insert(mapper.toDomain(action.data as Media)) }
-            else -> throw Exception("Not Implemented!")
+    fun sendAction(act: Action) {
+        when(act) {
+            is Action.Insert<*> -> withAsync { insert(mapper.toDomain(act.data as Media)) }
+            is Action.DeleteById -> withAsync { deleteById(act.id as Long) }
+            is Action.DeleteByUid -> withAsync { deleteByUid(act.uid) }
+            else -> throw NotImplementedError("Action not implemented: $act")
         }
-    }
-
-    private fun act(action: suspend () -> Unit) {
-        screenModelScope.launch(Dispatchers.IO) { action.invoke() }
     }
 }
