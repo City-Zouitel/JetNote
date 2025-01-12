c12829e445a2c91025f96ca9f234d68ed60e49ab
yon <bourntivoli@gmail.com>
2025-01-12 19:17:09 +0100

    feat: Add UpdateById and DeleteById actions
    Added `UpdateById` and `DeleteById` data classes to the `Action` sealed class. These new
     actions allow for updating and deleting data based on an ID.

--- domain/src/main/java/city/zouitel/domain/utils/Action.kt
+++ domain/src/main/java/city/zouitel/domain/utils/Action.kt
@@ -3,7 +3,9 @@ package city.zouitel.domain.utils
 sealed class Action {
     data class Insert<out T>(val data: T) : Action()
     data class Update<out T>(val data: T) : Action()
+    data class UpdateById(val id: Number) : Action()
     data class Delete<out T> (val data: T) : Action()
+    data class DeleteById (val id: Number): Action()
     data class DeleteByUid (val uid: String): Action()
     data object DeleteAll : Action()
 }
