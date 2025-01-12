a7422b5da9c6465ccdb651cd4652ff628fdcda50
yon <bourntivoli@gmail.com>
2025-01-12 20:32:24 +0100

    Refactor: Rename ReminderDataSource methods for clarity
    Renamed methods in `ReminderDataSource` to improve clarity and consistency.
    
    - `observeById` to `
    observeByUid` to reflect that it observes by note UID
    - `update` to `updateById` to clarify that the update is by id
    - `delete` to `deleteById` to clarify that the delete is by id
    - `deleteAll` to `deleteByUid` to reflect that it deletes
     based on a UID

--- core/local/repository/src/main/java/city/zouitel/repository/datasource/ReminderDataSource.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/datasource/ReminderDataSource.kt
@@ -9,15 +9,15 @@ import kotlinx.coroutines.flow.Flow
 interface ReminderDataSource {
 
     /**
-     * Observes reminders for a specific user ID.
+     * Observes reminders for a specific note UID.
      *
      * This function returns a [Flow] that emits a list of Reminder objects
-     * associated with the given user ID whenever there are changes in the data.
+     * associated with the given note UID whenever there are changes in the data.
      *
      * @param uid The unique identifier of the user.
      * @return A [Flow] emitting lists of Reminder objects for the user.
      */
-    suspend fun observeById(uid: String): Flow<List<Reminder>>
+    suspend fun observeByUid(uid: String): Flow<List<Reminder>>
 
     /**
      * Inserts a new reminder into the data source.
@@ -31,17 +31,37 @@ interface ReminderDataSource {
      *
      * @param id The identifier of the entity to update.
      */
-    suspend fun update(id: Int)
+    suspend fun updateById(id: Int)
 
     /**
-     * Deletes an entity with the given ID.
+     * Deletes an entity from the data source by its unique identifier.
      *
-     * @param id The ID of the entity to delete.
+     * This is a suspending function, meaning it should be called within a coroutine or
+     * another suspending function.  It handles the asynchronous deletion operation.
+     *
+     * @param id The unique identifier of the entity to delete. Must be a positive integer.
+     * @throws IllegalArgumentException if the provided `id` is not valid (e.g., negative or zero).
+     * @throws NoSuchElementException if no entity with the given `id` is found. (Optional, depends on implementation)
+     * @throws Exception if an error occurs during the deletion process (e.g., database error, network error). (Optional, depends on implementation)
      */
-    suspend fun delete(id: Int)
+    suspend fun deleteById(id: Int)
 
     /**
-     * Deletes all entries from the data source.
+     * Deletes a record or resource associated with the given unique identifier (UID).
+     *
+     * This is a suspending function, meaning it must be called within a coroutine or another
+     * suspending function. It performs the deletion asynchronously.
+     *
+     * @param uid The unique identifier of the record/resource to delete.
+     * @throws Exception if any error occurs during the deletion process.  Consider more specific exceptions if possible.
+     * For example:
+     * @throws NotFoundException if a record with the given UID is not found.
+     * @throws DatabaseException if there is an issue communicating with the database.
+     * @throws PermissionDeniedException if the user doesn't have permission to delete.
+     * @throws IllegalArgumentException if the uid is invalid or empty.
+     *
+     * @see [suspend]
+     * @see [kotlinx.coroutines]
      */
-    suspend fun deleteAll()
+    suspend fun deleteByUid(uid: String)
 }
