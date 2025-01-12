2702167e153b468fd4660155ebcd4835c1e4db40
yon <bourntivoli@gmail.com>
2025-01-12 20:32:55 +0100

    Refactor: Use Action for Reminder operations
    Replaced `UiEvent` with `Action` for reminder operations (insert, delete).
    Removed the `perform
    UiEvent` function, and now `withAsync` is used to launch the coroutine for each action.
    The `initializeReminders` function now uses `withAsync` for data fetching.
    Removed `delete` and `deleteAll` and added `deleteById` and `deleteByUid`.
    These changes
     aim to streamline the data flow and simplify the event handling.

--- ui/features/reminder/src/main/java/city/zouitel/reminder/ui/ReminderScreenModel.kt
+++ ui/features/reminder/src/main/java/city/zouitel/reminder/ui/ReminderScreenModel.kt
@@ -1,47 +1,39 @@
 package city.zouitel.reminder.ui
 
 import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.ReminderUseCase
+import city.zouitel.domain.utils.Action
+import city.zouitel.domain.utils.withAsync
 import city.zouitel.logic.withFlow
-import city.zouitel.logic.events.UiEvent
-import city.zouitel.logic.events.UiEventHandler
 import city.zouitel.reminder.mapper.ReminderMapper
 import city.zouitel.reminder.model.Reminder
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
+@Suppress("DeferredResultUnused")
 class ReminderScreenModel(
-    private val observeById: ReminderUseCase.ObserveById,
+    private val observeByUid: ReminderUseCase.ObserveByUid,
     private val insert: ReminderUseCase.Insert,
-    private val delete: ReminderUseCase.DeleteReminder,
-    private val deleteAll: ReminderUseCase.DeleteAll,
+    private val deleteById: ReminderUseCase.DeleteById,
+    private val deleteByUid: ReminderUseCase.DeleteByUid,
     private val mapper: ReminderMapper
-): ScreenModel, UiEventHandler<Reminder> {
+): ScreenModel {
 
     private val _observeAllById = MutableStateFlow<List<Reminder>>(emptyList())
     val observeAllById: StateFlow<List<Reminder>> = _observeAllById.withFlow(emptyList())
 
     fun initializeReminders(uid: String) {
-        performUiEvent {
-            observeById(uid).collect {
-                _observeAllById.value = mapper.fromDomain(it)
-            }
+        withAsync {
+            observeByUid(uid).collect { _observeAllById.value = mapper.fromDomain(it) }
         }
     }
 
-    override fun sendUiEvent(event: UiEvent<Reminder>) {
-        when(event) {
-            is UiEvent.Delete -> performUiEvent { delete(event.data.id) }
-            is UiEvent.Insert -> performUiEvent { insert(mapper.toDomain(event.data)) }
-            is UiEvent.DeleteAll -> performUiEvent { deleteAll() }
-            else -> throw NotImplementedError("Action not implemented: $event")
+    fun sendAction(act: Action) {
+        when(act) {
+            is Action.Insert<*> -> withAsync { insert(mapper.toDomain(act.data as Reminder)) }
+            is Action.DeleteById -> withAsync { deleteById(act.id as Int) }
+            is Action.DeleteByUid -> withAsync { deleteByUid(act.uid) }
+            else -> throw NotImplementedError("Action not implemented: $act")
         }
-    }
-
-    override fun performUiEvent(action: suspend () -> Unit) {
-        screenModelScope.launch(Dispatchers.IO) { action.invoke() }
     }
 }
