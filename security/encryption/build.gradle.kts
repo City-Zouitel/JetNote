8ac49bb89e45374e065794c1f23ffa8033f19078
yon <bourntivoli@gmail.com>
2025-01-12 19:33:09 +0100

    Refactor: Update security module build configuration
    Simplified the `security/encryption/build.gradle.kts` file by removing unnecessary configurations and dependencies. It now
     uses `cityzouitel.androidLibrary` plugin and defines the `namespace`.

--- security/encryption/build.gradle.kts
+++ security/encryption/build.gradle.kts
@@ -1,43 +1,7 @@
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.cityzouitel.androidLibrary)
 }
 
 android {
     namespace = "city.zouitel.encryption"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
