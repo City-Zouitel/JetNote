607c287f4e36a1240b2bc8bc90012be0f51b9787
yon <bourntivoli@gmail.com>
2025-01-15 19:20:04 +0100

    feat: Add SharedScreen for screen navigation
    Introduced a new `SharedScreen` sealed class for screen navigation with the following data classes:
    - `Main
    Screen` with `isHome` property.
    - `Workplace` with properties for workplace details.
    - `Assistant`, `AudioList` with `uid`, `Reminder` with properties for reminder details, `Tags` with `uid`, `Tasks` with `uid`.
    
    This change facilitates centralized navigation across
     modules using Voyager's `ScreenProvider`.

---
+++ domain/src/main/java/city/zouitel/domain/provider/SharedScreen.kt
@@ -0,0 +1,25 @@
+package city.zouitel.domain.provider
+
+import cafe.adriel.voyager.core.registry.ScreenProvider
+
+sealed class SharedScreen: ScreenProvider {
+    data class MainScreen(val isHome: Boolean): SharedScreen()
+    data class Workplace(
+        val uid: String,
+        val isNew: Boolean,
+        val title: String?,
+        val description: String?,
+        val backgroundColor: Int,
+        val textColor: Int,
+        val priority: String
+    ): SharedScreen()
+    data object Assistant: SharedScreen()
+    data class AudioList(val uid: String): SharedScreen()
+    data class Reminder(
+        val uid: String,
+        val title: String?,
+        val message: String?
+    ): SharedScreen()
+    data class Tags(val uid: String): SharedScreen()
+    data class Tasks(val uid: String): SharedScreen()
+}
