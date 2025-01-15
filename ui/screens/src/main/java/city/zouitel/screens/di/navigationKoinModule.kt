6a594fdd81fec6c0e209b024a80620f865049ddd
yon <bourntivoli@gmail.com>
2025-01-15 19:18:40 +0100

    feat: Add MainScreen to Voyager screen registry
    Added `screensScreenModule` to register `MainScreen` with Voyager's screen registry.
    This allows
     navigating to `MainScreen` using `SharedScreen.MainScreen`.

--- ui/screens/src/main/java/city/zouitel/screens/di/navigationKoinModule.kt
+++ ui/screens/src/main/java/city/zouitel/screens/di/navigationKoinModule.kt
@@ -1,6 +1,9 @@
 package city.zouitel.screens.di
 
+import cafe.adriel.voyager.core.registry.screenModule
 import city.zouitel.assistant.di.assistantKoinModule
+import city.zouitel.domain.provider.SharedScreen
+import city.zouitel.screens.main_screen.MainScreen
 import city.zouitel.screens.main_screen.MainScreenModel
 import city.zouitel.screens.mapper.NoteMapper
 import city.zouitel.screens.navigation_drawer.NavigationDrawerScreenModel
@@ -13,4 +16,10 @@ val screensKoinModule = module {
     factoryOf(::NavigationDrawerScreenModel)
 
     includes(assistantKoinModule)
+}
+
+val screensScreenModule = screenModule {
+    register<SharedScreen.MainScreen> {
+        MainScreen(isHome = it.isHome)
+    }
 }
