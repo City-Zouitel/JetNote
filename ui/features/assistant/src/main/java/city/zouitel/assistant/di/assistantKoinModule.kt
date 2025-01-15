1a8b42a8bea447f16c9a42ca0c37911ad8a9e9c5
yon <bourntivoli@gmail.com>
2025-01-15 19:16:21 +0100

    feat: Add assistant screen to Voyager navigation
    Added `assistantScreenModule` to register `AssistantScreen` with Voyager using `SharedScreen.Assistant` as the
     key. This allows navigation to the Assistant screen using Voyager's navigation system.

--- ui/features/assistant/src/main/java/city/zouitel/assistant/di/assistantKoinModule.kt
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/di/assistantKoinModule.kt
@@ -1,11 +1,20 @@
 package city.zouitel.assistant.di
 
+import cafe.adriel.voyager.core.registry.screenModule
+import city.zouitel.assistant.mapper.AssistantMapper
+import city.zouitel.assistant.ui.AssistantScreen
+import city.zouitel.assistant.ui.AssistantScreenModel
+import city.zouitel.domain.provider.SharedScreen
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
-import city.zouitel.assistant.mapper.AssistantMapper
-import city.zouitel.assistant.ui.AssistantScreenModel
 
 val assistantKoinModule = module {
     factoryOf(::AssistantMapper)
     factoryOf(::AssistantScreenModel)
+}
+
+val assistantScreenModule = screenModule {
+    register<SharedScreen.Assistant> {
+        AssistantScreen()
+    }
 }
