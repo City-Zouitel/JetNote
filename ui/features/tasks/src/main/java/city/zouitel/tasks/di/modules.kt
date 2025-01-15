8018237afab66e522584be982e3b64d776ebdc19
yon <bourntivoli@gmail.com>
2025-01-15 19:18:01 +0100

    Refactor: Rename `tasksKoinModule` to `modules` and add `tasksScreenModule`
    Renamed `tasksKoinModule` to `modules` and added a new `tasksScreenModule` to register `TasksScreen` with Voyager's screen registry.
    
    - Renamed `tasksKoinModule.kt` to `modules.kt`.
    - Added `tasksScreenModule` using Voyager's `screenModule` to register `TasksScreen` using the provided `uid`.

--- ui/features/tasks/src/main/java/city/zouitel/tasks/di/modules.kt
+++ ui/features/tasks/src/main/java/city/zouitel/tasks/di/modules.kt
@@ -1,11 +1,20 @@
 package city.zouitel.tasks.di
 
+import cafe.adriel.voyager.core.registry.screenModule
+import city.zouitel.domain.provider.SharedScreen
 import city.zouitel.tasks.mapper.TaskMapper
 import city.zouitel.tasks.ui.TaskScreenModel
+import city.zouitel.tasks.ui.TasksScreen
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val tasksKoinModule = module {
     factoryOf(::TaskMapper)
     factoryOf(::TaskScreenModel)
+}
+
+val tasksScreenModule = screenModule {
+    register<SharedScreen.Tasks> {
+        TasksScreen(it.uid)
+    }
 }
