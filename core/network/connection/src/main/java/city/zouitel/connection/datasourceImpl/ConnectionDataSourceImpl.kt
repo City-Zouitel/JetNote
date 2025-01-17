fe66b510e2aa022d74ed3192161bca0eb8475cb6
yon <bourntivoli@gmail.com>
2025-01-17 17:06:54 +0100

    feat: Implement Network Connection Data Source
    Implemented `ConnectionDataSourceImpl` to monitor network connectivity. This data source uses a `callbackFlow` to emit `true
    ` when the device is online and `false` when offline.
    
    - Registers a `NetworkCallback` to listen for network availability changes.
    - Uses `ConnectivityManager` to determine current connection status.
    - Emits the current connection status on initial collection.
    - Unregisters the callback on flow cancellation.

---
+++ core/network/connection/src/main/java/city/zouitel/connection/datasourceImpl/ConnectionDataSourceImpl.kt
@@ -0,0 +1,51 @@
+package city.zouitel.connection.datasourceImpl
+
+//import android.health.connect.datatypes.SexualActivityRecord
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkRequest.Builder
+import androidx.core.content.getSystemService
+import city.zouitel.repository.datasource.ConnectionDataSource
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+
+class ConnectionDataSourceImpl(
+    private val context: Context
+): ConnectionDataSource {
+    override val isOnline: Flow<Boolean> = callbackFlow {
+        val callback = object : NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                channel.trySend(true)
+            }
+
+            override fun onLost(network: Network) {
+                channel.trySend(false)
+            }
+        }
+        val connectivityManager = context.getSystemService<ConnectivityManager>()
+        connectivityManager?.registerNetworkCallback(
+            Builder()
+                .addCapability(NET_CAPABILITY_INTERNET)
+                .build(),
+            callback
+        )
+        channel.trySend(connectivityManager.isCurrentlyConnected())
+
+        awaitClose {
+            connectivityManager?.unregisterNetworkCallback(callback)
+        }
+    }.conflate()
+
+    private fun ConnectivityManager?.isCurrentlyConnected() = when(this) {
+        null -> false
+        else -> activeNetwork
+            ?.let(::getNetworkCapabilities)
+            ?.hasCapability(NET_CAPABILITY_INTERNET)
+            ?: false
+    }
+}
