33fd6681495fbc424aec0501b5cadc7784267e81
yon <bourntivoli@gmail.com>
2025-01-17 17:04:32 +0100

    feat: Add connection implementation to assistant module
    Added `ConnectionImpl` and `Connection` to the Koin module.
    - Registered `ConnectionImpl` as
     a single instance, bound to the `Connection` interface.
    - Initialized `ConnectionImpl` with `androidContext()`.

--- ui/features/assistant/src/main/java/city/zouitel/assistant/di/assistantKoinModule.kt
+++ ui/features/assistant/src/main/java/city/zouitel/assistant/di/assistantKoinModule.kt
@@ -1,16 +1,26 @@
 package city.zouitel.assistant.di
 
 import cafe.adriel.voyager.core.registry.screenModule
+import city.zouitel.assistant.connection.Connection
+import city.zouitel.assistant.connection.ConnectionImpl
 import city.zouitel.assistant.mapper.AssistantMapper
 import city.zouitel.assistant.ui.AssistantScreen
 import city.zouitel.assistant.ui.AssistantScreenModel
 import city.zouitel.domain.provider.SharedScreen
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val assistantKoinModule = module {
     factoryOf(::AssistantMapper)
     factoryOf(::AssistantScreenModel)
+    singleOf(::ConnectionImpl) bind Connection::class
+
+    single {
+        ConnectionImpl(androidContext())
+    }
 }
 
 val assistantScreenModule = screenModule {
