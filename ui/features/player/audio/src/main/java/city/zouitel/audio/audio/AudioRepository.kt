9534671bac79e402a9ae061275ad7397098e0645
yon <bourntivoli@gmail.com>
2025-01-25 22:42:57 +0100

    feat: Add AudioRepository
    Added a new class `AudioRepository` to handle audio-related operations.
    
    It provides methods for loading audio files, retrieving
     audio details by URI, and loading audio amplitudes.
    
    This change introduces a dedicated repository for managing audio data and interactions.

---
+++ ui/features/player/audio/src/main/java/city/zouitel/audio/audio/AudioRepository.kt
@@ -0,0 +1,27 @@
+package city.zouitel.audio.audio
+
+import city.zouitel.audio.model.Audio
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+class AudioRepository(
+    private val localMediaDataSource: LocalMediaDataSource,
+    private val audioManager: AudioManager,
+    private val ioDispatcher: CoroutineDispatcher
+) {
+
+    suspend fun loadAudioFiles(query: String): List<Audio> = withContext(ioDispatcher) {
+        return@withContext localMediaDataSource.loadAudioFiles(query)
+    }
+
+    suspend fun loadAudioByUri(uri: String): Audio? = withContext(ioDispatcher) {
+        return@withContext localMediaDataSource.loadAudioByUri(uri)
+    }
+
+    suspend fun loadAudioAmplitudes(
+        localAudio: String
+    ): List<Int> = withContext(ioDispatcher) {
+        return@withContext audioManager.getAmplitudes(localAudio)
+    }
+
+}
