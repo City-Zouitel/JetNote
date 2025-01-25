35d69890046a7c4a896a3ebaedd9d8152fa4ad03
yon <bourntivoli@gmail.com>
2025-01-25 22:43:08 +0100

    Refactor: Restructure AudioScreenModel for PlaybackManager Integration
    Refactored `AudioScreenModel` to utilize `PlaybackManager` for audio playback and
     state management.
    
    - Replaced `ExoPlayerImpl` with `PlaybackManager` for audio handling.
    - Introduced `UiState` to manage UI-related states, replacing `AudioUiState`.
    - Added functions to initialize audio playback using Uid and observe playback events.
    - Updated functions to control playback (
    play/pause) and seek progress using `PlaybackManager`.
    - Refactored audio loading and amplitude calculation logic to leverage `PlaybackManager` and repository functions.
    - Implemented event observation for playback position and state updates.
    - Updated progress and playing state updates to use `UiState`.
    - Added `onDispose` to release the playback controller.
    
    This change streamlines audio playback and state management within the `AudioScreenModel`.

--- ui/features/audios/src/main/java/city/zouitel/audios/ui/component/AudioScreenModel.kt
+++ ui/features/audios/src/main/java/city/zouitel/audios/ui/component/AudioScreenModel.kt
@@ -5,90 +5,136 @@ import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.audios.audio.AudioRepository
 import city.zouitel.audios.mapper.AudioMapper
 import city.zouitel.audios.model.Audio
-import city.zouitel.audios.state.AudioUiState
-import city.zouitel.domain.exoplayer.ExoPlayerImpl
+import city.zouitel.audios.player.PlaybackManager
+import city.zouitel.audios.state.UiState
 import city.zouitel.domain.usecase.AudioUseCase
-import city.zouitel.systemDesign.CommonIcons
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class AudioScreenModel (
-    getAllAudios: AudioUseCase.GetAllAudios,
-    private val exoBuilder : ExoPlayerImpl,
+    observeAll: AudioUseCase.ObserveAll,
+    private val _observeByUid_: AudioUseCase.ObserveByUid,
+    private val player: PlaybackManager,
     private val repository: AudioRepository,
-    private val audioMapper: AudioMapper,
-    private val recPath: String
+    private val mapper: AudioMapper
 ): ScreenModel {
 
-    private val _audioUiState: MutableStateFlow<AudioUiState> = MutableStateFlow(
-        AudioUiState()
-    )
-    internal val audioUiState: StateFlow<AudioUiState> = _audioUiState
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState())
+    internal val uiState: StateFlow<UiState> = _uiState
         .stateIn(
             screenModelScope,
             SharingStarted.WhileSubscribed(),
-            AudioUiState()
+            UiState()
         )
 
-    private val _allAudios: MutableStateFlow<List<Audio>> = MutableStateFlow(emptyList())
-    val allAudios: StateFlow<List<Audio>> = _allAudios
+    private val _allAudios: MutableStateFlow<List<Audio?>> = MutableStateFlow(emptyList())
+    val allAudios: StateFlow<List<Audio?>> = _allAudios
         .stateIn(
             screenModelScope,
             SharingStarted.WhileSubscribed(),
             listOf()
         )
 
+    private val _observeByUid: MutableStateFlow<Audio?> = MutableStateFlow(null)
+    val observeByUid: StateFlow<Audio?> = _observeByUid
+        .stateIn(
+            screenModelScope,
+            SharingStarted.WhileSubscribed(),
+            null
+        )
+
+    private var currentAudio = MutableStateFlow<Audio?>(null)
+
     init {
         screenModelScope.launch(Dispatchers.IO) {
-            getAllAudios.invoke().collect { audios ->
-                _allAudios.value = audioMapper.fromDomain(audios)
+            observeAll.invoke().collect { audios ->
+                _allAudios.value = mapper.fromDomain(audios)
             }
         }
     }
 
-    suspend fun prepareMediaPlayer(mediaPath: String) {
-        runCatching {
-            exoBuilder.prepareMediaPlayer(mediaPath)
-            val amplitudes = repository.loadAudioAmplitudes(mediaPath)
-            _audioUiState.value = _audioUiState.value.copy(amplitudes = amplitudes)
-        }
-    }
-
-    fun playMedia() {
-        screenModelScope.launch {
-            exoBuilder.playMedia()
-            _audioUiState.value = _audioUiState.value.copy(isPlaying = true)
-            _audioUiState.value = _audioUiState.value.copy(icon = CommonIcons.PAUSE_CIRCLE_FILLED_ICON_24)
-        }
-    }
-
-    fun pauseMedia() {
-        screenModelScope.launch {
-            exoBuilder.pauseMedia()
-            _audioUiState.value = _audioUiState.value.copy(isPlaying = false)
-            _audioUiState.value = _audioUiState.value.copy(icon = CommonIcons.PLAY_CIRCLE_FILLED_ICON_24)
-        }
-    }
-
-    fun seekMedia(mediaUri: String, value: Long) {
-        screenModelScope.launch {
-            exoBuilder.prepareMediaPlayer(mediaUri).seekTo(value)
-        }
-    }
-
-    fun getMediaArtist(mediaUri: String) =
+    fun initializeUid(uid: String) {
         screenModelScope.launch(Dispatchers.IO) {
-            exoBuilder.getMediaArtist(mediaUri)
+            player.initializeController()
+            _observeByUid_(uid).collect {
+                runCatching {
+                    _observeByUid.value = mapper.fromDomain(it)
+                }.onSuccess {
+                    currentAudio.value = _observeByUid.value
+                    loadAudio(_observeByUid.value?.uri ?: "")
+                }
+            }
         }
+    }
+
+    fun playbackState() {
+        screenModelScope.launch {
+            when {
+                _uiState.value.isPlaying -> player.pause()
+                else -> player.play()
+            }
+        }
+    }
+
+    fun updateProgress(progress: Float) {
+        val position = currentAudio.value?.duration?.times(progress)?.toLong()
+        player.seekTo(position ?: 0L)
+        _uiState.value = _uiState.value.copy(progress = progress)
+    }
 
     internal fun formatLong(value: Long): String {
         val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
         return dateFormat.format(value)
+    }
+
+    private fun loadAudio(uri: String) {
+        screenModelScope.launch {
+            runCatching {
+                currentAudio.value = repository.loadAudioByUri(uri) ?: return@launch
+                currentAudio.value?.let { player.setAudio(it) }
+                launch {
+                    currentAudio.value?.let {
+                        val amplitudes = repository.loadAudioAmplitudes(it.path)
+                        _uiState.value = _uiState.value.copy(amplitudes = amplitudes)
+                    }
+                }
+                launch { observePlaybackEvents() }
+                _uiState.value = _uiState.value.copy(
+                    displayName = currentAudio.value?.nameWithoutFormat ?: "",
+                )
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
+    }
+
+    private suspend fun observePlaybackEvents() {
+        player.events.collectLatest {
+            when(it) {
+                is PlaybackManager.Event.PositionChanged -> updatePlaybackProgress(it.position)
+                is PlaybackManager.Event.PlayingChanged -> updatePlayingState(it.isPlaying)
+            }
+        }
+    }
+
+    private fun updatePlaybackProgress(position: Long) {
+        val audio = currentAudio.value ?: return
+        _uiState.value = uiState.value.copy(progress = position.toFloat() / audio.duration)
+    }
+
+    private fun updatePlayingState(isPlaying: Boolean) {
+        _uiState.value = uiState.value.copy(isPlaying = isPlaying)
+    }
+
+    override fun onDispose() {
+        super.onDispose()
+        player.releaseController()
     }
 }
