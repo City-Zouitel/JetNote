d6b4495e2ee65f10e8979cc208731fa203396c33
yon <bourntivoli@gmail.com>
2025-01-25 22:39:41 +0100

    feat: Add AudioActivity
    Created a new `AudioActivity` class, which is a basic `ComponentActivity` with an empty `setContent` block. This
     likely serves as the entry point for the audio player feature.

---
+++ ui/features/player/audio/src/main/java/city/zouitel/audio/AudioActivity.kt
@@ -0,0 +1,14 @@
+package city.zouitel.audio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class AudioActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
