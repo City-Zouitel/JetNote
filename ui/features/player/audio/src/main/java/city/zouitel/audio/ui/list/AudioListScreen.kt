2b4777c5b7e6749e3b4b8152c49e0c19b6e23070
yon <bourntivoli@gmail.com>
2025-01-25 22:40:43 +0100

    feat: Add AudioListScreen
    Added a new screen, `AudioListScreen`, which displays a list of audio files.
    
    - The screen uses a `LazyColumn` to display the audio items.
    - Each audio item is represented by an `AudioItem` composable, which shows the audio file's name and size.
    - Clicking on an audio item triggers the `onClick` action provided in the `ItemUiState`.
    - The screen uses the `AudioListScreenModel` to manage its state and data.
    - The `CommonBottomSheet` is used as the container for the audio list.

---
+++ ui/features/player/audio/src/main/java/city/zouitel/audio/ui/list/AudioListScreen.kt
@@ -0,0 +1,109 @@
+package city.zouitel.audio.ui.list
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.getScreenModel
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
+import city.zouitel.audio.state.ItemUiState
+import city.zouitel.systemDesign.CommonBottomSheet
+
+data class AudioListScreen(val uid: String): Screen {
+    @Composable
+    override fun Content() {
+        AudioList(audioListModel = getScreenModel())
+    }
+
+    @Composable
+    private fun AudioList(audioListModel: AudioListScreenModel) {
+        val bottomSheetNavigator = LocalBottomSheetNavigator.current
+        val navigator = LocalNavigator.current
+        val uiState by remember(audioListModel, audioListModel::listUiState).collectAsState()
+
+        LaunchedEffect(true) {
+            audioListModel.updateId(uid)
+                .updateBottomSheetNavigator(bottomSheetNavigator)
+                .updateNavigator(navigator)
+        }
+
+
+        Navigator(
+            CommonBottomSheet {
+                LazyColumn(modifier = Modifier.animateContentSize()) {
+                    items(uiState.audioFiles) {
+                        AudioItem(itemState = it, audioListModel = audioListModel)
+                    }
+                }
+            }
+        )
+    }
+
+    @Composable
+    private fun AudioItem(
+        itemState: ItemUiState,
+        audioListModel: AudioListScreenModel
+    ) {
+        Surface(
+            modifier = Modifier.padding(3.dp),
+            color = MaterialTheme.colorScheme.onSurface,
+            shape = ShapeDefaults.Large,
+            tonalElevation = 0.dp,
+            onClick = {
+                itemState.onClick.invoke()
+                audioListModel.updateNewAudio(false)
+            }
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.onSurface),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                    painter = painterResource(id = city.zouitel.systemDesign.CommonIcons.CASSETTE_ICON),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.surface
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp, vertical = 8.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = itemState.displayName,
+                        color = MaterialTheme.colorScheme.surface
+                    )
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = itemState.size,
+                        color = MaterialTheme.colorScheme.surface
+                    )
+                }
+            }
+        }
+    }
+}
