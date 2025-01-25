4a38f454c02a3b31fe8946df8d5f81c2dcab6844
yon <bourntivoli@gmail.com>
2025-01-25 22:41:09 +0100

    Refactor: Update AudioListUiState to use ItemUiState
    Updated `AudioListUiState` to use `ItemUiState` instead of `Single
    AudioUiState` for representing audio files.
    
    This change likely generalizes the representation of items in the audio list, potentially allowing for different item types in the future.

--- ui/features/audios/src/main/java/city/zouitel/audios/state/AudioListUiState.kt
+++ ui/features/audios/src/main/java/city/zouitel/audios/state/AudioListUiState.kt
@@ -5,7 +5,7 @@ import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
 
 internal data class AudioListUiState(
     val searchQuery: String = "",
-    val audioFiles: List<SingleAudioUiState> = emptyList(),
+    val audioFiles: List<ItemUiState> = emptyList(),
     val isLoadingAudios: Boolean = false,
     val currentId: String = "",
     val bottomSheetNavigator: BottomSheetNavigator? = null,
