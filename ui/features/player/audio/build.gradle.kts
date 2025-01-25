b01a17962aceea44c2b743cd97a10f5ba36d80b1
yon <bourntivoli@gmail.com>
2025-01-25 22:46:03 +0100

    feat: Add audio player feature module
    Created a new feature module for the audio player.
    
    This module includes dependencies for:
    - Data persistence using Data
    Store.
    - Domain logic.
    - System design and common logic components.
    - Waveform visualization.
    - UI components like ConstraintLayout and Accompanist permissions.
    - Sliders using Amplituda.
    - Media playback using Media3.

---
+++ ui/features/player/audio/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.cityzouitel.androidLibrary)
+    alias(libs.plugins.cityzouitel.androidCompose)
+    alias(libs.plugins.cityzouitel.composeVoyager)
+}
+
+android {
+    namespace = "city.zouitel.audio"
+}
+
+dependencies {
+    //Modules.
+    implementation(projects.core.local.datastore)
+    implementation(projects.domain)
+    implementation(projects.common.systemDesign)
+    implementation(projects.common.logic)
+    implementation(projects.ui.features.player.waveform)
+
+    //AndroidX.
+    implementation(libs.constraintlayout)
+
+    //Accompanist.
+    implementation(libs.accompanist.permissions)
+
+    //Sliders.
+//    implementation(libs.audiowaveform)
+    implementation(libs.amplituda)
+
+    //Media3.
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.session)
+    implementation(libs.media3.ui)
+}
