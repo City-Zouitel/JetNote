361d2b1b3f4c79c881498d9d5f4558a22ab6a4fa
yon <bourntivoli@gmail.com>
2025-01-25 22:41:40 +0100

    Refactor: Update AudioMapper to handle nullable Audio entities
    Updated the `AudioMapper` to handle nullable `Audio` entities. This change ensures robustness and prevents
     potential crashes when dealing with optional audio data.
    
    - Modified `toDomain` functions to accept nullable `Audio` objects and provide default values for properties when the input is null.
    - Added documentation to clarify the purpose and behavior of the mapping functions.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/AudioMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/AudioMapper.kt
@@ -1,23 +1,62 @@
 package city.zouitel.repository.mapper
 
+import city.zouitel.repository.model.Audio
 import city.zouitel.domain.model.Audio as OutAudio
-import city.zouitel.repository.model.Audio as InAudio
 
+/**
+ * Mapper class responsible for converting between [Audio] (repository layer representation) and
+ * [OutAudio] (domain layer representation) objects.
+ */
 class AudioMapper {
 
-    fun toDomain(audios: List<InAudio>) = audios.map { toDomain(it) }
+    /**
+     * Converts a list of [Audio] entities to a list of corresponding domain [Audio] models.
+     *
+     * This function iterates through a list of [Audio] entities and applies the [toDomain(Audio)]
+     * conversion function to each entity, resulting in a new list containing the domain
+     * representations of those Audios.
+     *
+     * @param Audios The list of [Audio] entities to convert.
+     * @return A list of [Audio] domain models, where each model corresponds to a
+     *         converted entity from the input list.
+     *
+     * @see toDomain(Audio)
+     */
+    fun toDomain(audios: List<Audio?>) = audios.map { toDomain(it) }
 
-    fun toDomain(audio: InAudio) = OutAudio(
-        id = audio.id,
-        title = audio.title,
-        path = audio.path,
-        uri = audio.uri,
-        size = audio.size,
-        duration = audio.duration
+    /**
+     * Converts a [Audio] entity repository model to an [OutAudio] domain model.
+     *
+     * This function maps the properties of a [Audio] object to a corresponding [OutAudio] object,
+     * effectively transforming a data-layer representation of a Audio into a domain-layer
+     * representation. The mapping includes the Audio's ID, UID, item description, and completion status.
+     *
+     * @param Audio The [Audio] entity to convert.
+     * @return An [OutAudio] domain model representing the provided Audio.
+     */
+    fun toDomain(audio: Audio?) = OutAudio(
+        id = audio?.id ?: 0L,
+        uid = audio?.uid ?: "",
+        title = audio?.title ?: "",
+        path = audio?.path ?: "",
+        uri = audio?.uri ?: "",
+        size = audio?.size ?: 0L,
+        duration = audio?.duration ?: 0L
     )
 
-    fun fromDomain(audio: OutAudio) = InAudio(
+    /**
+     * Converts an [OutAudio] (domain model) to a [Audio] (repository model).
+     *
+     * This function maps the properties of an [OutAudio] to a new [Audio] instance.
+     * It's typically used when transferring data from the domain layer to the
+     * data/presentation layer.
+     *
+     * @param Audio The [OutAudio] object to convert.
+     * @return A new [Audio] object with properties copied from the input [OutAudio].
+     */
+    fun fromDomain(audio: OutAudio) = Audio(
         id = audio.id,
+        uid = audio.uid,
         title = audio.title,
         path = audio.path,
         uri = audio.uri,
