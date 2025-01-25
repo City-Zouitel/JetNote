d9cdf1fba3f37b0d436c64d44d95c98309a56a01
yon <bourntivoli@gmail.com>
2025-01-25 22:39:35 +0100

    feat: Add Audio data class
    Added a new data class `Audio` to the `city.zouitel.audio.model` package.
    
    This
     class represents an audio file and includes properties like id, uid, title, path, uri, size, and duration. It also provides calculated properties for the file name without format and the format extension.

---
+++ ui/features/player/audio/src/main/java/city/zouitel/audio/model/Audio.kt
@@ -0,0 +1,17 @@
+package city.zouitel.audio.model
+
+import androidx.annotation.Keep
+
+@Keep
+data class Audio(
+    var id: Long = 0L,
+    val uid: String = "",
+    var title: String = "Unknown",
+    var path: String = "",
+    var uri: String = "",
+    var size: Long = 0L,
+    var duration: Long = 0L,
+) {
+    val nameWithoutFormat: String get() = title.substringBeforeLast('.')
+    val format: String get() = title.substringAfterLast('.')
+}
