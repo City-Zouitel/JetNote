9f1be88fb8d421356bc935f7b01cb8f9add3fe38
yon <bourntivoli@gmail.com>
2025-01-25 22:39:52 +0100

    Refactor: Update AudioDao to use Upsert and observe methods
    Updated the `AudioDao` to use `Upsert` for inserting or updating audio records
     and added observe methods for retrieving audio data as flows.
    
    - Replaced `addAudio`, `updateAudio`, and `deleteAudio` with `insert`, `deleteByUid`, and `deleteById` respectively.
    - Added `observeAll` and `observeByUid` methods to observe audio data changes.
    -
     Renamed the `AudioEntity` to `Audio`.
    - Updated queries to use constants for table and column names.
    - Added documentation for all methods.

--- core/local/database/src/main/java/city/zouitel/database/dao/AudioDao.kt
+++ core/local/database/src/main/java/city/zouitel/database/dao/AudioDao.kt
@@ -1,30 +1,81 @@
 package city.zouitel.database.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
-import city.zouitel.database.model.AudioEntity
-import city.zouitel.database.utils.Constants
+import androidx.room.Upsert
+import city.zouitel.database.model.Audio
+import city.zouitel.database.model.Audio.Companion.TABLE_NAME
+import city.zouitel.database.utils.Constants.ID
+import city.zouitel.database.utils.Constants.UUID
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Data Access Object (DAO) for interacting with the [Audio] entity in the database.
+ * Provides methods for observing, inserting, and deleting audio records.
+ */
 @Dao
 interface AudioDao {
 
-    @Query("SELECT * FROM AUDIOS_TABLE")
-    fun getAllAudios(): Flow<List<AudioEntity>>
+    /**
+     * Observes all [Audio] entities from the database.
+     *
+     * This property returns a [Flow] that emits a list of [Audio] objects whenever the
+     * underlying data in the `$TABLE_NAME` table changes.  It uses a database query to
+     * select all rows from the table.
+     *
+     * Note: This flow will continuously emit new lists of [Audio] objects as the database
+     * changes. Be mindful of resource consumption when collecting this flow, especially in
+     * UI contexts.
+     *
+     * @return A [Flow] emitting a [List] of all [Audio] entities.
+     */
+    @get:Query("SELECT * FROM $TABLE_NAME")
+    val observeAll: Flow<List<Audio>>
 
-    @Query("SELECT * FROM AUDIOS_TABLE WHERE ${Constants.ID} = :id")
-    suspend fun getAudioById(id: Long): AudioEntity?
+    /**
+     * Observes a single [Audio] entity from the database matching the given UUID.
+     *
+     * This function queries the `$TABLE_NAME` table for an entry where the `$UUID` column
+     * matches the provided [uid]. It then emits the corresponding [Audio] object as a stream
+     * via a [Flow]. If no matching entry is found, the [Flow] will emit nothing and complete.
+     *
+     * @param uid The UUID of the [Audio] entity to observe.
+     * @return A [Flow] emitting the matching [Audio] entity, or nothing if not found.
+     */
+    @Query("SELECT * FROM $TABLE_NAME WHERE $UUID = :uid")
+    fun observeByUid(uid: String): Flow<Audio>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addAudio(audioEntity: AudioEntity)
+    /**
+     * Inserts or updates an [Audio] entity in the data store.
+     *
+     * If an [Audio] with the same primary key (e.g., audioId) already exists,
+     * this function will update the existing record with the new data.
+     * Otherwise, it will insert a new [Audio] record.
+     *
+     * This function is marked as a suspending function, meaning it should be
+     * called within a coroutine or another suspending function.
+     *
+     * @param audio The [Audio] entity to be inserted or updated.
+     */
+    @Upsert
+    suspend fun insert(audio: Audio)
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun updateAudio(audioEntity: AudioEntity)
+    /**
+     * Deletes a record from the [TABLE_NAME] table based on the provided UUID.
+     *
+     * @param uid The UUID of the record to delete.
+     * @throws Exception If any error occurs during the database operation.
+     * @return Unit. The function returns nothing explicitly, but it suspends until the database operation is complete.
+     */
+    @Query("DELETE FROM $TABLE_NAME WHERE $UUID = :uid")
+    suspend fun deleteByUid(uid: String)
 
-    @Delete
-    suspend fun deleteAudio(audioEntity: AudioEntity)
+    /**
+     * Deletes a row from the table with the specified ID.
+     *
+     * @param id The ID of the row to delete.
+     * @throws SQLiteException if an error occurs during the database operation.
+     */
+    @Query("DELETE FROM $TABLE_NAME WHERE $ID = :id")
+    suspend fun deleteById(id: Long)
 }
