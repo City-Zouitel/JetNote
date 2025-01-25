b3a8bb46705d75b28fe7a523b318ebac091a016d
yon <bourntivoli@gmail.com>
2025-01-25 22:41:50 +0100

    Refactor: Update AudioMapper to handle nullable OutAudio
    Updated `AudioMapper` to handle nullable `OutAudio` objects in the `fromDomain` and
     `toDomain` functions.
    
    - Modified `fromDomain` to accept a list of nullable `OutAudio` objects.
    - Modified `fromDomain` to handle null values in the `OutAudio` object when creating an `Audio` object.
    - Modified `toDomain` to accept an `Audio` object.
    - Added uid property to the Audio class.

--- ui/features/audios/src/main/java/city/zouitel/audios/mapper/AudioMapper.kt
+++ ui/features/audios/src/main/java/city/zouitel/audios/mapper/AudioMapper.kt
@@ -1,15 +1,15 @@
 package city.zouitel.audios.mapper
 
-import city.zouitel.audios.mapper.base.Mapper
-import city.zouitel.audios.model.Audio as InAudio
+import city.zouitel.audios.model.Audio
 import city.zouitel.domain.model.Audio as OutAudio
 
 class  AudioMapper {
 
-    fun fromDomain(audios: List<OutAudio>) = audios.map { fromDomain(it) }
+    fun fromDomain(audios: List<OutAudio?>) = audios.map { fromDomain(it) }
 
-    fun toDomain(audio: InAudio) = OutAudio(
+    fun toDomain(audio: Audio) = OutAudio(
         id = audio.id,
+        uid = audio.uid,
         title = audio.title,
         path = audio.path,
         uri = audio.uri,
@@ -17,12 +17,14 @@ class  AudioMapper {
         duration = audio.duration
     )
 
-    fun fromDomain(audio: OutAudio) = InAudio(
-        id = audio.id,
-        title = audio.title,
-        path = audio.path,
-        uri = audio.uri,
-        size = audio.size,
-        duration = audio.duration
+
+    fun fromDomain(audio: OutAudio?) = Audio(
+        id = audio?.id ?: 0L,
+        uid = audio?.uid ?: "",
+        title = audio?.title ?: "",
+        path = audio?.path ?: "",
+        uri = audio?.uri ?: "",
+        size = audio?.size ?: 0L,
+        duration = audio?.duration ?: 0L
     )
 }
