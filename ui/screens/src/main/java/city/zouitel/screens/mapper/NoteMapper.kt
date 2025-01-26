44f4c6662cb7df2d592f84cbe5517df9430fea91
yon <bourntivoli@gmail.com>
2025-01-26 20:48:48 +0100

    Refactor: Remove AudioEntity mapping from NoteMapper
    Removed the mapping of `AudioEntity` from the `NoteMapper`.
    The `audioMapper` dependency and the corresponding mapping logic for `audioEntities` within the `fromDomain` function were removed.
    
    This change likely simplifies the `NoteMapper` by removing the responsibility of handling audio-related entities.

--- ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
+++ ui/screens/src/main/java/city/zouitel/screens/mapper/NoteMapper.kt
@@ -1,6 +1,5 @@
 package city.zouitel.screens.mapper
 
-import city.zouitel.audios.mapper.AudioMapper
 import city.zouitel.note.mapper.DataMapper
 import city.zouitel.note.model.Note
 import city.zouitel.tags.mapper.TagMapper
@@ -9,7 +8,6 @@ import city.zouitel.domain.model.Note as OutNote
 class NoteMapper(
     private val dataMapper: DataMapper,
     private val tagMapper: TagMapper,
-    private val audioMapper: AudioMapper
 ) {
     fun fromDomain(notes: List<OutNote>) = notes.map { fromDomain(it) }
 
@@ -17,7 +15,6 @@ class NoteMapper(
         Note(
             dataEntity = dataMapper.fromDomain(dataEntity),
             tagEntities = tagEntities.map { tagMapper.fromDomain(it) },
-            audioEntities = audioEntities.map { audioMapper.fromDomain(it) }
         )
     }
 }
