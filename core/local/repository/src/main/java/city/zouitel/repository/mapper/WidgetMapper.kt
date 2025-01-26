e4618b7cb3415a3d6a415b3f9ee55e6bcec73b76
yon <bourntivoli@gmail.com>
2025-01-26 20:54:22 +0100

    Refactor: Remove Audio Entities from WidgetMapper
    Removed the `audioMapper` dependency and the mapping of `audioEntities` from the `WidgetMapper`. This
     change likely simplifies the widget data model by removing audio-related information.

--- core/local/repository/src/main/java/city/zouitel/repository/mapper/WidgetMapper.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/mapper/WidgetMapper.kt
@@ -5,16 +5,14 @@ import city.zouitel.repository.model.Note as InNote
 
 class WidgetMapper(
     private val dataMapper: DataMapper,
-    private val tagMapper: TagMapper,
-    private val audioMapper: AudioMapper
+    private val tagMapper: TagMapper
 ) {
     fun toDomain(notes: List<InNote>) = notes.map { toDomain(it) }
 
     fun toDomain(data: InNote): OutNote = with(data){
         OutNote(
             dataEntity = dataMapper.toDomain(dataEntity),
-            tagEntities = tagEntities.map { tagMapper.toDomain(it) },
-            audioEntities = audioEntities.map { audioMapper.toDomain(it) }
+            tagEntities = tagEntities.map { tagMapper.toDomain(it) }
         )
     }
 }
