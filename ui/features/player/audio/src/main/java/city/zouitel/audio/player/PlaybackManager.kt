b002fa856ecd349d25a6ffa08f15248895c8c981
yon <bourntivoli@gmail.com>
2025-01-26 20:49:33 +0100

    feat: Implement Audio Playback Manager
    This commit introduces a new `PlaybackManager` class responsible for managing audio playback using Media3.
    
    - It utilizes `Media
    SessionService` and `MediaController` for background playback and external control.
    - It exposes events for position changes and playback state updates via a `MutableSharedFlow`.
    - The manager provides functions for setting audio, playing, pausing, seeking, initializing, and releasing the controller.
    - It also handles automatic pausing
     and seeking to the beginning at the end of playback.

---
+++ ui/features/player/audio/src/main/java/city/zouitel/audio/player/PlaybackManager.kt
@@ -0,0 +1,149 @@
+package city.zouitel.audio.player
+
+import android.content.ComponentName
+import android.content.Context
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import androidx.core.net.toUri
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+import androidx.media3.common.Player
+import androidx.media3.session.MediaController
+import androidx.media3.session.MediaSessionService
+import androidx.media3.session.SessionToken
+import city.zouitel.audio.model.Audio
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.runBlocking
+import java.util.UUID
+
+class PlaybackManager (
+    private val context: Context,
+    private val playService: MediaSessionService
+): Player.Listener {
+
+    companion object {
+        private const val PLAYER_POSITION_UPDATE_TIME = 500L
+    }
+
+    val events: MutableSharedFlow<Event> = MutableSharedFlow()
+
+    private var lastEmittedPosition: Long = 0
+    private var currentMediaItem: MediaItem? = null
+    private var controllerFuture: ListenableFuture<MediaController>? = null
+    private val controller: MediaController?
+        get() = if (controllerFuture?.isDone == true) controllerFuture?.get() else null
+
+    private var handler: Handler? = null
+
+    private val playerPositionRunnable = object : Runnable {
+        override fun run() {
+            val playbackPosition = controller?.currentPosition ?: 0
+            // Emit only new player position
+            if(playbackPosition != lastEmittedPosition) {
+                sendEvent(Event.PositionChanged(playbackPosition))
+                lastEmittedPosition = playbackPosition
+            }
+            handler?.postDelayed(this, PLAYER_POSITION_UPDATE_TIME)
+        }
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        super.onIsPlayingChanged(isPlaying)
+        sendEvent(Event.PlayingChanged(isPlaying))
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+        if(playbackState == Player.STATE_ENDED) {
+            controller?.pause()
+            controller?.seekTo(0)
+        }
+    }
+
+    fun setAudio(audio: Audio) {
+        setAudio(audio.uri.toUri(), audio.nameWithoutFormat)
+    }
+
+    private fun setAudio(uri: Uri, title: String) {
+        setAudio(getMediaItem(uri, title))
+    }
+
+    fun setAudio(mediaItem: MediaItem) {
+        val controllerItemId = controller?.currentMediaItem?.mediaId
+        currentMediaItem = mediaItem
+        if(controllerFuture?.isDone == false || controllerItemId == mediaItem.mediaId) {
+            return
+        }
+        controller?.setMediaItem(mediaItem)
+        controller?.prepare()
+    }
+
+    private fun clearAudio() {
+        controller?.stop()
+        controller?.clearMediaItems()
+        currentMediaItem = null
+    }
+
+    fun play() {
+        controller?.play()
+    }
+
+    fun pause() {
+        controller?.pause()
+    }
+
+    fun seekTo(position: Long) {
+        controller?.seekTo(position)
+    }
+
+    fun initializeController() {
+        controllerFuture = MediaController.Builder(
+            context,
+            SessionToken(context, ComponentName(context, playService::class.java))
+        ).buildAsync()
+        controllerFuture?.addListener(::onControllerCreated, MoreExecutors.directExecutor())
+    }
+
+    fun releaseController() {
+        clearAudio()
+        controller?.removeListener(this)
+        controllerFuture?.let(MediaController::releaseFuture)
+        controllerFuture = null
+        handler?.removeCallbacks(playerPositionRunnable)
+        handler = null
+    }
+
+    private fun getMediaItem(uri: Uri, title: String): MediaItem {
+        val mmd = MediaMetadata.Builder()
+            .setTitle(title)
+            .build()
+        val rmd = MediaItem.RequestMetadata.Builder()
+            .setMediaUri(uri)
+            .build()
+        return MediaItem.Builder()
+            .setMediaId(UUID.randomUUID().toString())
+            .setMediaMetadata(mmd)
+            .setRequestMetadata(rmd)
+            .build()
+    }
+
+    private fun onControllerCreated() {
+        currentMediaItem?.let(::setAudio)
+        handler = Handler(Looper.getMainLooper())
+        handler?.postDelayed(playerPositionRunnable, PLAYER_POSITION_UPDATE_TIME)
+        controller?.addListener(this)
+    }
+
+    private fun sendEvent(event: Event) {
+        runBlocking { events.emit(event) }
+    }
+
+    sealed interface Event {
+        data class PositionChanged(val position: Long) : Event
+        data class PlayingChanged(val isPlaying: Boolean) : Event
+    }
+
+}
