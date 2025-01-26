2234c4969df846dcca18e2dc08c0f13bc261a707
yon <bourntivoli@gmail.com>
2025-01-26 17:44:41 +0100

    Refactor: Rename network repository package and module
    Renamed the network repository package from `city.zouitel.repository` to `city.zouitel.
    networkRepo`.
    
    - Renamed the repository module from `networkRepositoryKoinModule` to `networkRepoDIModule`.
    - Updated import statements to reflect the package change.

--- core/network/networkRepo/src/main/java/city/zouitel/networkRepo/di/modules.kt
+++ core/network/networkRepo/src/main/java/city/zouitel/networkRepo/di/modules.kt
@@ -1,15 +1,15 @@
-package city.zouitel.repository.di
+package city.zouitel.networkRepo.di
 
 import city.zouitel.domain.repository.MessageRepository
 import city.zouitel.domain.usecase.MessageUseCase
-import city.zouitel.repository.mapper.MessageMapper
-import city.zouitel.repository.repositoryImpl.GeminiRepositoryImpl
+import city.zouitel.networkRepo.mapper.MessageMapper
+import city.zouitel.networkRepo.repositoryImpl.GeminiRepositoryImpl
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
-val networkRepositoryKoinModule = module {
+val networkRepoDIModule = module {
     factoryOf(::MessageMapper)
     singleOf(::GeminiRepositoryImpl) bind MessageRepository::class
 
