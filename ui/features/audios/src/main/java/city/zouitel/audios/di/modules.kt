c9ca9a9cdca03b9d9d1c0d59a6f5563e2e74566b
yon <bourntivoli@gmail.com>
2025-01-26 17:44:26 +0100

    Refactor: Remove NoteAndAudio related dependencies and introduce PlaybackManager
    Removed dependencies related to NoteAndAudio functionality, including `NoteAndAudioMapper` and `NoteAndAudioScreenModel`.
    
    Modified `AudioScreenModel` and `AudioListScreenModel` to remove NoteAndAudio related parameters.
    
    Introduced `PlaybackManager` as a singleton, likely for managing audio playback. This refactoring likely focuses on streamlining audio functionality and removing unused or redundant code related to notes.

--- ui/features/audios/src/main/java/city/zouitel/audios/di/modules.kt
+++ ui/features/audios/src/main/java/city/zouitel/audios/di/modules.kt
@@ -5,22 +5,19 @@ import city.zouitel.audios.audio.AudioManager
 import city.zouitel.audios.audio.AudioRepository
 import city.zouitel.audios.audio.LocalMediaDataSource
 import city.zouitel.audios.mapper.AudioMapper
-import city.zouitel.audios.mapper.NoteAndAudioMapper
+import city.zouitel.audios.player.PlaybackManager
 import city.zouitel.audios.ui.component.AudioScreenModel
-import city.zouitel.audios.ui.component.NoteAndAudioScreenModel
 import city.zouitel.audios.ui.list.AudioListScreen
 import city.zouitel.audios.ui.list.AudioListScreenModel
 import city.zouitel.domain.exoplayer.ExoPlayerImpl
 import city.zouitel.domain.exoplayer.ExoRepo
 import city.zouitel.domain.provider.SharedScreen
-import city.zouitel.systemDesign.CommonConstants.REC_DIR
 import com.google.android.exoplayer2.ExoPlayer
 import kotlinx.coroutines.Dispatchers
 import linc.com.amplituda.Amplituda
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
-import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -33,14 +30,6 @@ val audioPlayerDIModule = module {
         }
     }
 
-//    single {
-//        AudioAttributes.Builder()
-//            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-//            .setUsage(AudioAttributes.USAGE_MEDIA)
-//            .setLegacyStreamType(android.media.AudioManager.STREAM_MUSIC)
-//            .build()
-//    }
-
     singleOf(::ExoPlayerImpl) bind ExoRepo::class
 
     single { Amplituda(androidContext()) }
@@ -48,12 +37,12 @@ val audioPlayerDIModule = module {
     single { AudioRepository(get(), get(), get()) }
     single { LocalMediaDataSource(androidContext(), get()) }
 
-    factory { AudioScreenModel(get(), get(),get(),get(), get(named(REC_DIR))) }
-    factory { AudioListScreenModel(get(), get(), get(), get(), get(), get(), get(), get(), get()) }
-    factory { NoteAndAudioScreenModel(get(), get(), get()) }
+    factory { AudioScreenModel(get(), get(),get(),get(), get()) }
+    factory { AudioListScreenModel(get(), get(), get(), get(), get()) }
 
     factoryOf(::AudioMapper)
-    factoryOf(::NoteAndAudioMapper)
+
+    single { PlaybackManager(androidContext(), get()) }
 }
 
 val audioPlayerScreenModule = screenModule {
