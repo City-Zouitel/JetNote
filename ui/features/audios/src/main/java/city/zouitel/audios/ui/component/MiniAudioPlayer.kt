9ea13e8cdaeb1731dfadc6d51190c4778e977186
yon <bourntivoli@gmail.com>
2025-01-26 17:45:06 +0100

    Refactor: Remove redundant code and update logic in MiniAudioPlayer
    Removed redundant code related to `isPlaying` in the `MiniAudioPlayer` component. Updated
     the playback logic to leverage the `DataStoreScreenModel` directly instead of local states.
    
    This change streamlines the audio player's behavior and potentially improves playback control.

--- ui/features/audios/src/main/java/city/zouitel/audios/ui/component/MiniAudioPlayer.kt
+++ ui/features/audios/src/main/java/city/zouitel/audios/ui/component/MiniAudioPlayer.kt
@@ -6,8 +6,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,14 +28,12 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import city.zouitel.audios.model.Audio
-import city.zouitel.systemDesign.CommonRow
-import city.zouitel.systemDesign.CommonConstants
 import city.zouitel.systemDesign.CommonConstants.LIST
-import city.zouitel.systemDesign.DataStoreScreenModel
 import city.zouitel.systemDesign.CommonIcons
+import city.zouitel.systemDesign.CommonRow
+import city.zouitel.systemDesign.DataStoreScreenModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.io.File
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
@@ -49,7 +60,7 @@ fun MiniAudioPlayer(
         }
     }
 
-    if (isPlaying.value) audioScreenModel.playMedia() else audioScreenModel.pauseMedia()
+//    if (isPlaying.value) audioScreenModel.playbackState() else audioScreenModel.pause()
 
     Card(
         modifier = Modifier.padding(10.dp),
