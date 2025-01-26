bed7b5bed08c44fd81b19901bfb3c217c4767c32
yon <bourntivoli@gmail.com>
2025-01-26 20:47:32 +0100

    Refactor: Remove Audio Relation from NoteEntity
    Removed the `@Relation` annotation and associated properties for linking `NoteEntity` with `AudioEntity` through
     `NoteAndAudioEntity`. This suggests that notes are no longer directly related to audio entities in the database schema.

--- core/local/database/src/main/java/city/zouitel/database/model/relational/NoteEntity.kt
+++ core/local/database/src/main/java/city/zouitel/database/model/relational/NoteEntity.kt
@@ -3,9 +3,7 @@ package city.zouitel.database.model.relational
 import androidx.room.Embedded
 import androidx.room.Junction
 import androidx.room.Relation
-import city.zouitel.database.model.AudioEntity
 import city.zouitel.database.model.DataEntity
-import city.zouitel.database.model.NoteAndAudioEntity
 import city.zouitel.database.model.NoteAndTagEntity
 import city.zouitel.database.model.TagEntity
 import city.zouitel.database.utils.Constants.ID
@@ -21,14 +19,5 @@ data class NoteEntity(
             entityColumn = "labelId"
         )
     )
-    val tagEntities :List<TagEntity>,
-    @Relation(
-        parentColumn = UUID, entityColumn = ID, entity = AudioEntity::class,
-        associateBy = Junction(
-            NoteAndAudioEntity::class,
-            parentColumn = "noteUid",
-            entityColumn = "audioId"
-        )
-    )
-    val audioEntities: List<AudioEntity>
+    val tagEntities :List<TagEntity>
 )
