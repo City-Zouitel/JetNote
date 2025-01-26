783a7c419c3bc16b23d55b6cf040661a5c5749d2
yon <bourntivoli@gmail.com>
2025-01-26 20:44:38 +0100

    Refactor: Remove audioEntities from Note model
    Removed the `audioEntities` property from the `Note` model class. This change likely simplifies the data structure
     of notes by removing the direct association with audio entities.

--- ui/features/workplace/src/main/java/city/zouitel/note/model/Note.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/model/Note.kt
@@ -1,13 +1,10 @@
 package city.zouitel.note.model
 
 import androidx.annotation.Keep
-import city.zouitel.audios.model.Audio
-import city.zouitel.links.model.Link
 import city.zouitel.tags.model.Tag
 
 @Keep
 data class Note(
     val dataEntity: Data,
     val tagEntities: List<Tag>,
-    val audioEntities: List<Audio>
 )
