9c123715428ca02920e16fe60b893f653b6b24b3
yon <bourntivoli@gmail.com>
2025-01-26 20:53:40 +0100

    feat: Add Waveform composable
    This commit introduces a new `Waveform` composable that displays a waveform visualization of audio data.
    
    The `Waveform` composable takes parameters such as:
    - `waveformBrush` and `progressBrush` to customize the colors of the waveform and progress indicator.
    - `waveformAlignment` to control the vertical alignment of the waveform.
    - `amplitudeType` to specify how the amplitudes are calculated.
    
    - `onProgressChange` and `onProgressChangeFinished` callbacks to handle progress updates.
    - `spikeWidth`, `spikeRadius`, and `spikePadding` to customize the appearance of the waveform spikes.
    - `progress` to indicate the playback progress.
    - `amplitudes` a list of audio amplitudes to be visualized.
    
    This composable enables the creation of interactive waveform visualizations within Jetpack Compose applications.

---
+++ ui/features/player/waveform/src/main/java/city/zouitel/waveform/Waveform.kt
@@ -0,0 +1,156 @@
+package city.zouitel.waveform
+
+import android.view.MotionEvent
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.coerceIn
+import androidx.compose.ui.unit.dp
+import city.zouitel.waveform.model.AmplitudeType
+import city.zouitel.waveform.model.WaveformAlignment
+
+private val MinSpikeWidthDp: Dp = 1.dp
+private val MaxSpikeWidthDp: Dp = 24.dp
+private val MinSpikePaddingDp: Dp = 0.dp
+private val MaxSpikePaddingDp: Dp = 12.dp
+private val MinSpikeRadiusDp: Dp = 0.dp
+private val MaxSpikeRadiusDp: Dp = 12.dp
+
+private const val MinProgress: Float = 0F
+private const val MaxProgress: Float = 1F
+
+private const val MinSpikeHeight: Float = 1F
+private const val DefaultGraphicsLayerAlpha: Float = 0.99F
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun Waveform(
+    modifier: Modifier = Modifier,
+    style: DrawStyle = Fill,
+    waveformBrush: Brush = SolidColor(Color.White),
+    progressBrush: Brush = SolidColor(Color.Blue),
+    waveformAlignment: WaveformAlignment = WaveformAlignment.Center,
+    amplitudeType: AmplitudeType = AmplitudeType.Avg,
+    onProgressChangeFinished: (() -> Unit)? = null,
+    spikeAnimationSpec: AnimationSpec<Float> = tween(500),
+    spikeWidth: Dp = 4.dp,
+    spikeRadius: Dp = 2.dp,
+    spikePadding: Dp = 1.dp,
+    progress: Float = 0F,
+    amplitudes: List<Int>,
+    onProgressChange: (Float) -> Unit
+) {
+    val _progress = remember(progress) { progress.coerceIn(MinProgress, MaxProgress) }
+    val _spikeWidth = remember(spikeWidth) { spikeWidth.coerceIn(MinSpikeWidthDp, MaxSpikeWidthDp) }
+    val _spikePadding = remember(spikePadding) { spikePadding.coerceIn(MinSpikePaddingDp, MaxSpikePaddingDp) }
+    val _spikeRadius = remember(spikeRadius) { spikeRadius.coerceIn(MinSpikeRadiusDp, MaxSpikeRadiusDp) }
+    val _spikeTotalWidth = remember(spikeWidth, spikePadding) { _spikeWidth + _spikePadding }
+    var canvasSize by remember { mutableStateOf(Size(0f, 0f)) }
+    var spikes by remember { mutableStateOf(0F) }
+    val spikesAmplitudes = remember(amplitudes, spikes, amplitudeType) {
+        amplitudes.toDrawableAmplitudes(
+            amplitudeType = amplitudeType,
+            spikes = spikes.toInt(),
+            minHeight = MinSpikeHeight,
+            maxHeight = canvasSize.height.coerceAtLeast(MinSpikeHeight)
+        )
+    }.map { animateFloatAsState(it, spikeAnimationSpec).value }
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .requiredHeight(48.dp)
+            .graphicsLayer(alpha = DefaultGraphicsLayerAlpha)
+            .pointerInteropFilter {
+                return@pointerInteropFilter when (it.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (it.x in 0F..canvasSize.width) {
+                            onProgressChange(it.x / canvasSize.width)
+                            true
+                        } else false
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        onProgressChangeFinished?.invoke()
+                        true
+                    }
+                    else -> false
+                }
+            }
+            .then(modifier)
+    ) {
+        canvasSize = size
+        spikes = size.width / _spikeTotalWidth.toPx()
+        spikesAmplitudes.forEachIndexed { index, amplitude ->
+            drawRoundRect(
+                brush = waveformBrush,
+                topLeft = Offset(
+                    x = index * _spikeTotalWidth.toPx(),
+                    y = when(waveformAlignment) {
+                        WaveformAlignment.Top -> 0F
+                        WaveformAlignment.Bottom -> size.height - amplitude
+                        WaveformAlignment.Center -> size.height / 2F - amplitude / 2F
+                    }
+                ),
+                size = Size(
+                    width = _spikeWidth.toPx(),
+                    height = amplitude
+                ),
+                cornerRadius = CornerRadius(_spikeRadius.toPx(), _spikeRadius.toPx()),
+                style = style
+            )
+            drawRect(
+                brush = progressBrush,
+                size = Size(
+                    width = _progress * size.width,
+                    height = size.height
+                ),
+                blendMode = BlendMode.SrcAtop
+            )
+        }
+    }
+}
+
+private fun List<Int>.toDrawableAmplitudes(
+    amplitudeType: AmplitudeType,
+    spikes: Int,
+    minHeight: Float,
+    maxHeight: Float
+): List<Float> {
+    val amplitudes = map(Int::toFloat)
+    if(amplitudes.isEmpty() || spikes == 0) {
+        return List(spikes) { minHeight }
+    }
+    val transform = { data: List<Float> ->
+        when(amplitudeType) {
+            AmplitudeType.Avg -> data.average()
+            AmplitudeType.Max -> data.max()
+            AmplitudeType.Min -> data.min()
+        }.toFloat().coerceIn(minHeight, maxHeight)
+    }
+    return when {
+        spikes > amplitudes.count() -> amplitudes.fillToSize(spikes, transform)
+        else -> amplitudes.chunkToSize(spikes, transform)
+    }.normalize(minHeight, maxHeight)
+}
