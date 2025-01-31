8995013df8205608fb0a26fe861b8e0736db85ba
yon <bourntivoli@gmail.com>
2025-01-31 19:40:58 +0100

    Refactor: Replace UiEvent with Action for NoteAndTag operations
    - Replaced the `UiEvent` system with a new `Action` based approach for
     managing NoteAndTag operations.
    - Removed `UiEventHandler` interface and the `performUiEvent` method.
    - Refactored `NoteAndTagScreenModel` to handle actions using a `sendAction` function.
    - Changed from using `getAll` to `_observeAll_` which return `Flow`.
    - Changed from `add` to `insert`, and from `delete` to `deleteById`.
    - Instead of `performUiEvent` now it use `withAsync` and `withFlow`.
    - Instead of using `UiEvent.Insert` and `UiEvent.Delete`, now `sendAction` function use `Action.Insert` and `Action.DeleteById`.
    - Now `observeAll` return `StateFlow` after using `withFlow`.
    - Simplified the logic for adding and deleting NoteAndTag entries.
    - Removed the unnecessary `stateIn` logic.

--- ui/features/tags/src/main/java/city/zouitel/tags/ui/NoteAndTagScreenModel.kt
+++ ui/features/tags/src/main/java/city/zouitel/tags/ui/NoteAndTagScreenModel.kt
@@ -1,52 +1,35 @@
 package city.zouitel.tags.ui
 
 import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.NoteAndTagUseCase
-import city.zouitel.logic.events.UiEvent
-import city.zouitel.logic.events.UiEventHandler
+import city.zouitel.domain.utils.Action
+import city.zouitel.domain.utils.withAsync
+import city.zouitel.logic.withFlow
 import city.zouitel.tags.mapper.NoteAndTagMapper
 import city.zouitel.tags.model.NoteAndTag
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
-import city.zouitel.tags.model.NoteAndTag as InNoteAndTag
 
 class NoteAndTagScreenModel(
-    private val getAll: NoteAndTagUseCase.GetAllNotesAndTags,
-    private val add: NoteAndTagUseCase.AddNoteAndTag,
-    private val delete: NoteAndTagUseCase.DeleteNoteAndTag,
+    private val _observeAll_: NoteAndTagUseCase.ObserveAll,
+    private val insert: NoteAndTagUseCase.Insert,
+    private val deleteById: NoteAndTagUseCase.DeleteById,
     private val mapper: NoteAndTagMapper
-): ScreenModel, UiEventHandler<NoteAndTag> {
+): ScreenModel {
 
-    private val _getAllNotesAndTags = MutableStateFlow<List<InNoteAndTag>>(emptyList())
-    val getAllNotesAndTags: StateFlow<List<InNoteAndTag>>
-        get() = _getAllNotesAndTags
-            .stateIn(
-                screenModelScope,
-                SharingStarted.WhileSubscribed(),
-                emptyList()
-            )
-
-    init {
-        performUiEvent {
-            getAll.invoke().collect { notesAndTag ->
-                _getAllNotesAndTags.value = mapper.fromDomain(notesAndTag)
+    private val _observeAll = MutableStateFlow<List<NoteAndTag>>(emptyList())
+    val observeAll: StateFlow<List<NoteAndTag>>
+        get() = _observeAll
+            .withFlow(emptyList()) {
+                withAsync {
+                    _observeAll_().collect { _observeAll.value = mapper.fromDomain(it) }
+                }
             }
-        }
-    }
 
-    override fun performUiEvent(action: suspend () -> Unit) {
-        screenModelScope.launch(Dispatchers.IO) { action.invoke() }
-    }
-
-    override fun sendUiEvent(event: UiEvent<NoteAndTag>) {
-        when (event) {
-            is UiEvent.Insert -> performUiEvent { add(mapper.toDomain(event.data)) }
-            is UiEvent.Delete -> performUiEvent { /*delete(mapper.toDomain(event.data))*/ }
+    fun sendAction(act: Action) {
+        when (act) {
+            is Action.Insert<*> -> withAsync { insert(mapper.toDomain(act.data as NoteAndTag)) }
+            is Action.DeleteById -> withAsync { deleteById(act.id as Long) }
             else -> throw Exception("Not implemented")
         }
     }
