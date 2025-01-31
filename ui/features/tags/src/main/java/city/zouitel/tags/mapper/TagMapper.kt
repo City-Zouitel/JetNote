433fcc0c29cbd0828c08f495e905570017048b04
yon <bourntivoli@gmail.com>
2025-01-31 19:43:36 +0100

    Refactor: Simplify TagMapper by removing unnecessary base class
    Removed the `Mapper` base class and simplified the `TagMapper`
     by directly mapping between `Tag` and `OutTag` domain models.
    
    This change streamlines the mapper logic and reduces code complexity.

--- ui/features/tags/src/main/java/city/zouitel/tags/mapper/TagMapper.kt
+++ ui/features/tags/src/main/java/city/zouitel/tags/mapper/TagMapper.kt
@@ -1,20 +1,19 @@
 package city.zouitel.tags.mapper
 
+import city.zouitel.tags.model.Tag
 import city.zouitel.domain.model.Tag as OutTag
-import city.zouitel.tags.mapper.base.Mapper
-import city.zouitel.tags.model.Tag as InTag
 
 class TagMapper {
 
     fun fromDomain(tags: List<OutTag>) = tags.map { fromDomain(it) }
 
-    fun toDomain(tag: InTag) = OutTag(
+    fun toDomain(tag: Tag) = OutTag(
         id = tag.id,
         label = tag.label,
         color = tag.color
     )
 
-    fun fromDomain(tag: OutTag) = InTag(
+    fun fromDomain(tag: OutTag) = Tag(
         id = tag.id,
         label = tag.label,
         color = tag.color
