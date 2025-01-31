eda516c4208de66a8a23833fb460d827d7c46b02
yon <bourntivoli@gmail.com>
2025-01-31 19:40:02 +0100

    Refactor: Update NoteAndTagMapper for clarity and consistency
    - Renamed `NoteAndTagEntity` to `NoteAndTag` for clarity.
    
    - Modified `toRepo` and `fromRepo` functions to work with updated `NoteAndTag` and `OutNoteAndTag`.
    - Added documentation to `NoteAndTagMapper`, `toRepo` and `fromRepo` for better code understanding.
    - Refactored `toRepo` method to
     receive `NoteAndTag` and return `OutNoteAndTag`.
    - Refactored `fromRepo` method to receive `OutNoteAndTag` and return `NoteAndTag`.
    - Updated properties names from `noteUid`, `labelId` to `uid` and `id` in `NoteAndTag` model.
    
    This change enhances the mapping between repository and database layers for Note and Tag relations, improving the overall clarity and maintainability of the code.

--- core/local/database/src/main/java/city/zouitel/database/mapper/NoteAndTagMapper.kt
+++ core/local/database/src/main/java/city/zouitel/database/mapper/NoteAndTagMapper.kt
@@ -1,20 +1,44 @@
 package city.zouitel.database.mapper
 
-import city.zouitel.database.model.NoteAndTagEntity
-import city.zouitel.database.model.NoteAndTagEntity as InNoteAndTag
+import city.zouitel.database.model.NoteAndTag
 import city.zouitel.repository.model.NoteAndTag as OutNoteAndTag
 
+/**
+ * A mapper class responsible for converting between repository [NoteAndTag] and database [OutNoteAndTag].
+ * This class provides methods for mapping lists of NoteAndTag and individual medias between the two types.
+ */
 class NoteAndTagMapper {
+    /**
+     * Maps a list of NoteAndTag objects to a list of their repository representations.
+     *
+     * @param NoteAndTags The list of NoteAndTags objects to map.
+     * @return A list containing the repository representations of the given NoteAndTags
+     * that are mapped from NoteAndTag to OutNoteAndTag.
+     */
+    fun toRepo(notesAndTag: List<NoteAndTag>) = notesAndTag.map { toRepo(it) }
 
-    fun toRepo(notesAndTag: List<InNoteAndTag>) = notesAndTag.map { toRepo(it) }
-
-    private fun toRepo(noteAndTag: InNoteAndTag) = OutNoteAndTag(
-        noteUid = noteAndTag.noteUid,
-        labelId = noteAndTag.labelId
+    /**
+     * Maps a Task object to an OutNoteAndTag object.
+     *
+     * This function converts a Task object, which is typically used internally,
+     * to an OutNoteAndTag object, which is likely used for external communication or data transfer.
+     *
+     * @param NoteAndTag The input NoteAndTag object to be mapped.
+     * @return An OutNoteAndTag object representing the mapped NoteAndTag.
+     */
+    private fun toRepo(noteAndTag: NoteAndTag) = OutNoteAndTag(
+        uid = noteAndTag.uid,
+        id = noteAndTag.id
     )
 
-    fun fromRepo(noteAndTag: OutNoteAndTag) = InNoteAndTag(
-        noteUid = noteAndTag.noteUid,
-        labelId = noteAndTag.labelId
+    /**
+     * Maps an OutNoteAndTag data object from the repository layer to a NoteAndTag data object for the repository layer.
+     *
+     * @param NoteAndTag The OutNoteAndTag object to map.
+     * @return A NoteAndTag object representing the same data.
+     */
+    fun fromRepo(noteAndTag: OutNoteAndTag) = NoteAndTag(
+        uid = noteAndTag.uid,
+        id = noteAndTag.id
     )
 }
