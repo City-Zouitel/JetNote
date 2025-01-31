b127aa03ed57185385638258e504f892b103615d
yon <bourntivoli@gmail.com>
2025-01-31 19:40:31 +0100

    Refactor: Improve NoteAndTagRepository Implementation
    - Renamed `getAllNotesAndTags` to `observeAll` for better clarity and consistency.
    -
     Replaced `addNoteAndTag` with `insert` for more general-purpose data insertion.
    - Replaced `deleteNoteAndTag` with `deleteByUid` and added a new `deleteById` function for flexibility in deleting by different identifiers.
    - Added KDoc documentation for class and all the
     function to enhance code readability and maintainability.
    - Updated function parameter names for clarity.
    
    These changes enhance the repository's readability, maintainability, and flexibility. They also improve the consistency of the naming conventions used within the repository.

--- core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/NoteAndTagRepositoryImpl.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/repositoryImpl/NoteAndTagRepositoryImpl.kt
@@ -1,24 +1,74 @@
 package city.zouitel.repository.repositoryImpl
 
-import city.zouitel.domain.model.NoteAndTag as OutNoteAndTag
 import city.zouitel.domain.repository.NoteAndTagRepository
 import city.zouitel.repository.datasource.NoteAndTagDataSource
 import city.zouitel.repository.mapper.NoteAndTagMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import city.zouitel.domain.model.NoteAndTag as OutNoteAndTag
 
+/**
+ * Implementation of the [NoteAndTagRepository] interface.
+ *
+ * This class handles the data access logic for note and tag associations,
+ * interacting with a [NoteAndTagDataSource] for data persistence and a
+ * [NoteAndTagMapper] for data transformations between the data layer and the domain layer.
+ *
+ * @property dataSource The data source for note and tag associations.
+ * @property mapper The mapper for converting between data and domain layer representations of note and tag associations.
+ */
 class NoteAndTagRepositoryImpl(
     private val dataSource: NoteAndTagDataSource,
     private val mapper: NoteAndTagMapper
 ): NoteAndTagRepository {
-    override val getAllNotesAndTags: Flow<List<OutNoteAndTag>>
-        get() = dataSource.getAllNotesAndTags.map { notesAndTag -> mapper.toDomain(notesAndTag) }
+    /**
+     * A [Flow] that emits a list of [OutNoteAndTag] whenever there is a change in the underlying data source.
+     *
+     * This property provides a reactive stream of all notes and their associated tags.  It observes changes
+     * in the data source, and when a change occurs, it transforms the raw data from the data source
+     * ([dataSource.observeAll]) into domain objects ([OutNoteAndTag]) using the [mapper].
+     *
+     * @see dataSource.observeAll for the underlying data source flow.
+     * @see mapper.toDomain for the transformation logic.
+     *
+     * @return A [Flow] emitting a [List] of [OutNoteAndTag] objects.
+     */
+    override val observeAll: Flow<List<OutNoteAndTag>>
+        get() = dataSource.observeAll.map { mapper.toDomain(it) }
 
-    override suspend fun addNoteAndTag(noteAndTag: OutNoteAndTag) {
-        dataSource.addNoteAndTag(mapper.fromDomain(noteAndTag))
+    /**
+     * Inserts a [OutNoteAndTag] into the data source.
+     *
+     * This function takes a [OutNoteAndTag] object, maps it to its data source representation using the
+     * configured [mapper], and then inserts it into the underlying data source. This operation is
+     * performed asynchronously as a suspending function.
+     *
+     * @param noteAndTag The [OutNoteAndTag] object to be inserted.
+     */
+    override suspend fun insert(noteAndTag: OutNoteAndTag) {
+        dataSource.insert(mapper.fromDomain(noteAndTag))
     }
 
-    override suspend fun deleteNoteAndTag(noteAndTag: OutNoteAndTag) {
-        dataSource.deleteNoteAndTag(mapper.fromDomain(noteAndTag))
+    /**
+     * Deletes a record from the data source using the provided unique identifier (UID).
+     *
+     * This function delegates the deletion operation to the underlying [dataSource].
+     *
+     * @param uid The unique identifier of the record to be deleted.
+     */
+    override suspend fun deleteByUid(uid: String) {
+        dataSource.deleteByUid(uid)
+    }
+
+    /**
+     * Deletes an entity from the data source by its unique identifier.
+     *
+     * This function delegates the deletion operation to the underlying [dataSource].
+     *
+     * @param id The unique identifier of the entity to be deleted.
+     * @throws Exception if any error occurs during the deletion process within the data source. The specific exception depends on the implementation of `dataSource.deleteById()`.
+     */
+    override suspend fun deleteById(id: Long) {
+        dataSource.deleteById(id)
     }
 }
