13e1e7ec5979c278085b428463089b494e7dec09
yon <bourntivoli@gmail.com>
2025-01-31 19:43:11 +0100

    Refactor: Map Tag to OutTag with TagMapper class
    - Added `TagMapper` class to handle mapping between `Tag` (database) and `OutTag` (repository) models.
    - Implemented `toRepo` functions to convert `Tag` to `OutTag` and from lists of `Tag` to lists of `OutTag`.
    - Implemented `fromRepo` function to convert `OutTag` to `Tag`.
    - The Tag
     model is used in the repository layer, and OutTag is used in the database layer.

--- core/local/database/src/main/java/city/zouitel/database/mapper/TagMapper.kt
+++ core/local/database/src/main/java/city/zouitel/database/mapper/TagMapper.kt
@@ -1,19 +1,44 @@
 package city.zouitel.database.mapper
 
-import city.zouitel.database.model.TagEntity as InTag
+import city.zouitel.database.model.Tag
 import city.zouitel.repository.model.Tag as OutTag
 
+/**
+ * A mapper class responsible for converting between repository [Tag] and database [OutTag].
+ * This class provides methods for mapping lists of Tag and individual Tags between the two types.
+ */
 class TagMapper {
+    /**
+     * Maps a list of Tag objects to a list of their repository representations.
+     *
+     * @param Tags The list of Tags objects to map.
+     * @return A list containing the repository representations of the given Tags
+     * that are mapped from Tag to OutTag.
+     */
+    fun toRepo(tags: List<Tag>) = tags.map { toRepo(it) }
 
-    fun toRepo(tags: List<InTag>) = tags.map { toRepo(it) }
-
-    fun toRepo(tag: InTag) = OutTag(
+    /**
+     * Maps a Task object to an OutTag object.
+     *
+     * This function converts a Task object, which is typically used internally,
+     * to an OutTag object, which is likely used for external communication or data transfer.
+     *
+     * @param Tag The input Tag object to be mapped.
+     * @return An OutTag object representing the mapped Tag.
+     */
+    fun toRepo(tag: Tag) = OutTag(
         id = tag.id,
         label = tag.label,
         color = tag.color
     )
 
-    fun fromRepo(tag: OutTag) = InTag(
+    /**
+     * Maps an OutTag data object from the repository layer to a Tag data object for the repository layer.
+     *
+     * @param Tag The OutTag object to map.
+     * @return A Tag object representing the same data.
+     */
+    fun fromRepo(tag: OutTag) = Tag(
         id = tag.id,
         label = tag.label,
         color = tag.color
