5fdf997df8d062a4f95431120fb7d6f984e643be
yon <bourntivoli@gmail.com>
2025-01-31 19:41:36 +0100

    Refactor: Replace NoteAndTagEntity and TagEntity with NoteAndTag and Tag
    Replaced `NoteAndTagEntity` with `NoteAndTag` and `TagEntity` with `Tag` in `NoteEntity`. The change updates the `Junction` and `Relation` annotations to use the new classes, these classes are more simple.
    
    - Replaced `NoteAndTagEntity` class in the `Junction` annotation with `NoteAndTag`.
    
    - Replaced `TagEntity` class in the `Relation` annotation with `Tag`.
    - Replaced `tagEntities` `List<TagEntity>` with `List<Tag>`.
    - Updated parent and entity column names in the `Junction` to match the new schema.

--- core/local/database/src/main/java/city/zouitel/database/model/relational/NoteEntity.kt
+++ core/local/database/src/main/java/city/zouitel/database/model/relational/NoteEntity.kt
@@ -4,20 +4,20 @@ import androidx.room.Embedded
 import androidx.room.Junction
 import androidx.room.Relation
 import city.zouitel.database.model.DataEntity
-import city.zouitel.database.model.NoteAndTagEntity
-import city.zouitel.database.model.TagEntity
+import city.zouitel.database.model.NoteAndTag
+import city.zouitel.database.model.Tag
 import city.zouitel.database.utils.Constants.ID
 import city.zouitel.database.utils.Constants.UUID
 
 data class NoteEntity(
     @Embedded val dataEntity: DataEntity,
     @Relation(
-        parentColumn = UUID, entityColumn = ID, entity = TagEntity::class,
+        parentColumn = UUID, entityColumn = ID, entity = Tag::class,
         associateBy = Junction(
-            NoteAndTagEntity::class,
-            parentColumn = "noteUid",
-            entityColumn = "labelId"
+            NoteAndTag::class,
+            parentColumn = "uid",
+            entityColumn = "id"
         )
     )
-    val tagEntities :List<TagEntity>
+    val tagEntities :List<Tag>
 )
