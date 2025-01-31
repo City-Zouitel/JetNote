c92465d44606f6e36262aef4a08b4c092a5986db
yon <bourntivoli@gmail.com>
2025-01-31 19:39:22 +0100

    Refactor: Rename fields in NoteAndTag data class for clarity
    Renamed the fields in the `NoteAndTag` data class from `noteUid` and `labelId` to `uid` and `id`, respectively. This change improves readability and better reflects the purpose of each field:
    
    -   `noteUid` is now `uid`: Represents the unique identifier of the note.
    -   `labelId` is now `id`: Represents the unique identifier
     of the tag.
    
    Additionally, added documentation to the `NoteAndTag` class to clarify its usage and the purpose of its properties.

--- domain/src/main/java/city/zouitel/domain/model/NoteAndTag.kt
+++ domain/src/main/java/city/zouitel/domain/model/NoteAndTag.kt
@@ -2,8 +2,18 @@ package city.zouitel.domain.model
 
 import androidx.annotation.Keep
 
+/**
+ * Represents the association between a Note and a Tag.
+ *
+ * This data class is used to represent the relationship between a note and a tag,
+ * primarily used within a database or data storage context. It links a note identified
+ * by a unique string [uid] with a tag identified by a numerical [id].
+ *
+ * @property uid The unique identifier of the Note (e.g., UUID or other unique string).
+ * @property id The unique identifier of the Tag, typically a numerical ID.
+ */
 @Keep
 data class NoteAndTag(
-    val noteUid: String,
-    val labelId: Long
+    val uid: String,
+    val id: Long
 )
