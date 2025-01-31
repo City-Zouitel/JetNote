b852eb0dba404a2b73e00c9ac712a9add38de339
yon <bourntivoli@gmail.com>
2025-01-31 19:40:16 +0100

    Refactor: Rename fields in NoteAndTagMapper and update data class
    Renamed `noteUid` to `uid` and `labelId` to `id` in `NoteAndTagMapper` and `NoteAndTag` data class. This change improves the consistency and clarity of the data class by using more generic field names. Also removed the `Mapper` interface and `InNoteAndTag`/ `OutNoteAndTag` typealias.
    
    -   Renamed
     `noteUid` to `uid` in `NoteAndTag` data class.
    -   Renamed `labelId` to `id` in `NoteAndTag` data class.
    -   Removed the `Mapper` interface and `InNoteAndTag`/ `OutNoteAndTag` typealias.
    -   Renamed the `fromDomain` function.
    -   Renamed the `toDomain` function.

--- ui/features/tags/src/main/java/city/zouitel/tags/mapper/NoteAndTagMapper.kt
+++ ui/features/tags/src/main/java/city/zouitel/tags/mapper/NoteAndTagMapper.kt
@@ -1,20 +1,19 @@
 package city.zouitel.tags.mapper
 
+import city.zouitel.tags.model.NoteAndTag
 import city.zouitel.domain.model.NoteAndTag as OutNoteAndTag
-import city.zouitel.tags.mapper.base.Mapper
-import city.zouitel.tags.model.NoteAndTag as InNoteAndTag
 
 class NoteAndTagMapper {
 
     fun fromDomain(notesAndTag: List<OutNoteAndTag>) = notesAndTag.map { fromDomain(it) }
 
-    fun toDomain(noteAndTag: InNoteAndTag) = OutNoteAndTag(
-        noteUid = noteAndTag.noteUid,
-        labelId = noteAndTag.labelId
+    fun toDomain(noteAndTag: NoteAndTag) = OutNoteAndTag(
+        uid = noteAndTag.uid,
+        id = noteAndTag.id
     )
 
-    fun fromDomain(noteAndTag: OutNoteAndTag) = InNoteAndTag(
-        noteUid = noteAndTag.noteUid,
-        labelId = noteAndTag.labelId
+    private fun fromDomain(noteAndTag: OutNoteAndTag) = NoteAndTag(
+        uid = noteAndTag.uid,
+        id = noteAndTag.id
     )
 }
