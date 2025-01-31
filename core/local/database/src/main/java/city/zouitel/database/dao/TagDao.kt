ec4e786eb23f756ee2fefdba35a115e6ada022b0
yon <bourntivoli@gmail.com>
2025-01-31 19:42:28 +0100

    Refactor: Redesign TagDao for improved efficiency and consistency
    - Replaced `getAllTags`, `getTagById`, `addTag`, `updateTag`,
     and `deleteTag` with `observeAll`, `insert`, and `deleteById`.
    - Used `Upsert` instead of `Insert` and `Update` to simplify data management.
    - Used `Query` to perform a `Delete` by id operation.
    - Applied `TABLE_NAME` and
     `ID` constants for the `Tag` entity.
    - Added Kdoc documentation for all TagDao methods.
    
    These changes improve the efficiency and consistency of the `TagDao` by using standard operations and the upsert feature.
    The code now observes data instead of retrieving it one time.
    And the use of constants provide a better readability.
    The kdoc help to understand the code.

--- core/local/database/src/main/java/city/zouitel/database/dao/TagDao.kt
+++ core/local/database/src/main/java/city/zouitel/database/dao/TagDao.kt
@@ -1,30 +1,50 @@
 package city.zouitel.database.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
-import city.zouitel.database.model.TagEntity
-import city.zouitel.database.utils.Constants
+import androidx.room.Upsert
+import city.zouitel.database.model.Tag
+import city.zouitel.database.model.Tag.Companion.TABLE_NAME
+import city.zouitel.database.utils.Constants.ID
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Data Access Object (DAO) for interacting with the [Tag] entity in the database.
+ *
+ * This interface defines methods for observing, inserting, and deleting tags.
+ */
 @Dao
 interface TagDao {
+    /**
+     * Observes all tags in the database.
+     *
+     * This property provides a [Flow] that emits a list of all [Tag] entities
+     * stored in the database. The flow will emit a new list whenever the
+     * underlying data in the table changes.
+     *
+     * @return A [Flow] of [List] of [Tag] entities.
+     */
+    @get:Query("SELECT * FROM $TABLE_NAME")
+    val observeAll: Flow<List<Tag>>
 
-    @Query("SELECT * FROM LABELS_TABLE")
-    fun getAllTags(): Flow<List<TagEntity>>
+    /**
+     * Inserts a new [Tag] into the data store or updates an existing one if it already exists.
+     *
+     * This function uses an upsert operation, meaning it will either insert a new row with the
+     * provided [Tag] data if no matching record is found, or update the existing row that matches the
+     * [Tag]'s primary key (or other identifying attributes) with the provided data.
+     *
+     * @param tag The [Tag] object to be inserted or updated.
+     */
+    @Upsert
+    suspend fun insert(tag: Tag)
 
-    @Query("SELECT * FROM LABELS_TABLE WHERE ${Constants.ID} = :id")
-    suspend fun getTagById(id: Long): TagEntity?
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addTag(tagEntity: TagEntity)
-
-    @Update
-    suspend fun updateTag(tagEntity: TagEntity)
-
-    @Delete
-    suspend fun deleteTag(tagEntity: TagEntity)
+    /**
+     * Deletes a row from the table with the specified ID.
+     *
+     * @param id The ID of the row to delete.
+     * @throws SQLiteException if an error occurs during the database operation.
+     */
+    @Query("DELETE FROM $TABLE_NAME WHERE $ID = :id")
+    suspend fun deleteById(id: Long)
 }
