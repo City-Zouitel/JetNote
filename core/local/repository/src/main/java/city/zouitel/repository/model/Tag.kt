6be1d35e3551e20077070cf20879c2bdae6f3a9a
yon <bourntivoli@gmail.com>
2025-01-31 19:42:00 +0100

    Refactor: Improve Tag data class with default values
    - Added default values for `id`, `label`, and `color` in the `Tag` data
     class using constants from `Constants`.
    - `id` now defaults to `DEFAULT_LONG`, `label` to `DEFAULT_TXT`, and `color` to `DEFAULT_INT`.
    - Added KDoc to describe Tag's role.
    This change likely improves code readability and reduces boilerplate when creating `Tag` objects.

--- core/local/repository/src/main/java/city/zouitel/repository/model/Tag.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Tag.kt
@@ -1,10 +1,22 @@
 package city.zouitel.repository.model
 
 import androidx.annotation.Keep
+import city.zouitel.domain.utils.Constants.DEFAULT_INT
+import city.zouitel.domain.utils.Constants.DEFAULT_LONG
+import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
+/**
+ * Represents a tag with an ID, label, and color.
+ *
+ * Tags are used to categorize or label items within the application.
+ *
+ * @property id The unique identifier for the tag. Defaults to [DEFAULT_LONG] if not provided.
+ * @property label The text label associated with the tag. Defaults to [DEFAULT_TXT] if not provided.
+ * @property color The color associated with the tag, typically represented as an integer (e.g., ARGB). Defaults to [DEFAULT_INT] if not provided.
+ */
 @Keep
 data class Tag(
-     val id:Long = 0L,
-     val label:String? = null,
-     val color: Int = 0x0000
+     val id: Long = DEFAULT_LONG,
+     val label: String = DEFAULT_TXT,
+     val color: Int = DEFAULT_INT
 )
