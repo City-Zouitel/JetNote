02d393867f85bacd69f463a4c737118bd9dc5f19
yon <bourntivoli@gmail.com>
2025-02-03 15:25:21 +0100

    Refactor: Update and add new actions in `Action` sealed class
    - Replaced the generic `Update` action with `Rollback` and `Archive
    ` actions, each now taking a `uid` (String) parameter.
    - Replaced the `DeleteAll` action with `Erase`.
    -  `Insert`, `UpdateById`, `Delete`, `DeleteById`, `DeleteByUid`  are maintained.
    
    These changes refine the types of actions
     and their parameters, likely to improve the handling of specific operations.

--- domain/src/main/java/city/zouitel/domain/utils/Action.kt
+++ domain/src/main/java/city/zouitel/domain/utils/Action.kt
@@ -2,10 +2,11 @@ package city.zouitel.domain.utils
 
 sealed class Action {
     data class Insert<out T>(val data: T) : Action()
-    data class Update<out T>(val data: T) : Action()
+    data class Rollback(val uid: String) : Action()
+    data class Archive(val uid: String) : Action()
     data class UpdateById(val id: Number) : Action()
     data class Delete<out T> (val data: T) : Action()
     data class DeleteById (val id: Number): Action()
     data class DeleteByUid (val uid: String): Action()
-    data object DeleteAll : Action()
+    data object Erase : Action()
 }
