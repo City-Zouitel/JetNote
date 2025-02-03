a1e264d41cf59f6719c3c078a673faa381102353
yon <bourntivoli@gmail.com>
2025-02-03 15:24:58 +0100

    Refactor: Remove Widget-related components and dependencies
    Removed the `Widget` feature, including its associated mappers, data sources, repositories, use cases,
     and DAO.
    
    - Deleted `WidgetDao`, `WidgetDataSource`, `WidgetDataSourceImpl`, `WidgetMapper` (in database and repository), `WidgetRepository`, `WidgetRepositoryImpl` and `WidgetUseCase`.
    - Deleted `Mapper.kt`.
    - These changes are likely part of a refactoring or simplification
    , as they remove a complete feature from the application.

--- core/local/repository/src/main/java/city/zouitel/repository/datasource/WidgetDataSource.kt
+++
@@ -1,9 +0,0 @@
-package city.zouitel.repository.datasource
-
-import city.zouitel.repository.model.Note
-import kotlinx.coroutines.flow.Flow
-
-interface WidgetDataSource {
-
-    val getAllWidgetMainEntityById: Flow<List<Note>>
-}
--- domain/src/main/java/city/zouitel/domain/repository/WidgetRepository.kt
+++
@@ -1,8 +0,0 @@
-package city.zouitel.domain.repository
-
-import kotlinx.coroutines.flow.Flow
-import city.zouitel.domain.model.Note as OutNote
-interface WidgetRepository {
-
-    val getAllWidgetMainEntityById: Flow<List<OutNote>>
-}
