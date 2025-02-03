82db2662401d9e79dc2c39ef731aace51acb2ec3
yon <bourntivoli@gmail.com>
2025-02-03 17:56:12 +0100

    Refactor: Improve Data class properties with default values
    Updated the `Data` data class properties to use default values.
    
    - Replaced empty string or null
     with default values from Constants:
        - `uid`, `title`, `description`, and `date` now default to `DEFAULT_TXT`.
        - `priority`, `background` and `textColor` now default to `DEFAULT_INT`.
        - `archived` now defaults to `DEFAULT_
    BOOLEAN`.
    - `removed` property was changed to `archived`.
    - Updated properties types from `String` to `Int` and from `Int` to `Boolean`.
    
    This change improves data integrity and code readability by providing clear default values and correct properties types for data fields.

--- core/local/repository/src/main/java/city/zouitel/repository/model/Data.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Data.kt
@@ -1,16 +1,34 @@
 package city.zouitel.repository.model
 
 import androidx.annotation.Keep
-import city.zouitel.repository.utils.Constants.NON
+import city.zouitel.domain.utils.Constants.DEFAULT_BOOLEAN
+import city.zouitel.domain.utils.Constants.DEFAULT_INT
+import city.zouitel.domain.utils.Constants.DEFAULT_TXT
 
+/**
+ * Represents a data item with various properties.
+ *
+ * This data class holds information about a single item, including its unique identifier,
+ * title, description, priority, visual appearance (background and text color), date, and
+ * whether it's archived.
+ *
+ * @property uid A unique identifier for the data item. Defaults to [DEFAULT_TXT].
+ * @property title The title of the data item. Defaults to [DEFAULT_TXT].
+ * @property description A detailed description of the data item. Defaults to [DEFAULT_TXT].
+ * @property priority The priority level of the data item (e.g., 1 for high, 3 for low). Defaults to [DEFAULT_INT].
+ * @property background The resource ID of the background color or drawable for the data item. Defaults to [DEFAULT_INT].
+ * @property textColor The resource ID of the text color for the data item. Defaults to [DEFAULT_INT].
+ * @property date The date associated with the data item, typically in a string format. Defaults to [DEFAULT_TXT].
+ * @property archived Indicates whether the data item is archived or not. Defaults to [DEFAULT_BOOLEAN].
+ */
 @Keep
 data class Data(
-    var uid: String = "",
-    var title: String? = null,
-    var description: String? = null,
-    var priority: String = NON,
-    var color: Int = 0,
-    var textColor: Int = 0x000000,
-    var date: String = "",
-    var removed: Int = 0
+    var uid: String = DEFAULT_TXT,
+    var title: String = DEFAULT_TXT,
+    var description: String = DEFAULT_TXT,
+    var priority: Int = DEFAULT_INT,
+    var background: Int = DEFAULT_INT,
+    var textColor: Int = DEFAULT_INT,
+    var date: String = DEFAULT_TXT,
+    var archived: Boolean = DEFAULT_BOOLEAN
 )
