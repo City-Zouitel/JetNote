a47e1bacb8f6c1599329437038c8c8c4de10a443
yon <bourntivoli@gmail.com>
2025-02-03 18:03:05 +0100

    Refactor: Use Action instead of UiEvent in DataScreenModel
    Replaced the `UiEvent` system with `Action` in `DataScreenModel` for handling data operations.
    
    - Changed `sendUiEvent` to `sendAction` which now accepts `Action` enums.
    - Refactored data operations to use specific actions: `Insert`, `Archive`, `Rollback`, `DeleteByUid`, and `Erase`.
    - Introduced use cases
    : `Insert`, `Archive`, `Rollback`, `DataUseCase.Delete`, `Erase`, `NoteAndTagUseCase.DeleteByUid`, `AudioUseCase.DeleteByUid`, `LinkUseCase.DeleteByUid`, `MediaUseCase.DeleteByUid`, `ReminderUseCase.DeleteByUid`, `TaskUseCase.DeleteByUid`.
    - Implemented bulk deletion of related data (noteAndTags, audio, link, media, reminder, task) when `Action.DeleteByUid` is triggered.
    - Changed the way to run actions, now use `withAsync` extension function.
    - Removed `UiEventHandler` and `performUiEvent` and `UiEvent`.
    - Removed `Dispatchers` and `screenModelScope` imports.

--- ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/ui/DataScreenModel.kt
@@ -1,34 +1,50 @@
 package city.zouitel.note.ui
 
 import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.screenModelScope
+import city.zouitel.domain.usecase.AudioUseCase
 import city.zouitel.domain.usecase.DataUseCase
-import city.zouitel.logic.events.UiEvent
-import city.zouitel.logic.events.UiEventHandler
+import city.zouitel.domain.usecase.LinkUseCase
+import city.zouitel.domain.usecase.MediaUseCase
+import city.zouitel.domain.usecase.NoteAndTagUseCase
+import city.zouitel.domain.usecase.ReminderUseCase
+import city.zouitel.domain.usecase.TaskUseCase
+import city.zouitel.domain.utils.Action
+import city.zouitel.domain.utils.withAsync
 import city.zouitel.note.mapper.DataMapper
 import city.zouitel.note.model.Data
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
+@Suppress("DeferredResultUnused")
 class DataScreenModel(
-    private val add: DataUseCase.AddData,
-    private val edit: DataUseCase.EditData,
-    private val delete: DataUseCase.DeleteData,
-    private val eraseTrash: DataUseCase.DeleteAllTrashedData,
+    private val insert: DataUseCase.Insert,
+    private val archive: DataUseCase.Archive,
+    private val rollback: DataUseCase.Rollback,
+    private val data: DataUseCase.Delete,
+    private val erase: DataUseCase.Erase,
+    private val noteAndTags: NoteAndTagUseCase.DeleteByUid,
+    private val audio: AudioUseCase.DeleteByUid,
+    private val link: LinkUseCase.DeleteByUid,
+    private val media: MediaUseCase.DeleteByUid,
+    private val reminder: ReminderUseCase.DeleteByUid,
+    private val task: TaskUseCase.DeleteByUid,
     private val mapper: DataMapper
-): ScreenModel, UiEventHandler<Data> {
+): ScreenModel {
 
-    override fun sendUiEvent(event: UiEvent<Data>) {
-        when(event) {
-            is UiEvent.Delete -> performUiEvent { delete(mapper.toDomain(event.data)) }
-            is UiEvent.Insert -> performUiEvent { add(mapper.toDomain(event.data)) }
-            is UiEvent.Update -> performUiEvent { edit(mapper.toDomain(event.data)) }
-            is UiEvent.DeleteAll -> performUiEvent { eraseTrash() }
-            else -> throw NotImplementedError("This event is not implemented: $event")
+    fun sendAction(act: Action) {
+        when(act) {
+            is Action.Insert<*> -> withAsync { insert(mapper.toDomain(act.data as Data)) }
+            is Action.Archive -> withAsync { archive(act.uid) }
+            is Action.Rollback -> withAsync { rollback(act.uid) }
+            is Action.DeleteByUid -> withAsync {
+                data(act.uid)
+                noteAndTags(act.uid)
+                audio(act.uid)
+                link(act.uid)
+                media(act.uid)
+                reminder(act.uid)
+                task(act.uid)
+            }
+            is Action.Erase -> withAsync { erase() }
+            else -> throw NotImplementedError("This event is not implemented: $act")
         }
-    }
-
-    override fun performUiEvent(action: suspend () -> Unit) {
-        screenModelScope.launch(Dispatchers.IO) { action.invoke() }
     }
 }
