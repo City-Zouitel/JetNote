8211bef05534100216535fe57beee2929f71197a
yon <bourntivoli@gmail.com>
2025-02-03 18:00:36 +0100

    Refactor: Convert DataMapper to map between Data and OutData
    Updated the `DataMapper` class to convert between `Data` (domain model) and
     `OutData` (repository model) instead of `InData`.
    - Modified `toRepo` and `fromRepo` to use `Data` and `OutData`.
    - Changed the attribute `color` to `background`, and `removed` to `archived`.
    - Added KDocs to the `DataMapper`, `toRepo`, and `fromRepo` for better documentation.
    
    This change makes the data mapping more clear and maintainable. This modification ensures that the mapper correctly converts between the domain and repository layers, improving consistency and code readability.

--- core/local/database/src/main/java/city/zouitel/database/mapper/DataMapper.kt
+++ core/local/database/src/main/java/city/zouitel/database/mapper/DataMapper.kt
@@ -1,29 +1,49 @@
 package city.zouitel.database.mapper
 
-import city.zouitel.database.model.DataEntity as InData
+import city.zouitel.database.model.Data
 import city.zouitel.repository.model.Data as OutData
 
+/**
+ * Mapper class responsible for converting between [Data] (domain model) and [OutData] (repository model).
+ *
+ * This class provides methods to transform a list of tasks or a single Data
+ * between the domain and repository representations.
+ */
 class DataMapper {
-
-    fun toRepo(data: InData) = OutData(
+    /**
+     * Maps a Task object to an OutData object.
+     *
+     * This function converts a Task object, which is typically used internally,
+     * to an OutData object, which is likely used for external communication or data transfer.
+     *
+     * @param Data The input Data object to be mapped.
+     * @return An OutData object representing the mapped Data.
+     */
+    fun toRepo(data: Data) = OutData(
         uid = data.uid,
         title = data.title,
         description = data.description,
         priority = data.priority,
-        color = data.color,
+        background = data.background,
         textColor = data.textColor,
         date = data.date,
-        removed = data.removed
+        archived = data.archived
     )
 
-    fun fromRepo(data: OutData) = InData(
+    /**
+     * Maps an OutData data object from the repository layer to a Data data object for the repository layer.
+     *
+     * @param Data The OutData object to map.
+     * @return A Data object representing the same data.
+     */
+    fun fromRepo(data: OutData) = Data(
         uid = data.uid,
         title = data.title,
         description = data.description,
         priority = data.priority,
-        color = data.color,
+        background = data.background,
         textColor = data.textColor,
         date = data.date,
-        removed = data.removed
+        archived = data.archived
     )
 }
