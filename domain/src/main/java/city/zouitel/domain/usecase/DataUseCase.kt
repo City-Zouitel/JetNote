6e64ae7963bac416380c3956de8095f206e98dd9
yon <bourntivoli@gmail.com>
2025-02-03 18:03:12 +0100

    Refactor: Use sealed classes for DataUseCase operations
    - Replaced individual `DataUseCase` classes (`AddData`, `EditData`, `DeleteData`,
     `DeleteAllTrashedData`) with a sealed class `DataUseCase` to manage data operations.
    - Introduced `Insert`, `Archive`, `Rollback`, `Delete`, and `Erase` data classes within `DataUseCase`.
    - Each sealed class data class represents a specific operation and encapsulates a `DataRepository
    `.
    - Each operation class implement `suspend operator fun invoke(...)` for performing the action.

--- domain/src/main/java/city/zouitel/domain/usecase/DataUseCase.kt
+++ domain/src/main/java/city/zouitel/domain/usecase/DataUseCase.kt
@@ -3,29 +3,70 @@ package city.zouitel.domain.usecase
 import city.zouitel.domain.model.Data
 import city.zouitel.domain.repository.DataRepository
 
+/**
+ * A sealed class representing various use cases for interacting with data.
+ * Each use case is a data class that encapsulates a specific operation on a [DataRepository].
+ *
+ * This class utilizes the `suspend operator fun invoke(...)` pattern to provide a concise way
+ * to execute each use case. This pattern makes it easy to call the use case as if it were a function.
+ */
 sealed class DataUseCase {
-
-    class AddData(
-        private val repository: DataRepository
-    ): DataUseCase() {
-        suspend operator fun invoke(data: Data) = repository.addData(data)
+    /**
+     * [Insert] is a use case responsible for inserting a [Data] entity into the data layer.
+     *
+     * It leverages a [DataRepository] to perform the actual insertion.
+     *
+     * @property repo The [DataRepository] instance used for data operations.
+     */
+    data class Insert(private val repo: DataRepository): DataUseCase() {
+        suspend operator fun invoke(data: Data) = repo.insert(data)
     }
 
-    class EditData(
-        private val repository: DataRepository
-    ): DataUseCase() {
-        suspend operator fun invoke(data: Data) = repository.editData(data)
+    /**
+     * Use case for archiving a specific data item.
+     *
+     * This class encapsulates the logic for marking a data item as archived within the underlying
+     * [DataRepository]. It provides a single, suspendable operator function ([invoke]) to perform
+     * the archiving operation.
+     *
+     * @property repo The [DataRepository] instance used to interact with the data storage layer.
+     * It should be pre-configured and injected into this use case.
+     */
+    data class Archive(private val repo: DataRepository): DataUseCase() {
+        suspend operator fun invoke(uid: String) = repo.archive(uid)
     }
 
-    class DeleteData(
-        private val repository: DataRepository
-    ): DataUseCase() {
-        suspend operator fun invoke(data: Data) = repository.deleteData(data)
+    /**
+     * [Rollback] is a [DataUseCase] responsible for rolling back data changes associated with a specific user ID.
+     *
+     * It leverages a [DataRepository] to perform the actual rollback operation.
+     *
+     * @property repo The [DataRepository] instance used to interact with the data layer.
+     */
+    data class Rollback(private val repo: DataRepository): DataUseCase() {
+        suspend operator fun invoke(uid: String) = repo.rollback(uid)
     }
 
-    class DeleteAllTrashedData(
-        private val repository: DataRepository
-    ) : DataUseCase() {
-        suspend operator fun invoke() = repository.deleteAllTrashedData()
+    /**
+     * Represents a use case for deleting data from the repository.
+     *
+     * This use case provides a suspendable operation to delete a data item identified by a unique ID.
+     *
+     * @property repo The [DataRepository] instance used to interact with the data layer.
+     */
+    data class Delete(private val repo: DataRepository): DataUseCase() {
+        suspend operator fun invoke(uid: String) = repo.delete(uid)
+    }
+
+    /**
+     * [Erase] is a [DataUseCase] responsible for erasing all data managed by the [DataRepository].
+     *
+     * This use case provides a single public function, [invoke], which triggers the data erasure process.
+     * It delegates the actual data deletion to the provided [DataRepository].
+     *
+     * @property repo The [DataRepository] used to manage and erase data.
+     */
+    data class Erase(private val repo: DataRepository): DataUseCase() {
+        suspend operator fun invoke() = repo.erase()
     }
 }
