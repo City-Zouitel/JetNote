6172f7ce998a3c44b8577852dd54073ab4a6b243
yon <bourntivoli@gmail.com>
2025-02-04 10:50:43 +0100

    Refactor: Update Note model to use data and tags properties
    Updated the `Note` model to use `data` and `tags` properties instead of `dataEntity` and `tagEntities`.
    
    - Renamed `dataEntity` to `data`.
    - Renamed `tagEntities` to `tags`.
    - Added Kdoc for the `Note` class.
    
    This change improves the readability and clarity of the `Note` model by using more concise and descriptive
     property names and adding documentation.

--- core/local/repository/src/main/java/city/zouitel/repository/model/Note.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Note.kt
@@ -2,8 +2,15 @@ package city.zouitel.repository.model
 
 import androidx.annotation.Keep
 
+/**
+ * Represents a note containing data and associated tags.
+ *
+ * A note is a structured piece of information that can be categorized
+ * and organized using tags.  It consists of the actual content ([data])
+ * and a list of associated [tags] that provide context or classification.
+ *
+ * @property data The primary content or data of the note.
+ * @property tags A list of [Tag]s associated with this note, used for categorization and organization.
+ */
 @Keep
-data class Note(
-    val dataEntity: Data,
-    val tagEntities: List<Tag>
-)
+data class Note(val data: Data, val tags: List<Tag>)
