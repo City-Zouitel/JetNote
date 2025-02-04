e2ae5008ccfd86609035832ceb80782fcbd88132
yon <bourntivoli@gmail.com>
2025-02-04 10:51:15 +0100

    Refactor: Remove Widget dependencies and update Koin modules
    Removed all dependencies related to the `Widget` feature:
    
    - Removed `WidgetMapper` and `WidgetRepositoryImpl`.
    - Removed `WidgetRepository` and `WidgetUseCase`.
    - Removed `WidgetUseCase::GetAllWidgetMainEntityById` in `modules.kt`
    - Updated `DataUseCase` to use `Insert`, `Archive`, `Rollback`, `Delete`, and `Erase` functions instead
     of  `AddData`, `EditData`, `DeleteData`, and `DeleteAllTrashedData`.
    - Updated `NoteUseCase` to use `ObserveByDefault`, `ObserveByName`, `ObserveByOldest`, `ObserveByNewest`, `ObserveArchives` and `ObserveByPriority` functions instead of `GetAllNotesById`, `GetAllNotesByName`, `GetAllNotesByNewest`, `GetAllNotesByOldest`, `GetAllNotesByPriority` and `GetAllRemovedNotes`.
    - Reorganized Koin module definitions in `modules.kt` for better readability.
    
    This change eliminates unused code and likely improves the project
    's maintainability by focusing on essential features.

--- core/local/repository/src/main/java/city/zouitel/repository/di/modules.kt
+++ core/local/repository/src/main/java/city/zouitel/repository/di/modules.kt
@@ -10,7 +10,6 @@ import city.zouitel.domain.repository.ReminderRepo
 import city.zouitel.domain.repository.RootRepository
 import city.zouitel.domain.repository.TagRepository
 import city.zouitel.domain.repository.TaskRepository
-import city.zouitel.domain.repository.WidgetRepository
 import city.zouitel.domain.usecase.AudioUseCase
 import city.zouitel.domain.usecase.DataUseCase
 import city.zouitel.domain.usecase.LinkUseCase
@@ -21,7 +20,6 @@ import city.zouitel.domain.usecase.ReminderUseCase
 import city.zouitel.domain.usecase.RootUseCase
 import city.zouitel.domain.usecase.TagUseCase
 import city.zouitel.domain.usecase.TaskUseCase
-import city.zouitel.domain.usecase.WidgetUseCase
 import city.zouitel.repository.mapper.AudioMapper
 import city.zouitel.repository.mapper.DataMapper
 import city.zouitel.repository.mapper.LinkMapper
@@ -32,7 +30,6 @@ import city.zouitel.repository.mapper.ReminderMapper
 import city.zouitel.repository.mapper.RootMapper
 import city.zouitel.repository.mapper.TagMapper
 import city.zouitel.repository.mapper.TaskMapper
-import city.zouitel.repository.mapper.WidgetMapper
 import city.zouitel.repository.repositoryImpl.AudioRepoImpl
 import city.zouitel.repository.repositoryImpl.DataRepositoryImpl
 import city.zouitel.repository.repositoryImpl.LinkRepositoryImpl
@@ -43,7 +40,6 @@ import city.zouitel.repository.repositoryImpl.ReminderRepoImpl
 import city.zouitel.repository.repositoryImpl.RootRepositoryImpl
 import city.zouitel.repository.repositoryImpl.TagRepositoryImpl
 import city.zouitel.repository.repositoryImpl.TaskRepositoryImpl
-import city.zouitel.repository.repositoryImpl.WidgetRepositoryImpl
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -64,28 +60,25 @@ val localRepoDIModule = module {
     factory {
         NoteMapper(get(), get())
     }
-    factory {
-        WidgetMapper(get(), get())
-    }
 
     //Repositories.
-    singleOf(::DataRepositoryImpl) bind DataRepository::class
-    singleOf(::NoteRepositoryImpl) bind NoteRepository::class
-    singleOf(::LinkRepositoryImpl) bind LinkRepository::class
-    singleOf(::TagRepositoryImpl) bind TagRepository::class
-    singleOf(::NoteAndTagRepositoryImpl) bind NoteAndTagRepository::class
-    singleOf(::TaskRepositoryImpl) bind TaskRepository::class
     singleOf(::AudioRepoImpl) bind AudioRepo::class
-    singleOf(::WidgetRepositoryImpl) bind WidgetRepository::class
-    singleOf(::RootRepositoryImpl) bind RootRepository::class
+    singleOf(::DataRepositoryImpl) bind DataRepository::class
+    singleOf(::LinkRepositoryImpl) bind LinkRepository::class
     singleOf(::MediaRepoImpl) bind MediaRepository::class
+    singleOf(::NoteAndTagRepositoryImpl) bind NoteAndTagRepository::class
+    singleOf(::NoteRepositoryImpl) bind NoteRepository::class
     singleOf(::ReminderRepoImpl) bind ReminderRepo::class
+    singleOf(::RootRepositoryImpl) bind RootRepository::class
+    singleOf(::TagRepositoryImpl) bind TagRepository::class
+    singleOf(::TaskRepositoryImpl) bind TaskRepository::class
 
     //UseCases.
-    factoryOf(DataUseCase::AddData)
-    factoryOf(DataUseCase::EditData)
-    factoryOf(DataUseCase::DeleteData)
-    factoryOf(DataUseCase::DeleteAllTrashedData)
+    factoryOf(DataUseCase::Insert)
+    factoryOf(DataUseCase::Archive)
+    factoryOf(DataUseCase::Rollback)
+    factoryOf(DataUseCase::Delete)
+    factoryOf(DataUseCase::Erase)
 
     factoryOf(LinkUseCase::ObserveAll)
     factoryOf(LinkUseCase::ObserveByUid)
@@ -98,12 +91,12 @@ val localRepoDIModule = module {
     factoryOf(NoteAndTagUseCase::DeleteByUid)
     factoryOf(NoteAndTagUseCase::DeleteById)
 
-    factoryOf(NoteUseCase::GetAllNotesById)
-    factoryOf(NoteUseCase::GetAllNotesByName)
-    factoryOf(NoteUseCase::GetAllNotesByNewest)
-    factoryOf(NoteUseCase::GetAllNotesByOldest)
-    factoryOf(NoteUseCase::GetAllNotesByPriority)
-    factoryOf(NoteUseCase::GetAllRemovedNotes)
+    factoryOf(NoteUseCase::ObserveByDefault)
+    factoryOf(NoteUseCase::ObserveByName)
+    factoryOf(NoteUseCase::ObserveByOldest)
+    factoryOf(NoteUseCase::ObserveByNewest)
+    factoryOf(NoteUseCase::ObserveArchives)
+    factoryOf(NoteUseCase::ObserveByPriority)
 
     factoryOf(TagUseCase::ObserveAll)
     factoryOf(TagUseCase::Insert)
@@ -114,8 +107,6 @@ val localRepoDIModule = module {
     factoryOf(TaskUseCase::Insert)
     factoryOf(TaskUseCase::DeleteById)
     factoryOf(TaskUseCase::DeleteByUid)
-
-    factoryOf(WidgetUseCase::GetAllWidgetMainEntityById)
 
     factoryOf(RootUseCase::RootUseCase)
 
