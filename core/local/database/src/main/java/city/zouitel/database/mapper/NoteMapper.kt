54ae3d969798cee4a170c3b9429022ca4227530b
yon <bourntivoli@gmail.com>
2025-02-04 10:49:47 +0100

    Refactor: Introduce NoteMapper for mapping Note entities to repository models
    Introduced `NoteMapper` class to handle mapping between `Note` entities (database model
    ) and `OutNote` (repository model).
    
    - `toRepo`: Added methods for mapping `Note` entities and a list of `Note` entities to their `OutNote` representations.
    - `DataMapper` and `TagMapper` dependencies: Utilized `DataMapper` and `TagMapper` for handling the mapping of nested data and tags within a `Note`.
    - Improved the previous structure by converting the data from model to repository model.
    
    This change establishes a clear mapping layer for note data, improving data flow and separation of concerns.

--- core/local/database/src/main/java/city/zouitel/database/mapper/NoteMapper.kt
+++ core/local/database/src/main/java/city/zouitel/database/mapper/NoteMapper.kt
@@ -1,18 +1,42 @@
 package city.zouitel.database.mapper
 
-import city.zouitel.database.model.relational.NoteEntity as InNote
+import city.zouitel.database.model.Note
 import city.zouitel.repository.model.Note as OutNote
 
+/**
+ * Mapper class responsible for converting between [Note] entities and their repository ([OutNote]) representations.
+ *
+ * This class utilizes [DataMapper] and [TagMapper] to handle the mapping of nested data and tags within a [Note].
+ *
+ * @property dataMapper The [DataMapper] instance used to map the `data` field of a [Note].
+ * @property tagMapper The [TagMapper] instance used to map the `tags` associated with a [Note].
+ */
 class NoteMapper(
     private val dataMapper: DataMapper,
-    private val tagMapper: TagMapper,
+    private val tagMapper: TagMapper
 ) {
-    fun toRepo(notes: List<InNote>) = notes.map { toRepo(it) }
+    /**
+     * Maps a list of Data objects to a list of their repository representations.
+     *
+     * @param Datas The list of Datas objects to map.
+     * @return A list containing the repository representations of the given Datas
+     * that are mapped from Data to OutData.
+     */
+    fun toRepo(notes: List<Note>) = notes.map { toRepo(it) }
 
-    fun toRepo(data: InNote): OutNote = with(data){
+    /**
+     * Maps a Task object to an OutData object.
+     *
+     * This function converts a Task object, which is typically used internally,
+     * to an OutData object, which is likely used for external communication or data transfer.
+     *
+     * @param Data The input Data object to be mapped.
+     * @return An OutData object representing the mapped Data.
+     */
+    fun toRepo(data: Note): OutNote = with(data){
         OutNote(
-            dataEntity = dataMapper.toRepo(dataEntity),
-            tagEntities = tagEntities.map { tagMapper.toRepo(it) }
+            data = dataMapper.toRepo(this.data),
+            tags = tags.map { tagMapper.toRepo(it) }
         )
     }
 }
