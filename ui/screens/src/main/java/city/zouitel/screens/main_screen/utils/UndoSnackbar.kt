437cf59b6e43401aae32c5f05f2d50a614a2111f
yon <bourntivoli@gmail.com>
2025-02-04 11:49:49 +0100

    Refactor: Use Action.Rollback instead of UiEvent.Update in undoSnackbar
    Replaced the use of `UiEvent.Update` with `Action
    .Rollback` to handle the undo action in the `undoSnackbar` function.
    - Removed direct calls to `sendUiEvent` with `UiEvent.Update`.
    - Instead now uses `sendAction` with `Action.Rollback`.
    - Changed the snackbar message to "moved to archive.".
    
    This refactor likely improves the data flow by using a more specific action for the undo operation.

--- ui/screens/src/main/java/city/zouitel/screens/main_screen/utils/UndoSnackbar.kt
+++ ui/screens/src/main/java/city/zouitel/screens/main_screen/utils/UndoSnackbar.kt
@@ -3,44 +3,34 @@ package city.zouitel.screens.main_screen.utils
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.SnackbarResult
 import androidx.compose.runtime.State
-import city.zouitel.logic.events.UiEvent
+import city.zouitel.domain.utils.Action
 import city.zouitel.note.model.Data
 import city.zouitel.note.model.Note
 import city.zouitel.note.ui.DataScreenModel
-import city.zouitel.systemDesign.CommonConstants.TRASH_MESSAGE
 import city.zouitel.systemDesign.CommonConstants.UNDO
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-internal fun UndoSnackbar(
+internal fun undoSnackbar(
     dataModule: DataScreenModel,
     scaffoldState : ScaffoldState,
     scope : CoroutineScope,
-    removedNotesState : State<List<Note>>
+    archivedNotesState : State<List<Note>>
 ): (Data) -> Unit {
     val onShowSnackbar: (Data) -> Unit = { note ->
 
         scope.launch {
             val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
-                message = note.title + " " + TRASH_MESSAGE,
+                message = note.title + " " + "moved to archive.",
                 actionLabel = UNDO
             )
 
-            when(snackbarResult)  {
+            when (snackbarResult) {
                 SnackbarResult.Dismissed -> {} // Timber.d("Snackbar dismissed")
                 SnackbarResult.ActionPerformed -> {
-                    dataModule.sendUiEvent(UiEvent.Update(
-                        Data(
-                            title = removedNotesState.value.last().dataEntity.title,
-                            description = removedNotesState.value.last().dataEntity.description,
-                            priority = removedNotesState.value.last().dataEntity.priority,
-                            uid = removedNotesState.value.last().dataEntity.uid,
-                            textColor = removedNotesState.value.last().dataEntity.textColor,
-                            date = removedNotesState.value.last().dataEntity.date,
-                            color = removedNotesState.value.last().dataEntity.color,
-                            removed = 0,
-                        )
-                    ))
+                    dataModule.sendAction(
+                        Action.Rollback(archivedNotesState.value.last().data.uid)
+                    )
                 }
             }
         }
