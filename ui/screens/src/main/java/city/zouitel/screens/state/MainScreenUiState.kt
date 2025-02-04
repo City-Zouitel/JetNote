47aa7eea01d99eaf35a860eda82fe163e7ab1554
yon <bourntivoli@gmail.com>
2025-02-04 10:51:36 +0100

    Refactor: Update MainScreenUiState `selectedNote` type
    Changed the `selectedNote` property in `MainScreenUiState` from a nullable `Data` object to a non-nullable `String` with a default value.
    
    This likely simplifies the state management by using a String identifier instead of a nullable object.

--- ui/screens/src/main/java/city/zouitel/screens/state/MainScreenUiState.kt
+++ ui/screens/src/main/java/city/zouitel/screens/state/MainScreenUiState.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import city.zouitel.note.model.Data
 import city.zouitel.tags.model.Tag
 
-internal data class MainScreenUiState(
+data class MainScreenUiState(
     val searchTitle: String = "",
     val searchTag: Tag? = null,
     val expandedSortMenu: Boolean = false,
@@ -16,7 +16,7 @@ internal data class MainScreenUiState(
     val isHomeScreen: Boolean = true,
     val isOptionsDialog: Boolean = false,
     val selectedNotes: SnapshotStateList<Data> = mutableStateListOf(),
-    val selectedNote: Data? = null
+    val selectedNote: String = "oezdfrhozef"
 ) {
 
     fun addSelectedNote(note: Data): MainScreenUiState {
