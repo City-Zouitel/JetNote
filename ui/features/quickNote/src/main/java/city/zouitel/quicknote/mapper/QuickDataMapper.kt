94ff2e1218642b82414bede5b17ab46cb95d3538
yon <bourntivoli@gmail.com>
2025-02-04 10:48:16 +0100

    Refactor: Update QuickDataMapper to use QuickData
    Modified `QuickDataMapper` to utilize the `QuickData` model instead of `InData`.
    
    - Updated the `toDomain` function to accept `QuickData` as a parameter.
    - Renamed `color` property to `background` in the mapping to align with changes.
    - Renamed `removed` property to `archived`.
    This change aligns the mapper with the updated model and terminology.

--- ui/features/quickNote/src/main/java/city/zouitel/quicknote/mapper/QuickDataMapper.kt
+++ ui/features/quickNote/src/main/java/city/zouitel/quicknote/mapper/QuickDataMapper.kt
@@ -1,18 +1,18 @@
 package city.zouitel.quicknote.mapper
 
+import city.zouitel.quicknote.model.QuickData
 import city.zouitel.domain.model.Data as OutData
-import city.zouitel.quicknote.model.QuickData as InData
 
 class QuickDataMapper {
 
-    fun toDomain(data: InData) = OutData(
+    fun toDomain(data: QuickData) = OutData(
         uid = data.uid,
         title = data.title,
         description = data.description,
         priority = data.priority,
-        color = data.color,
+        background = data.background,
         textColor = data.textColor,
         date = data.date,
-        removed = data.removed
+        archived = data.archived
     )
 }
