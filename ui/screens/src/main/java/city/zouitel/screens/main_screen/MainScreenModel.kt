18dea05491fd17fa3f81e35f1f2dd76635422826
yon <bourntivoli@gmail.com>
2025-02-04 10:51:45 +0100

    Refactor: Use observe methods instead of getAll in MainScreenModel
    Replaced `getAllById`, `getAllByOldest`, `getAllByNewest`,
     `getAllByName`, `getAllRemoved`, and `getAllByPriority` with their corresponding `observeByDefault`, `observeByNewest`, `observeByOldest`, `observeByName`, `observeByArchives`, and `observeByPriority` methods.
    This change improves data flow by using `Flow` directly to observe
     and update data.
    
    - Changed `StateFlow` names from `allNotesById`, `allNotesByOldest`... to `observeDefaults`, `observeNewest` ...
    - Replaced `getAllById.invoke().collect { ... }` calls to `observeByDefault().collect { ... }` calls.
    - Added `withFlow` extension function to handle `StateFlow` initialization and updates with `withAsync`.
    - Removed unused `Dispatchers.IO`.
    - Removed unneeded `updateExpandedSortMenu`, `updateErasing` and `updateOptionsDialog` functions.
    - Moved `init` content to `observe` methods.
    - Updated `updateSelectedNote` to take `uid` as `String` instead of `Data`.
    - Removed `SharingStarted.WhileSubscribed` from flows.

--- ui/screens/src/main/java/city/zouitel/screens/main_screen/MainScreenModel.kt
+++ ui/screens/src/main/java/city/zouitel/screens/main_screen/MainScreenModel.kt
@@ -4,82 +4,78 @@ import androidx.compose.runtime.mutableStateListOf
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import city.zouitel.domain.usecase.NoteUseCase
+import city.zouitel.domain.utils.withAsync
+import city.zouitel.logic.withFlow
 import city.zouitel.note.model.Data
+import city.zouitel.note.model.Note
 import city.zouitel.screens.mapper.NoteMapper
 import city.zouitel.screens.state.MainScreenUiState
 import city.zouitel.tags.model.Tag
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import city.zouitel.note.model.Note as InNote
 
+@Suppress("DeferredResultUnused")
 class MainScreenModel(
-    private val getAllById: NoteUseCase.GetAllNotesById,
-    private val getAllByOldest: NoteUseCase.GetAllNotesByOldest,
-    private val getAllByNewest: NoteUseCase.GetAllNotesByNewest,
-    private val getAllByName: NoteUseCase.GetAllNotesByName,
-    private val getAllRemoved: NoteUseCase.GetAllRemovedNotes,
-    private val getAllByPriority: NoteUseCase.GetAllNotesByPriority,
+    private val observeByDefault: NoteUseCase.ObserveByDefault,
+    private val observeByNewest: NoteUseCase.ObserveByNewest,
+    private val observeByOldest: NoteUseCase.ObserveByOldest,
+    private val observeByName: NoteUseCase.ObserveByName,
+    private val observeByPriority: NoteUseCase.ObserveByPriority,
+    private val observeByArchives: NoteUseCase.ObserveArchives,
     private val mapper: NoteMapper
 ): ScreenModel {
+    private val _uiState: MutableStateFlow<MainScreenUiState> = MutableStateFlow(MainScreenUiState())
+    val uiState: StateFlow<MainScreenUiState> = _uiState
+        .withFlow(MainScreenUiState())
 
-    private val _uiState: MutableStateFlow<MainScreenUiState> = MutableStateFlow(
-        MainScreenUiState()
-    )
-    internal val uiState: StateFlow<MainScreenUiState> = _uiState
-        .stateIn(
-            screenModelScope,
-            SharingStarted.WhileSubscribed(),
-            MainScreenUiState()
-        )
-
-    // for observing the data changes.
-    private val _allNotesById = MutableStateFlow<List<InNote>>(emptyList())
-    val allNotesById: StateFlow<List<InNote>> = _allNotesById.stateIn(screenModelScope, SharingStarted.WhileSubscribed(), listOf())
-
-    private val _allNotesByOldest = MutableStateFlow<List<InNote>>(emptyList())
-    val allNotesByOldest: StateFlow<List<InNote>> = _allNotesByOldest.stateIn(screenModelScope, SharingStarted.WhileSubscribed(), listOf())
-
-    private val _allNotesByNewest = MutableStateFlow<List<InNote>>(emptyList())
-    val allNotesByNewest: StateFlow<List<InNote>> = _allNotesByNewest.stateIn(screenModelScope, SharingStarted.WhileSubscribed(), listOf())
-
-    private val _allNotesByName = MutableStateFlow<List<InNote>>(emptyList())
-    val allNotesByName: StateFlow<List<InNote>> = _allNotesByName.stateIn(screenModelScope, SharingStarted.WhileSubscribed(), listOf())
-
-    private val _allTrashedNotes = MutableStateFlow<List<InNote>>(emptyList())
-    val allTrashedNotes: StateFlow<List<InNote>> = _allTrashedNotes.stateIn(screenModelScope, SharingStarted.WhileSubscribed(), listOf())
-
-    private val _allNotesByPriority = MutableStateFlow<List<InNote>>(emptyList())
-    val allNotesByPriority: StateFlow<List<InNote>> = _allNotesByPriority.stateIn(screenModelScope, SharingStarted.WhileSubscribed(), listOf())
-
-    private val _allRemindingNotes = MutableStateFlow<List<InNote>>(emptyList())
-    val allRemindingNotes: StateFlow<List<InNote>> = _allRemindingNotes.stateIn(screenModelScope, SharingStarted.WhileSubscribed(), listOf())
-
-    init {
-        screenModelScope.apply {
-            launch(Dispatchers.IO) {
-                getAllById.invoke().collect { notes -> _allNotesById.value = mapper.fromDomain(notes) }
-            }
-            launch(Dispatchers.IO) {
-                getAllByName.invoke().collect { notes -> _allNotesByName.value = mapper.fromDomain(notes) }
-            }
-            launch(Dispatchers.IO) {
-                getAllByNewest.invoke().collect { notes -> _allNotesByNewest.value = mapper.fromDomain(notes) }
-            }
-            launch(Dispatchers.IO) {
-                getAllByOldest.invoke().collect { notes -> _allNotesByOldest.value = mapper.fromDomain(notes) }
-            }
-            launch(Dispatchers.IO) {
-                getAllRemoved.invoke().collect { notes -> _allTrashedNotes.value = mapper.fromDomain(notes) }
-            }
-            launch(Dispatchers.IO) {
-                getAllByPriority.invoke().collect { notes -> _allNotesByPriority.value = mapper.fromDomain(notes) }
+    private val _observeDefaults = MutableStateFlow<List<Note>>(emptyList())
+    val observeDefaults: StateFlow<List<Note>> = _observeDefaults
+        .withFlow(emptyList()){
+            withAsync {
+                observeByDefault().collect { _observeDefaults.value = mapper.fromDomain(it) }
             }
         }
-    }
+
+    private val _observeNewest = MutableStateFlow<List<Note>>(emptyList())
+    val observeNewest: StateFlow<List<Note>> = _observeNewest
+        .withFlow(emptyList()){
+            withAsync {
+                observeByNewest().collect { _observeNewest.value = mapper.fromDomain(it) }
+            }
+        }
+
+    private val _observeOldest = MutableStateFlow<List<Note>>(emptyList())
+    val observeOldest: StateFlow<List<Note>> = _observeOldest
+        .withFlow(emptyList()){
+            withAsync {
+                observeByOldest().collect { _observeOldest.value = mapper.fromDomain(it) }
+            }
+        }
+
+    private val _observeNames = MutableStateFlow<List<Note>>(emptyList())
+    val observeNames: StateFlow<List<Note>> = _observeNames
+        .withFlow(emptyList()){
+            withAsync {
+                observeByName().collect { _observeNames.value = mapper.fromDomain(it) }
+            }
+        }
+
+    private val _observePriorities = MutableStateFlow<List<Note>>(emptyList())
+    val observePriorities: StateFlow<List<Note>> = _observePriorities
+        .withFlow(emptyList()){
+            withAsync {
+                observeByPriority().collect { _observePriorities.value = mapper.fromDomain(it) }
+            }
+        }
+
+    private val _observeArchives = MutableStateFlow<List<Note>>(emptyList())
+    val observeArchives: StateFlow<List<Note>> = _observeArchives
+        .withFlow(emptyList()){
+            withAsync {
+                observeByArchives().collect { _observeArchives.value = mapper.fromDomain(it) }
+            }
+        }
 
     fun updateSearchTitle(title: String) {
         screenModelScope.launch {
@@ -93,12 +89,6 @@ class MainScreenModel(
         }
     }
 
-    fun updateExpandedSortMenu(isShow: Boolean) {
-        screenModelScope.launch {
-            _uiState.value = _uiState.value.copy(expandedSortMenu = isShow)
-        }
-    }
-
     fun updateSelection(value: Boolean) {
         screenModelScope.launch {
             _uiState.value = _uiState.value.copy(isSelection = value)
@@ -109,17 +99,12 @@ class MainScreenModel(
         screenModelScope.launch {
             _uiState.value = _uiState.value.copy().addSelectedNote(note)
         }
+
     }
 
     fun clearSelectionNotes() {
         screenModelScope.launch {
             _uiState.value = _uiState.value.copy(selectedNotes = mutableStateListOf())
-        }
-    }
-
-    fun updateErasing(isErase: Boolean) {
-        screenModelScope.launch {
-            _uiState.value = _uiState.value.copy(isErase = isErase)
         }
     }
 
@@ -129,15 +114,9 @@ class MainScreenModel(
         }
     }
 
-    fun updateOptionsDialog(isShow: Boolean) {
+    fun updateSelectedNote(uid: String) {
         screenModelScope.launch {
-            _uiState.value = _uiState.value.copy(isOptionsDialog = isShow)
-        }
-    }
-
-    fun updateSelectedNote(data: Data?) {
-        screenModelScope.launch {
-            _uiState.value = _uiState.value.copy(selectedNote = data)
+            _uiState.value = _uiState.value.copy(selectedNote = uid)
         }
     }
 }
