9e0415e78831f8b9c2603bee30296574e37c1af5
yon <bourntivoli@gmail.com>
2025-02-12 14:55:44 +0100

    Refactor: Improve RecorderScreen with New Features and UI
    Improved the `RecorderScreen` to use a more efficient recording process and added new UI components.
    
    - Changed `RecorderScreen` to a class with a `uid` and random number.
    - Replaced `AlertDialog` with a custom layout in `Recorder`.
    - Added a timer display using `RecordTimer`.
    - Implemented start, pause, resume, and stop functionalities with `RecordController`.
    - Introduced a `Record` data class for handling record data.
    - Used `Action.Insert` to store the recorded file information.
    - Improved flow handling with `rememberCoroutineScope` and `LocalBottomSheetNavigator`.
    - Implemented file creation with random naming to prevent collision.
    - Added `reset` method in `RecorderScreenModel`.
    - Updated `RecorderScreenModel` to handle file operations and states.
    - Updated the behavior of the `stop` action in the `RecordController` to store the record and hide the `BottomSheet`.

--- ui/features/recorder/src/main/java/city/zouitel/recoder/ui/RecorderScreen.kt
+++ ui/features/recorder/src/main/java/city/zouitel/recoder/ui/RecorderScreen.kt
@@ -1,95 +1,78 @@
 package city.zouitel.recoder.ui
 
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.net.toUri
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
-import city.zouitel.logic.getRecPath
-import city.zouitel.recoder.model.Audio
-import city.zouitel.recoder.screenmodel.MediaRecordScreenModel
-import city.zouitel.recoder.screenmodel.RecorderScreenModel
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
+import city.zouitel.domain.utils.Action
+import city.zouitel.recoder.model.Record
+import city.zouitel.systemDesign.CommonBottomSheet
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.File
+import kotlin.random.Random
 import kotlin.time.DurationUnit
 
-data class RecorderScreen(
-    val id: String,
-    val onAction: () -> Unit
-): Screen {
+class RecorderScreen(val uid: String, private val random: Int = Random.nextInt()): Screen {
     @Composable
     override fun Content() {
-
-        Recorder(
-            mediaRecordModel = getScreenModel<MediaRecordScreenModel>(),
-            recorderModel =getScreenModel<RecorderScreenModel>()
-        )
+        Navigator(CommonBottomSheet {
+            Recorder(getScreenModel())
+        })
     }
 
     @Composable
-    private fun Recorder(
-        mediaRecordModel: MediaRecordScreenModel,
-        recorderModel: RecorderScreenModel
-    ) {
+    private fun Recorder(recorderModel: RecorderScreenModel) {
         val context = LocalContext.current
-        val path = id getRecPath context
-
+        val navBottom = LocalBottomSheetNavigator.current
+        val scope = rememberCoroutineScope()
         val uiState by remember(recorderModel, recorderModel::uiState).collectAsState()
+        val file = File(context.filesDir.path + "/" + random.toString() + ".mp3")
 
-        val record = mediaRecordModel.buildMediaRecord(id)
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.height(20.dp))
 
-        if (uiState.isRecording) {
-            record.start()
-        }
+            RecordTimer(uiState.seconds, uiState.minutes, uiState.hours)
 
-        AlertDialog(
-            onDismissRequest = {
-                runCatching {
-                    record.stop()
-                    record.reset()
-                    record.release()
-                }.onSuccess {
-                    recorderModel.onRecordingComplete(
-                        id,
-                        Audio(
-                            path = path,
-                            uri = path.toUri().toString(),
-                            duration = recorderModel.time.toLong(DurationUnit.SECONDS)
+            RecordController(
+                recorderModel = recorderModel,
+                onStart = {
+                    recorderModel.start(file)
+                },
+                onPause = recorderModel::pause,
+                onResume = recorderModel::resume,
+                onStop = {
+                    recorderModel.stop()
+                    recorderModel.sendAction(
+                        Action.Insert(
+                            Record(
+                                uid = uid,
+                                path = file.path,
+                                duration = recorderModel.time.toLong(DurationUnit.MILLISECONDS)
+                            )
                         )
                     )
-                    recorderModel.stop()
-                }
-                onAction.invoke()
-            },
-            confirmButton = {},
-            text = {
-                RecordController(
-                    recorderModel = recorderModel,
-                    mediaRecorder = record,
-                    onStart = { recorderModel.start() },
-                    onPause = { recorderModel.pause() },
-                    onStop = {
-                        runCatching {
-                            record.stop()
-                            record.reset()
-                            record.release()
-                        }.onSuccess {
-                            recorderModel.onRecordingComplete(
-                                id,
-                                Audio(
-                                    path = path,
-                                    uri = path.toUri().toString(),
-                                    duration = recorderModel.time.toLong(DurationUnit.SECONDS)
-                                )
-                            )
-                            recorderModel.stop()
-                        }
-                        onAction.invoke()
+                    recorderModel.reset()
+                    scope.launch {
+                        delay(400)
+                        navBottom.hide()
                     }
-                )
-            }
-        )
+                }
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+        }
     }
 }
