9da885cbc3240e59b99ec89544934d23bd1a2369
yon <bourntivoli@gmail.com>
2025-02-12 14:59:29 +0100

    Refactor: Enhance Audio Player UiState Data Structure
    Modified the `UiState` data class for the audio player feature to include additional properties.
    
    - Added `playingRecord` to hold the currently playing `Record`.
    - Added `isLoading` to indicate the loading state.
    - Added `currentPath` to store the path of the current record.
    - Updated `amplitudes` to default values to 1.

--- ui/features/player/audio/src/main/java/city/zouitel/audio/state/UiState.kt
+++ ui/features/player/audio/src/main/java/city/zouitel/audio/state/UiState.kt
@@ -1,5 +1,6 @@
 package city.zouitel.audio.state
 
+import city.zouitel.audio.model.Record
 import city.zouitel.systemDesign.CommonIcons
 
 internal data class UiState(
@@ -7,5 +8,8 @@ internal data class UiState(
     val icon: Int = CommonIcons.PLAY_ICON_24,
     val displayName: String = "",
     val progress: Float = 0F,
-    val amplitudes: List<Int> = (0..100).toList().map { 5 }
+    val amplitudes: List<Int> = (0..100).toList().map { 1 },
+    val playingRecord: Record? = null,
+    val isLoading: Boolean = false,
+    val currentPath: String = ""
 )
