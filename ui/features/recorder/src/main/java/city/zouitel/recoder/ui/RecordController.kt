3f89ae22b4e5629aede16fae4a5858043890c731
yon <bourntivoli@gmail.com>
2025-02-12 14:53:11 +0100

    Refactor: Improve UI and state management in RecordController
    Enhanced the `RecordController` composable for better UI/UX and state management.
    
    - Removed `MediaRecorder` from the `RecordController` function parameters.
    - Updated the `clickable` modifier to use the `onClick` lambda directly for better readability.
    - Added an `onResume` callback function to handle resuming recording from pause.
    - Used `animateContentSize` modifier for smooth content size changes.
    - Changed the color of icons to `onSurface` and `errorContainer`.
    - Replaced the `Column` layout with a `Row` layout for a horizontal arrangement.
    - Changed the `updatePausingState` function call to update the pause status directly instead of passing mediaRecorder.
    - Updated `updateRecordState` to just update record state.
    - Removed unnecessary `verticalArrangement` and `horizontalAlignment`.
    - Removed unnecessary comment about padding parameter.

--- ui/features/recorder/src/main/java/city/zouitel/recoder/ui/RecordController.kt
+++ ui/features/recorder/src/main/java/city/zouitel/recoder/ui/RecordController.kt
@@ -1,109 +1,92 @@
 package city.zouitel.recoder.ui
 
 import android.annotation.SuppressLint
-import android.media.MediaRecorder
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import city.zouitel.recoder.screenmodel.RecorderScreenModel
-import city.zouitel.systemDesign.CommonRow
 import city.zouitel.systemDesign.CommonIcons.MIC_ICON_36
 import city.zouitel.systemDesign.CommonIcons.PAUSE_CIRCLE_FILLED_ICON_36
 import city.zouitel.systemDesign.CommonIcons.PLAY_CIRCLE_FILLED_ICON_36
 import city.zouitel.systemDesign.CommonIcons.STOP_CIRCLE_ICON_36
+import city.zouitel.systemDesign.CommonRow
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun RecordController(
     recorderModel: RecorderScreenModel,
-    mediaRecorder: MediaRecorder,
     onStart: () -> Unit = {},
     onPause: () -> Unit = {},
-    onStop: () -> Unit = {},
+    onResume: () -> Unit = {},
+    onStop: () -> Unit = {}
 ) {
-
     val uiState by remember(recorderModel, recorderModel::uiState).collectAsState()
 
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    Row {
+        CommonRow(
+            modifier = Modifier
+                .animateContentSize()
+                .fillMaxWidth()
+        ) {
 
-        RecordTimer(uiState.seconds,uiState.minutes,uiState.hours)
-
-        Row {
-            CommonRow(
-                modifier = Modifier.fillMaxWidth()
+            AnimatedVisibility(
+                visible = !uiState.isRecording,
+                enter = scaleIn(),
+                exit = scaleOut()
             ) {
+                Icon(
+                    painter = painterResource(id = MIC_ICON_36),
+                    contentDescription = null,
+                    modifier = Modifier.clickable(onClick = onStart),
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
 
-                AnimatedVisibility(
-                    visible = !uiState.isRecording,
-                    enter = scaleIn(),
-                    exit = scaleOut()
-                ) {
+            AnimatedVisibility(
+                visible = uiState.isRecording,
+                enter = scaleIn(),
+                exit = scaleOut()
+            ) {
+                if (uiState.isPause) {
                     Icon(
-                        painter = painterResource(id = MIC_ICON_36),
+                        painter = painterResource(id = PLAY_CIRCLE_FILLED_ICON_36),
                         contentDescription = null,
-                        modifier = Modifier.clickable {
-                            onStart()
-                            recorderModel.updateRecordState(true)
-                        }
+                        modifier = Modifier.clickable(onClick = onResume),
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+
+                } else {
+                    Icon(
+                        painter = painterResource(id = PAUSE_CIRCLE_FILLED_ICON_36),
+                        contentDescription = null,
+                        modifier = Modifier.clickable(onClick = onPause),
+                        tint = MaterialTheme.colorScheme.onSurface
                     )
                 }
+            }
 
-                AnimatedVisibility(
-                    visible = uiState.isRecording,
-                    enter = scaleIn(),
-                    exit = scaleOut()
-                ) {
-                    if (uiState.isPause) {
-                        Icon(
-                            painter = painterResource(id = PLAY_CIRCLE_FILLED_ICON_36),
-                            contentDescription = null,
-                            modifier = Modifier.clickable {
-                                mediaRecorder.resume()
-                                recorderModel.updatePausingState(false)
-                                onStart()
-                            }
-                        )
-
-                    } else {
-                        Icon(
-                            painter = painterResource(id = PAUSE_CIRCLE_FILLED_ICON_36),
-                            contentDescription = null,
-                            modifier = Modifier.clickable {
-                                mediaRecorder.pause()
-                                recorderModel.updatePausingState(true)
-                                onPause()
-                            }
-                        )
-                    }
-                }
-
-                AnimatedVisibility(
-                    visible = uiState.isRecording,
-                    enter = scaleIn(),
-                    exit = scaleOut()
-                ) {
-                    Icon(
-                        painter = painterResource(id = STOP_CIRCLE_ICON_36),
-                        contentDescription = null,
-                        modifier = Modifier.clickable {
-                            onStop()
-                        },
-                        tint = MaterialTheme.colorScheme.error
-                    )
-                }
+            AnimatedVisibility(
+                visible = uiState.isRecording,
+                enter = scaleIn(),
+                exit = scaleOut()
+            ) {
+                Icon(
+                    painter = painterResource(id = STOP_CIRCLE_ICON_36),
+                    contentDescription = null,
+                    modifier = Modifier.clickable(onClick = onStop),
+                    tint = MaterialTheme.colorScheme.errorContainer
+                )
             }
         }
     }
