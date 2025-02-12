06d0d55c8eb1d160a8c364eacea9ae6549d87d4b
yon <bourntivoli@gmail.com>
2025-02-12 14:51:07 +0100

    Feat: Introduce Record data class for audio files
    Added the `Record` data class to `city.zouitel.repository.model` for managing audio file information.
    
    -   The `Record` class is defined with fields `id`, `uid`, `title`, `path`, and `duration`.
    -   It uses the `DEFAULT_LONG` and `DEFAULT_TXT` constants for default values.
    -   The class is annotated with `@Keep` to ensure that it is not obfuscated during build process.
    - The `Record` data class acts as a simple "data carrier" for audio file information.

---
+++ core/local/repository/src/main/java/city/zouitel/repository/model/Record.kt
@@ -0,0 +1,14 @@
+package city.zouitel.repository.model
+
+import androidx.annotation.Keep
+import city.zouitel.domain.utils.Constants.DEFAULT_LONG
+import city.zouitel.domain.utils.Constants.DEFAULT_TXT
+
+@Keep
+data class Record(
+    val id: Long = DEFAULT_LONG,
+    val uid: String = DEFAULT_TXT,
+    val title: String = DEFAULT_TXT,
+    val path: String = DEFAULT_TXT,
+    val duration: Long = DEFAULT_LONG
+)
