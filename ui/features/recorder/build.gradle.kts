d202b92b3b897a26357d209e0767ae6c3dbd98ce
yon <bourntivoli@gmail.com>
2025-02-12 14:45:16 +0100

    Feat: Add Media3 ExoPlayer dependency to recorder module
    Added the Media3 ExoPlayer dependency to the recorder module's `build.gradle.kts` file.
    
    - Added `implementation(libs.media3.exoplayer)` to incorporate ExoPlayer functionality.
    - Added `implementation(projects.ui.features.player.audio)` to incorporate the audio player module.

--- ui/features/recorder/build.gradle.kts
+++ ui/features/recorder/build.gradle.kts
@@ -15,7 +15,11 @@ dependencies {
     implementation(projects.common.systemDesign)
     implementation(projects.common.logic)
     implementation(projects.core.local.datastore)
+    implementation(projects.ui.features.player.audio)
 
     //AndroidX.
     implementation(libs.constraintlayout)
+
+    //Media3.
+    implementation(libs.media3.exoplayer)
 }
