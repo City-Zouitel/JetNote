44a5736b45c14e0764c2015f004674ceb596f56d
yon <bourntivoli@gmail.com>
2025-02-12 14:48:27 +0100

    Refactor: Add RecordScreenModel and RecordMapper
    - Added `RecordScreenModel` for managing the recording screen's state and logic.
    - Added `RecordMapper` for mapping record-related data.
    - Updated modules file to include new `RecordScreenModel` and `RecordMapper`.

--- ui/features/player/audio/src/main/java/city/zouitel/audio/di/modules.kt
+++ ui/features/player/audio/src/main/java/city/zouitel/audio/di/modules.kt
@@ -6,10 +6,12 @@ import city.zouitel.audio.audio.AudioManager
 import city.zouitel.audio.audio.AudioRepository
 import city.zouitel.audio.audio.LocalMediaDataSource
 import city.zouitel.audio.mapper.AudioMapper
+import city.zouitel.audio.mapper.RecordMapper
 import city.zouitel.audio.player.PlaybackManager
 import city.zouitel.audio.ui.component.AudioScreenModel
 import city.zouitel.audio.ui.list.AudioListScreen
 import city.zouitel.audio.ui.list.AudioListScreenModel
+import city.zouitel.audio.ui.record.RecordScreenModel
 import city.zouitel.domain.provider.SharedScreen
 import kotlinx.coroutines.Dispatchers
 import linc.com.amplituda.Amplituda
@@ -29,10 +31,13 @@ val audioPlayerDIModule = module {
 
     factory { AudioScreenModel(get(), get(), get(), get(), get(), get()) }
     factory { AudioListScreenModel(get(), get(), get()) }
+    factory { RecordScreenModel(androidContext(), get(), get(), get(), get(), get()) }
 
     factoryOf(::AudioMapper)
+    factoryOf(::RecordMapper)
 
     singleOf(::PlaybackManager) bind Player.Listener::class
+
     single { PlaybackManager(androidContext(), get()) }
 }
 
