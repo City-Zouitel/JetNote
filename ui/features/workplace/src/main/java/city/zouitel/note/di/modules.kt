fb389ad743c9eed3205d73bfe9202e0cbe4e513e
yon <bourntivoli@gmail.com>
2025-02-26 22:30:25 +0100

    Refactor: Update WorkplaceScreenModule and Related Components
    Modified the `workplaceScreenModule` to streamline screen registration and component dependencies.
    
    - Replaced `Rational` with `Options` in screen registration.
    - Removed the `permissionState` and `permissionName` parameters from the `OptionsScreen`.
    - Removed `imageLaunch` from the `WorkplaceScreenModel` constructor parameter.
    - Updated the dependencies and removed unused imports.

--- ui/features/workplace/src/main/java/city/zouitel/note/di/modules.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/di/modules.kt
@@ -4,13 +4,10 @@ import cafe.adriel.voyager.core.registry.screenModule
 import city.zouitel.domain.provider.SharedScreen
 import city.zouitel.note.mapper.DataMapper
 import city.zouitel.note.providers.Options
-import city.zouitel.note.providers.Rational
 import city.zouitel.note.ui.DataScreenModel
-import city.zouitel.note.ui.bottom_bar.OptionsScreen
-import city.zouitel.note.ui.bottom_bar.RationalScreen
+import city.zouitel.note.ui.workplace.OptionsScreen
 import city.zouitel.note.ui.workplace.WorkplaceScreen
 import city.zouitel.note.ui.workplace.WorkplaceScreenModel
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
@@ -23,7 +20,6 @@ val workplaceDIModule = module {
     }
 }
 
-@OptIn(ExperimentalPermissionsApi::class)
 val workplaceScreenModule = screenModule {
     register<SharedScreen.Workplace> {
         WorkplaceScreen(
@@ -42,15 +38,7 @@ val workplaceScreenModule = screenModule {
             uid = it.uid,
             titleState = it.titleState,
             descriptionState = it.descriptionState,
-            priorityState = it.priorityState,
-            imageLaunch = it.imageLaunch
-        )
-    }
-
-    register<Rational> {
-        RationalScreen(
-            permissionState = it.permissionState,
-            permissionName = it.permissionName
+            priorityState = it.priorityState
         )
     }
 }
