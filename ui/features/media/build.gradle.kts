afb76c9e2a36ac2450e2fea515124b4c41b7bae1
yon <bourntivoli@gmail.com>
2025-02-26 22:17:01 +0100

    Feat: Add Video Player module dependency to Media module
    Added the `video` player module as a dependency to the `media` module. This change allows the media module to use the video player features.
    
    - Added  `implementation(project(":ui:features:player:video"))` to the `media` module's `build.gradle.kts` file.
    - Added  `implementation(projects.ui.features.player.audio)` to the `media` module's `build.gradle.kts` file.

--- ui/features/media/build.gradle.kts
+++ ui/features/media/build.gradle.kts
@@ -12,9 +12,11 @@ dependencies {
     //Modules.
     implementation(projects.domain)
     implementation(projects.common.logic)
+    implementation(projects.ui.features.player.audio)
 
     //Coil.
     implementation(libs.coil.compose)
     implementation(libs.coil.svg)
     implementation(libs.zoomable.coil)
+    implementation(project(":ui:features:player:video"))
 }
