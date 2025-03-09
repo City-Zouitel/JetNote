43d3dc639535f4cecf66d18f816ec87d57f81127
yon <bourntivoli@gmail.com>
2025-03-09 18:52:30 +0100

    Refactor: Enhance WorkplaceScreenModel with WorkManager and Content Handling
    Refactored `WorkplaceScreenModel` to integrate `WorkManager` for background tasks and to handle content reception from other applications.
    
    - Added `Application` parameter to the constructor to initialize `WorkManager`.
    - Replaced `stateIn` with `withFlow` to manage the UI state flow.
    - Implemented `handleContentReceiver` to process content received from other applications.
    - Added `handleResultContracts` to handle the results of content contracts.
    - Created `enqueueFileProcessing` to manage background work using `WorkManager`, including setting constraints, input data, and queuing the work.
    - Added `CopyMediaWorker` for media copy operations.
    - Updated dependencies to include `androidx.work`.
    - Introduced `WORK_NAME` for unique work identification.
    - Updated WorkplaceScreenModel to add logic for handling content and working in the background.

--- ui/features/workplace/src/main/java/city/zouitel/note/ui/workplace/WorkplaceScreenModel.kt
+++ ui/features/workplace/src/main/java/city/zouitel/note/ui/workplace/WorkplaceScreenModel.kt
@@ -1,25 +1,35 @@
 package city.zouitel.note.ui.workplace
 
+import android.app.Application
+import android.content.ClipData
+import android.net.Uri
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.content.MediaType
+import androidx.compose.foundation.content.TransferableContent
+import androidx.compose.foundation.content.hasMediaType
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy.*
+import androidx.work.NetworkType
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
+import city.zouitel.logic.withFlow
 import city.zouitel.note.state.WorkplaceUiState
+import city.zouitel.note.worker.CopyMediaWorker
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
-class WorkplaceScreenModel: ScreenModel {
-
-    private val _uiState: MutableStateFlow<WorkplaceUiState> = MutableStateFlow(
-        WorkplaceUiState()
-    )
+@OptIn(ExperimentalFoundationApi::class)
+class WorkplaceScreenModel(application: Application): ScreenModel {
+    
+    private val _uiState: MutableStateFlow<WorkplaceUiState> = MutableStateFlow(WorkplaceUiState())
     internal var uiState: StateFlow<WorkplaceUiState> = _uiState
-        .stateIn(
-            screenModelScope,
-            SharingStarted.WhileSubscribed(),
-            WorkplaceUiState()
-        )
+        .withFlow(WorkplaceUiState())
+
+    private var workManager = WorkManager.getInstance(application)
 
     fun updatePriority(priority: Int): WorkplaceScreenModel {
         screenModelScope.launch {
@@ -52,5 +62,50 @@ class WorkplaceScreenModel: ScreenModel {
         screenModelScope.launch {
             _uiState.value = _uiState.value.copy(isDescriptionFieldFocused = value)
         }
+    }
+
+    internal fun handleContentReceiver(content: TransferableContent, uid: String) {
+        if (content.hasMediaType(MediaType.All)) {
+            val clipData = content.clipEntry.clipData
+            for (index in 0 until clipData.itemCount) {
+                val item: ClipData.Item = clipData.getItemAt(index)
+                val uri = item.uri
+                if (uri != null) {
+                    enqueueFileProcessing(uid, uri)
+                }
+            }
+        }
+    }
+
+    internal fun handleResultContracts(uid: String, uri: Uri) {
+        enqueueFileProcessing(uid, uri)
+    }
+
+    private fun enqueueFileProcessing(uid: String, uri: Uri) {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+            .setRequiresStorageNotLow(true)
+            .build()
+
+//        val inputData = Data.Builder()
+//            .putString("CONTENT_URI", uri.toString())
+//            .putString("CONTENT_UID", uid)
+//            .build()
+
+        val inputData = workDataOf(
+            "CONTENT_URI" to uri.toString(),
+            "CONTENT_UID" to uid
+        )
+
+        val request = androidx.work.OneTimeWorkRequestBuilder<CopyMediaWorker>()
+            .setInputData(inputData)
+            .setConstraints(constraints)
+            .build()
+
+        workManager.enqueueUniqueWork(WORK_NAME, KEEP, request)
+    }
+
+    companion object {
+        const val WORK_NAME = "unique_workplace_work"
     }
 }
