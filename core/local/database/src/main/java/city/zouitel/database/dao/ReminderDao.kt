dbc7efd3fd787a7087150fbb497c4168da3bd494
yon <bourntivoli@gmail.com>
2025-03-13 01:46:19 +0100

    Refactor: Rename and Improve ReminderDao methods
    Renamed methods in `ReminderDao` to enhance clarity and functionality.
    
    - Renamed `updateById` to `update` for better semantic understanding.
    - Renamed `deleteById` to `delete` for consistency.
    - Removed `deleteByUid` method.
    - Added a `deleteDrafts` method to remove unlinked reminder drafts from the database.

--- core/local/database/src/main/java/city/zouitel/database/dao/ReminderDao.kt
+++ core/local/database/src/main/java/city/zouitel/database/dao/ReminderDao.kt
@@ -38,10 +38,10 @@ interface ReminderDao {
     /**
      * Updates the record with the given ID, setting the 'passed' column to 1 (true).
      *
-     * @param id The ID of the record to updateById.
+     * @param id The ID of the record to update.
      */
     @Query("UPDATE $TABLE_NAME SET $PASSED = 1 WHERE $ID = :id")
-    suspend fun updateById(id: Int)
+    suspend fun update(id: Int)
 
     /**
      * Deletes a row from the database table based on the provided ID.
@@ -49,18 +49,26 @@ interface ReminderDao {
      * @param id: The ID of the row to be deleted.
      */
     @Query("DELETE FROM $TABLE_NAME WHERE $ID = :id")
-    suspend fun deleteById(id: Int)
+    suspend fun delete(id: Int)
 
     /**
-     * Deletes a record from the [TABLE_NAME] table based on the provided UUID.
+     * Deletes draft notes from the database.
      *
-     * @param uid The UUID of the record to delete.
-     * @throws Exception If any error occurs during the database operation.
-     * @return Unit. The function returns nothing explicitly, but it suspends until the database operation is complete.
+     * This function removes notes that are considered drafts, meaning they
+     * are present in the main table (`TABLE_NAME`) but are not linked to any
+     * existing note data in the `note_data_table`. This effectively cleans up
+     * incomplete or abandoned notes that were not properly saved.
+     *
+     * The deletion is performed using a SQL `DELETE` query with a subquery
+     * to identify drafts. The subquery selects the UUIDs from the `note_data_table`,
+     * and the outer query then deletes any records from `TABLE_NAME` whose UUID
+     * is not present in that result set.
+     *
+     * This function should be called within a coroutine scope because it's a
+     * suspend function.
+     *
+     * @throws SQLiteException If an error occurs while executing the database query.
      */
-    @Query("DELETE FROM $TABLE_NAME WHERE $UUID = :uid")
-    suspend fun deleteByUid(uid: String)
-
     @Query("DELETE FROM $TABLE_NAME WHERE $UUID NOT IN (SELECT $UUID FROM note_data_table)")
     suspend fun deleteDrafts()
 }
