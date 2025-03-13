da0fc4ee6b650de572f5d9d1ba1fc519dc3450e6
yon <bourntivoli@gmail.com>
2025-03-13 01:50:20 +0100

    Refactor: Set Default Values for Task Entity Columns
    Modified the `Task` entity to explicitly define default values for its columns.
    
    -   Set `id` default value to `0`.
    -   Set `uid` default value to an empty string.
    -   Set `isDone` default value to `false`.
    - Updated `ColumnInfo` annotations to include `defaultValue` attributes.

--- core/local/database/src/main/java/city/zouitel/database/model/Task.kt
+++ core/local/database/src/main/java/city/zouitel/database/model/Task.kt
@@ -4,6 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import city.zouitel.database.model.Task.Companion.TABLE_NAME
+import city.zouitel.database.utils.Constants.DEFAULT_BOOLEAN
+import city.zouitel.database.utils.Constants.DEFAULT_NUM
+import city.zouitel.database.utils.Constants.DEFAULT_TXT
 import city.zouitel.database.utils.Constants.DONE
 import city.zouitel.database.utils.Constants.ID
 import city.zouitel.database.utils.Constants.TITLE
@@ -27,10 +30,10 @@ import city.zouitel.database.utils.Constants.UUID
 @Entity(tableName = TABLE_NAME)
 data class Task(
     @PrimaryKey(autoGenerate = false)
-    @ColumnInfo(name = ID, defaultValue = "0") val id: Long,
-    @ColumnInfo(name = UUID, defaultValue = "") val uid: String,
+    @ColumnInfo(name = ID, defaultValue = DEFAULT_NUM) val id: Long,
+    @ColumnInfo(name = UUID, defaultValue = DEFAULT_TXT) val uid: String,
     @ColumnInfo(name = TITLE, defaultValue = "null") val item: String?,
-    @ColumnInfo(name = DONE, defaultValue = "false") val isDone: Boolean = false
+    @ColumnInfo(name = DONE, defaultValue = DEFAULT_BOOLEAN) val isDone: Boolean = false
 ) {
     companion object {
         const val TABLE_NAME = "tasks_table"
